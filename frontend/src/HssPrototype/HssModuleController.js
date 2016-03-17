702db933fb1edfc9facad655d083fdbab7303386
Nico <nico@pulilab.com>
2016-03-17 10:42:50 +0100

    Hss-Prototype: introduction

--- frontend/src/HssPrototype/HssModuleController.js
+++ frontend/src/HssPrototype/HssModuleController.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import { hss, interventionsLib, applicationsLib, taxonomyLib } from './hssMockData';
+import intro from 'intro.js';
 
 class HssModuleController {
 
@@ -27,6 +28,7 @@ class HssModuleController {
         this.applicationRow = this.applicationRows();
         this.tileClickCounter = 0;
         this.startTile = {};
+        this.startIntro = this.hssIntro;
     }
 
     headerMiddleColumnDecorator() {
@@ -74,7 +76,8 @@ class HssModuleController {
                     clickHandler: this.motherClickHandler.bind(this),
                     columnId: value,
                     activated: hss[value].activated,
-                    className: ((value + 1) % 2 === 0 ? 'even' : 'odd') + ' mother'
+                    className: ((value + 1) % 2 === 0 ? 'even' : 'odd') + ' mother',
+                    introName: 'mother_middle_' + value
                 };
             })
             .filter({
@@ -105,7 +108,8 @@ class HssModuleController {
                 columnId: value,
                 activated: hss[value].activated,
                 empty: !hss[value].child.title,
-                clickHandler: this.childClickHandler.bind(this)
+                clickHandler: this.childClickHandler.bind(this),
+                introName: 'child_middle_' + value
             };
         });
     }
@@ -129,7 +133,8 @@ class HssModuleController {
                 isInput: true,
                 columnId: value,
                 activated: false,
-                selectValues: this.interventions[value]
+                selectValues: this.interventions[value],
+                introName: 'interventions_middle_' + value
             };
         });
     }
@@ -190,7 +195,8 @@ class HssModuleController {
             isHeader: true,
             isMain: true,
             rowBubbles: [],
-            classGenerator: this.applicationClassGenerator.bind(this)
+            classGenerator: this.applicationClassGenerator.bind(this),
+            introName: 'applications_header_' + index
         }];
     }
 
@@ -211,7 +217,8 @@ class HssModuleController {
                 isMain: true,
                 applicationId: this.applications[index].id,
                 appLabel: this.applications[index].id,
-                activated: hss[value].activated
+                activated: hss[value].activated,
+                introName: 'applications_middle_' + value
             };
         });
     }
@@ -227,7 +234,8 @@ class HssModuleController {
             isTax: true,
             isMain: true,
             rowIndex: index,
-            rowEnabled: false
+            rowEnabled: false,
+            introName: 'taxonomy_column_' + index
         }];
     }
 
@@ -422,8 +430,73 @@ class HssModuleController {
     }
 
     classGenerator(tile) {
-        return tile.className + ' ' + (tile.activated ? 'activated' : '')
-            + ' ' + (tile.subAppOpen ? 'app-open' : 'app-closed');
+        return tile.className + ' ' + (tile.activated ? 'activated' : 'not-activated')
+            + ' ' + (tile.subAppOpen ? 'app-open' : 'app-closed')
+            + ' ' + tile.introName;
+    }
+
+    element(name) {
+        return document.querySelectorAll(name)[0];
+    }
+
+    hssIntro() {
+        const introObj = intro.introJs();
+        introObj.setOptions(
+            {
+                steps: [
+                    {
+                        intro: 'HSS Framework short tutorial'
+                    },
+                    {
+                        intro: 'to switch between edit mode and view mode do not use it for now',
+                        element: this.element('.intro_edit_mode')
+                    },
+                    {
+                        intro: 'Activate main continuum element by clicking here, do it now!',
+                        element: this.element('.mother_middle_1')
+                    },
+                    {
+                        intro: 'Select this one too for the sake of tutorial',
+                        element: this.element('.mother_middle_2')
+                    },
+                    {
+                        intro: 'Activate sub continuum element by clicking here, ' +
+                        'this element can not be activated because his mother column is not',
+                        element: this.element('.child_middle_5')
+                    },
+                    {
+                        intro: 'Once a column is activate select interventions from the select menu here',
+                        element: this.element('.interventions_middle_1')
+                    },
+                    {
+                        intro: 'Select one or more constraints from here',
+                        element: this.element('.intro_constraints')
+                    },
+                    {
+                        intro: 'To create an editing bubble click on an activated application cell',
+                        element: this.element('.applications_middle_1')
+                    },
+                    {
+                        intro: 'And then click to another application cell',
+                        element: this.element('.applications_middle_2')
+                    },
+                    {
+                        intro: 'Is now possible to edit the newly created bubble',
+                        element: this.element('.applications_middle_1')
+                    },
+                    {
+                        intro: 'Once a bubble is created the row get activated and it is possible to select constraints',
+                        element: this.element('.taxonomy_column_0')
+                    },
+                    {
+                        intro: 'The application is now active, to show the subapp click on the application name',
+                        element: this.element('.applications_header_0')
+                    }
+
+                ]
+            }
+        );
+        introObj.start();
     }
 
 }
