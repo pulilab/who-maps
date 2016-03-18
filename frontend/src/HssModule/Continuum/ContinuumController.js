d2b772f6502cac1c67f0cb2ac4bb30b185fd84c7
Nico <nico@pulilab.com>
2016-03-18 12:33:06 +0100

    HSS: continuum component vip

--- frontend/src/HssModule/Continuum/ContinuumController.js
+++ frontend/src/HssModule/Continuum/ContinuumController.js
@@ -1,22 +1,26 @@
 import _ from 'lodash';
+import { hss } from '../hssMockData';
 
 class ContinuumController {
 
     constructor($timeout) {
         const vm = this;
         $timeout(() => {
+            vm.EE = window.EE;
             vm.editMode = false;
-            vm.uList = this.uListGenerator();
             vm.firstRow = this.firstRowGenerator();
+            vm.motherRow = this.motherRowGenerator();
+            vm.childRow = this.childRowGenerator();
         });
     }
 
-    uListGenerator() {
-        return _.range(this.tiles);
+    editModeChange() {
+        const vm = this;
+        this.EE.emit('editMode', vm.editMode);
     }
 
     firstRowGenerator() {
-        return _.map(this.uList, (value) => {
+        return _.map(_.range(this.tiles), (value) => {
             return {
                 icon: require('./images/continuum-' + (value + 1) + '.svg'),
                 colSpan: 1,
@@ -25,6 +29,51 @@ class ContinuumController {
             };
         });
     }
+
+    motherRowGenerator() {
+        return _.chain(this.tiles)
+            .range()
+            .map((value) => {
+                return {
+                    content: hss[value].mother.title,
+                    colSpan: hss[value].mother.span,
+                    rowSpan: 1,
+                    invisible: _.isEmpty(hss[value].mother),
+                    clickHandler: void 0, // wire the clicking function
+                    columnId: value,
+                    activated: hss[value].activated,
+                    className: ((value + 1) % 2 === 0 ? 'even' : 'odd') + ' mother',
+                    introName: 'mother_middle_' + value
+                };
+            })
+            .filter({
+                invisible: false
+            })
+            .value();
+    }
+
+    childRowGenerator() {
+        return _.chain(this.tiles)
+            .range()
+            .map((value) => {
+                return {
+                    content: hss[value].child.title,
+                    className: (!hss[value].child.title ? 'empty' : '')
+                    + ' ' + ((value + 1) % 2 === 0 ? 'even' : 'odd')
+                    + ' child',
+                    colSpan: 1,
+                    rowSpan: 1,
+                    columnId: value,
+                    activated: hss[value].activated,
+                    empty: !hss[value].child.title,
+                    clickHandler: void 0, // wire the clicking function
+                    introName: 'child_middle_' + value,
+                    invisible: false
+                };
+            })
+            .value();
+    }
+
 
     static continuumFactory() {
         require('./Continuum.scss');
--- frontend/src/HssModule/Continuum/continuumController-spec.js
+++ frontend/src/HssModule/Continuum/continuumController-spec.js
@@ -1,4 +1,5 @@
 import ContinuumController from './ContinuumController';
+import { EE } from '../../Common/';
 
 /* global define, it, describe, expect, beforeEach */
 
@@ -10,13 +11,21 @@ const $timeout = (arg) => {
 describe('continuumController', () => {
 
     beforeEach(() => {
+        EE.initialize();
         cc = ContinuumController.continuumFactory()($timeout);
         cc.tiles = 7;
-        cc.uList = cc.uListGenerator();
+    });
+
+    it('should have a function that emit an event if editMode is changed', () => {
+        cc.editMode = true;
+        const spy = jasmine.createSpy();
+        window.EE.on('editMode', spy);
+        cc.editModeChange();
+        expect(spy).toHaveBeenCalled();
     });
 
 
-    it('should have a function that return a map of tiles', () => {
+    it('should have a function that return the icon header map of tiles', () => {
         const firstRow = cc.firstRowGenerator();
         expect(firstRow.length).toBe(cc.tiles);
 
@@ -25,6 +34,41 @@ describe('continuumController', () => {
             expect(value.hasOwnProperty('colSpan')).toBeTruthy();
             expect(value.hasOwnProperty('rowSpan')).toBeTruthy();
             expect(value.hasOwnProperty('className')).toBeTruthy();
+        });
+
+    });
+
+    it('should have a function that return the mother header map of tiles', () => {
+        const motherRow = cc.motherRowGenerator();
+
+        motherRow.forEach((value) => {
+            expect(value.hasOwnProperty('content')).toBeTruthy();
+            expect(value.hasOwnProperty('colSpan')).toBeTruthy();
+            expect(value.hasOwnProperty('rowSpan')).toBeTruthy();
+            expect(value.hasOwnProperty('className')).toBeTruthy();
+            expect(value.hasOwnProperty('invisible')).toBeTruthy();
+            expect(value.hasOwnProperty('clickHandler')).toBeTruthy();
+            expect(value.hasOwnProperty('columnId')).toBeTruthy();
+            expect(value.hasOwnProperty('activated')).toBeTruthy();
+            expect(value.hasOwnProperty('introName')).toBeTruthy();
+        });
+
+    });
+
+    it('should have a function that return the child header map of tiles', () => {
+        const childRow = cc.childRowGenerator();
+
+        childRow.forEach((value) => {
+            expect(value.hasOwnProperty('content')).toBeTruthy();
+            expect(value.hasOwnProperty('colSpan')).toBeTruthy();
+            expect(value.hasOwnProperty('rowSpan')).toBeTruthy();
+            expect(value.hasOwnProperty('className')).toBeTruthy();
+            expect(value.hasOwnProperty('invisible')).toBeTruthy();
+            expect(value.hasOwnProperty('clickHandler')).toBeTruthy();
+            expect(value.hasOwnProperty('columnId')).toBeTruthy();
+            expect(value.hasOwnProperty('activated')).toBeTruthy();
+            expect(value.hasOwnProperty('introName')).toBeTruthy();
+            expect(value.hasOwnProperty('empty')).toBeTruthy();
         });
 
     });
