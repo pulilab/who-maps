16ec74d536e3833272e20098ecf384b61c7294e3
lordfuoco <lordfuoco@gmail.com>
2016-03-30 13:35:09 +0200

    Minor Linting

--- frontend/src/HssModule/hssModuleController-spec.js
+++ frontend/src/HssModule/hssModuleController-spec.js
@@ -1,30 +1,30 @@
 import HssModuleController from './HssModuleController';
 
 /* global define, it, describe, expect */
+let cc = {};
 
 describe('HssModuleController', () => {
-	it('can be instatiated, and is defined', () => {
-		const cc = HssModuleController.hssControllerFactory()({});
-		expect(cc).toBeDefined();
-	});
+    it('can be instatiated, and is defined', () => {
+        cc = HssModuleController.hssControllerFactory()({});
+        expect(cc).toBeDefined();
+    });
 
-	it('has a constructor', () => {
-		const cc = HssModuleController.hssControllerFactory()({});
-		cc.constructor({});
-		expect(Array.isArray(cc.columnHasContent)).toBe(true);
-	});
+    it('has a constructor', () => {
+        cc.constructor({});
+        expect(Array.isArray(cc.columnHasContent)).toBe(true);
+    });
 
-	// xdescribe('after initialized', () => {
-	// 	it('holds the information about the columns having content', () = > {
+    // xdescribe('after initialized', () => {
+    // 	it('holds the information about the columns having content', () = > {
 
-	// 	});
+    // 	});
 
-	// 	it('fill the array with data on an event', () => {
+    // 	it('fill the array with data on an event', () => {
 
-	// 	});
+    // 	});
 
-	// 	it('if gets the proper event, reports back via another event', () => {
+    // 	it('if gets the proper event, reports back via another event', () => {
 
-	// 	});
-	// });
+    // 	});
+    // });
 });
