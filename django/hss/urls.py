8555fc8ea3e59ec2c95001078841956a1217d890
Zoltan Ilcsik <ilcsik@gmail.com>
2016-04-06 14:26:36 +0200

    Slash added to dat a endpoint

--- django/hss/urls.py
+++ django/hss/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     url(r"^projects/(?P<project_id>\d+)/hss/constraints/$", view=views.ConstraintView.as_view(), name="hss-constraints"),
     url(r"^projects/(?P<project_id>\d+)/hss/interventions/$", view=views.InterventionView.as_view(), name="hss-interventions"),
     url(r"^projects/(?P<project_id>\d+)/hss/taxonomies/$", view=views.TaxonomyView.as_view(), name="hss-taxonomies"),
-    url(r"^projects/(?P<project_id>\d+)/hss/data$", view=views.hss_data, name="hss-data"),
+    url(r"^projects/(?P<project_id>\d+)/hss/data/$", view=views.hss_data, name="hss-data"),
     url(r"^projects/hss/structure/$", view=views.hss_structure, name="hss-structure"),
 ]
