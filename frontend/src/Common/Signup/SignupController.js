9e1ea4235d4a14516d5e7eb9bb9880cafb13eaba
lordfuoco <lordfuoco@gmail.com>
2016-04-13 16:14:37 +0200

    Registration: backend error system integrated

--- frontend/src/Common/Signup/SignupController.js
+++ frontend/src/Common/Signup/SignupController.js
@@ -1,14 +1,17 @@
+import _ from 'lodash';
 import SignupService from './SignupService';
 
 class SignupController {
 
-    constructor() {
+    constructor($scope) {
         this.ss = new SignupService();
+        this.scope = $scope;
         this.processRegistrationResult = this.processRegistrationResult.bind(this);
         this.register = {
-            // email: 'a',
-            // password1: 'a',
-            // password2: 'b'
+            email: 'c@c.com',
+            password1: '123456',
+            password2: '123456',
+            role: 1
         };
         this.style = {
             height: this.calculateHeight()
@@ -24,27 +27,36 @@ class SignupController {
         if (signupForm.$valid) {
             this.ss.signup(this.register)
             .then(result => {
-                this.processRegistrationResult(result);
+                this.processRegistrationResult(result, signupForm);
             });
         }
     }
 
-    processRegistrationResult(result) {
+    processRegistrationResult(result, signupForm) {
         if (result.success) {
             console.log('success', result.data);
         }
         else {
-            console.log('fail', result.data);
+            _.forEach(result.data, (item, key) => {
+                signupForm[key].customError = item;
+                signupForm[key].$setValidity('custom', false);
+            });
+            this.scope.$evalAsync();
         }
+    }
+
+    handleCustomError(signupForm, key) {
+        signupForm[key].$setValidity('custom', true);
+        signupForm[key].customError = [];
 
     }
 
     static signupFactory() {
         require('./Signup.scss');
-        function signup() {
-            return new SignupController();
+        function signup($scope) {
+            return new SignupController($scope);
         }
-        signup.$inject = [];
+        signup.$inject = ['$scope'];
         return signup;
     }
 }
