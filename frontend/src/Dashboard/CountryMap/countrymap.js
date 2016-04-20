5b53c7ec8e0058caf1d88ccb9db560aff0e6372e
lordfuoco <lordfuoco@gmail.com>
2016-04-20 15:27:56 +0200

    case fix..

---
+++ frontend/src/Dashboard/CountryMap/countrymap.js
@@ -0,0 +1,14 @@
+import _template from './Countrymap.html';
+import CountrymapController from './CountryMapController';
+
+const coutrymap = {
+    controller: CountrymapController.countrymapFactory(),
+    controllerAs: 'vm',
+    template: _template,
+    bindings: {
+        country: '=',
+        data: '='
+    }
+};
+
+export default coutrymap;
