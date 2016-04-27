8dc562af5bbeb9a9050c2220c0c8f847e20b0451
Zoltan Ilcsik <ilcsik@gmail.com>
2016-04-27 18:17:25 +0200

    Country list fixed

--- django/country/serializers.py
+++ django/country/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from .models import Country
 
 
-class CountryListSerializer(serializers.Serializer):
+class CountryListSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Country
