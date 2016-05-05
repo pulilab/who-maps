4d14ebc2c80c7ee5e01377b8d73eaf18d658b0ca
Andras Takacs <att@pulilab.com>
2016-05-05 12:00:42 +0200

    statistic cards modularized

---
+++ frontend/src/Common/Donors/donorsComponent.js
@@ -0,0 +1,13 @@
+import _template from './donors.html';
+import './donors.scss';
+
+const donorsComponent = {
+    template: _template,
+    controllerAs: 'vm',
+    name: 'donors',
+    bindings: {
+        number: '='
+    }
+};
+
+export default donorsComponent;
