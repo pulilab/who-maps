c0a5a4b7958712602b6b40a400a6e7d48f032def
lordfuoco <lordfuoco@gmail.com>
2016-05-06 15:27:39 +0200

    edit project vip2

--- frontend/src/Common/NewProject/newProjectController-spec.js
+++ frontend/src/Common/NewProject/newProjectController-spec.js
@@ -20,6 +20,9 @@ describe('NewProjectController', () => {
     beforeEach(() => {
         spyOn(NewProjectController.prototype, 'handleDistrictData').and.callThrough();
         sc = NewProjectController.newProjectFactory()($scope);
+        sc.newProjectForm = {
+            $valid: true
+        };
     });
 
     it('should have a function that import all the icons ', () => {
@@ -44,20 +47,45 @@ describe('NewProjectController', () => {
 
     it('should have a function that handles the country ssmenu callback', () => {
         spyOn(sc.ns, 'countryDistrict').and.returnValue(Promise.resolve());
+        spyOn(sc, 'handleCustomError');
         sc.handleStructureLoad(mockData);
         sc.countryCloseCallback('asd');
         expect(sc.project.countryName).toBe('asd');
         expect(sc.ns.countryDistrict).toHaveBeenCalled();
     });
 
-    it('should have a function that save the data', () => {
+    it('should have a function that handle the saving process ', () => {
         spyOn(sc, 'mergeCustomAndDefault');
         spyOn(sc, 'createCoverageArray');
+        spyOn(sc, 'saveForm');
         spyOn(sc.ns, 'newProject').and.returnValue(Promise.resolve());
         sc.save();
         expect(sc.mergeCustomAndDefault).toHaveBeenCalled();
         expect(sc.createCoverageArray).toHaveBeenCalled();
+        expect(sc.saveForm).toHaveBeenCalled();
+    });
+
+    it('should have a function that save a new form', () => {
+        spyOn(sc.ns, 'newProject').and.returnValue(Promise.resolve());
+        sc.saveForm(sc.project);
         expect(sc.ns.newProject).toHaveBeenCalled();
     });
+
+    it('should have a function that update an existing form', () => {
+        spyOn(sc.ns, 'updateProject').and.returnValue(Promise.resolve());
+        sc.editMode = true;
+        sc.updateForm(sc.project);
+        expect(sc.ns.updateProject).toHaveBeenCalled();
+    });
+
+    it('should have some utility function', () => {
+        expect(sc.concatCustom).toBeDefined();
+        expect(sc.unfoldObjects).toBeDefined();
+        expect(sc.createCoverageArray).toBeDefined();
+    });
+
+    it('should have a function that handle the data loaded from the server', () => {
+        spyOn(sc.ns, '')
+    })
 
 });
