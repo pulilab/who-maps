f4b485c2e6ffac1888ccc0691d55b8961b9b46f4
Zoltan Ilcsik <ilcsik@gmail.com>
2016-05-07 15:36:12 +0200

    Fixes unique constraint validation on project name

--- django/project/views.py
+++ django/project/views.py
@@ -16,7 +16,7 @@ from hss.hss_data import hss_default
 from toolkit.models import Toolkit, ToolkitVersion
 from toolkit.toolkit_data import toolkit_default
 from country.models import Country
-from .serializers import ProjectSerializer
+from .serializers import ProjectSerializer, ProjectModelSerializer
 from .models import Project, File, CoverageVersion, PartnerLogo
 from .project_data import project_structure
 
@@ -52,19 +52,20 @@ class ProjectViewSet(TokenAuthMixin, ViewSet):
         """
         Creates a project.
         """
-        serializer = ProjectSerializer(data=request.data)
-        if serializer.is_valid():
-            project = Project.objects.create(name=serializer.data["name"], data=serializer.data)
+        data_serializer = ProjectSerializer(data=request.data)
+        model_serializer = ProjectModelSerializer(data={"name": data_serializer.initial_data["name"]})
+        if model_serializer.is_valid() and data_serializer.is_valid():
+            project = Project.objects.create(name=data_serializer.data["name"], data=data_serializer.data)
             project.save()
             # Add default HSS structure for the new project.
             HSS.objects.create(project_id=project.id, data=hss_default)
             # Add default Toolkit structure for the new project.
             Toolkit.objects.create(project_id=project.id, data=toolkit_default)
-            data = dict(serializer.data)
+            data = dict(data_serializer.data)
             data.update(id=project.id)
             return Response(data, status=status.HTTP_201_CREATED)
         else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(model_serializer.errors or data_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def retrieve(self, request, *args, **kwargs):
         """
@@ -79,15 +80,16 @@ class ProjectViewSet(TokenAuthMixin, ViewSet):
         """
         Updates a project.
         """
-        serializer = ProjectSerializer(data=request.data)
-        if serializer.is_valid():
-            project = get_object_or_400(Project, "No such project", id=kwargs["pk"])
-            project.data = serializer.data
+        data_serializer = ProjectSerializer(data=request.data)
+        project = get_object_or_400(Project, "No such project", id=kwargs["pk"])
+        model_serializer = ProjectModelSerializer(instance=project, data={"name": data_serializer.initial_data["name"]})
+        if model_serializer.is_valid() and data_serializer.is_valid():
+            project.name = data_serializer.validated_data["name"]
+            project.data = data_serializer.validated_data
             project.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(data_serializer.data, status=status.HTTP_200_OK)
         else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+            return Response(model_serializer.errors or data_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'POST'])
 @authentication_classes((TokenAuthentication,))
