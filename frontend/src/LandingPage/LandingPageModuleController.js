22c0f1d87ee8a8144a5ae1ebba675e51f226dd6a
lordfuoco <lordfuoco@gmail.com>
2016-05-10 19:01:30 +0200

    Landing page variatons and logic

--- frontend/src/LandingPage/LandingPageModuleController.js
+++ frontend/src/LandingPage/LandingPageModuleController.js
@@ -1,15 +1,23 @@
 import { trendingProjects } from './landingMock';
+import { Storage } from '../Common/'
 
 class LandingPageModuleController {
 
     constructor($scope, $location, $anchorScroll) {
+        this.storage = new Storage();
         this.trendingProjects = trendingProjects;
         this.$location = $location;
         this.$anchorScroll = $anchorScroll;
+        this.isLogin = this.storage.get('login');
+
+        if (this.isLogin) {
+            const commonService = require('../Common/CommonServices');
+            this.projectList = commonService.default.projectList;
+        }
+
     }
 
     scrollTo(idString) {
-
         this.$location.hash(idString);
         this.$anchorScroll();
     }
