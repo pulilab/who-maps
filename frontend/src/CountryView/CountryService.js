6adfb1eb00f906c3985d7451bb4eb07e25571b2c
Andras Takacs <att@pulilab.com>
2016-05-10 13:42:45 +0200

    country level view now handles list view with real data

---
+++ frontend/src/CountryView/CountryService.js
@@ -0,0 +1,28 @@
+import { AuthApi } from '../Common/';
+import _ from 'lodash';
+
+class CountryService extends AuthApi {
+
+    constructor() {
+        super('projects');
+    }
+
+    getProjects(queryObj) {
+
+        const queryArr = [];
+
+        _.forOwn(queryObj, (val, key) => {
+            queryArr.push(key + '=' + val);
+        });
+
+        const queryString = '?' + queryArr.join('&');
+
+        return this.get(queryString)
+            .then(data => {
+                // console.debug('PROJECTS data:', data);
+                return data;
+            });
+    }
+}
+
+export default CountryService;
--- frontend/src/CountryView/CountryViewModuleController.js
+++ frontend/src/CountryView/CountryViewModuleController.js
@@ -1,9 +1,48 @@
+import CountryMapService from './CountryMapService.js';
+import CountryService from './CountryService.js';
+
 class CountryViewModuleController {
 
-    static printAndReturnSome() {
-        console.log('some');
-        return 'some';
+    constructor() {
+
+        this.mapService = new CountryMapService();
+        this.service = new CountryService();
+        this.getCountries();
     }
+
+    getCountries() {
+
+        this.mapService.getCountries().then(data => {
+
+            // console.debug('COUNTRIES:', data);
+            this.countries = data;
+            this.countriesLib = {};
+            data.forEach(country => {
+                this.countriesLib[country.id] = country.name;
+            });
+            // console.debug('COUNTRY LIB', this.countriesLib);
+        });
+    }
+
+    getProjects(countryObj) {
+        // console.debug('Selected:', countryObj);
+        this.service.getProjects({ country: countryObj.id }).then(data => {
+            // console.debug('PROJECTS in ' + countryObj.name, data);
+            this.projectsData = data;
+        });
+    }
+
+    static countryControllerFactory() {
+        function countryController() {
+
+            return new CountryViewModuleController();
+        }
+
+        countryController.$inject = [];
+
+        return countryController;
+    }
+
 }
 
 export default CountryViewModuleController;
