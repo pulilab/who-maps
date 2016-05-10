1e00c44578e64c73de5213a912d9c5edd812df4c
Andras Takacs <att@pulilab.com>
2016-05-10 20:22:33 +0200

    fixed org to organisation binding

--- frontend/src/Dashboard/CountryMap/mock/clvMock.js
+++ frontend/src/Dashboard/CountryMap/mock/clvMock.js
@@ -3,76 +3,76 @@ const clvMock = {
         {
             id: 1,
             name: 'Helen Keller International',
-            org: 'Routine nutrition monitor'
+            organisation: 'Routine nutrition monitor'
         }, {
             id: 2,
             name: 'eHealth Africa',
-            org: 'SENSE contract tracing'
+            organisation: 'SENSE contract tracing'
         }, {
             id: 3,
             name: 'GOAL International',
-            org: 'SENSE contract tracing'
+            organisation: 'SENSE contract tracing'
         },
         {
             id: 1,
             name: 'Helen Keller International',
-            org: 'Routine nutrition monitor'
+            organisation: 'Routine nutrition monitor'
         }, {
             id: 2,
             name: 'eHealth Africa',
-            org: 'SENSE contract tracing'
+            organisation: 'SENSE contract tracing'
         }, {
             id: 3,
             name: 'GOAL International',
-            org: 'SENSE contract tracing'
+            organisation: 'SENSE contract tracing'
         },
         {
             id: 1,
             name: 'Helen Keller International',
-            org: 'Routine nutrition monitor'
+            organisation: 'Routine nutrition monitor'
         }, {
             id: 2,
             name: 'eHealth Africa',
-            org: 'SENSE contract tracing'
+            organisation: 'SENSE contract tracing'
         }, {
             id: 3,
             name: 'GOAL International',
-            org: 'SENSE contract tracing'
+            organisation: 'SENSE contract tracing'
         },
         {
             id: 1,
             name: 'Helen Keller International',
-            org: 'Routine nutrition monitor'
+            organisation: 'Routine nutrition monitor'
         }, {
             id: 2,
             name: 'eHealth Africa',
-            org: 'SENSE contract tracing'
+            organisation: 'SENSE contract tracing'
         }, {
             id: 3,
             name: 'GOAL International',
-            org: 'SENSE contract tracing'
+            organisation: 'SENSE contract tracing'
         }
     ],
     'Bonthe District': [
         {
             id: 1,
             name: 'Project 1',
-            org: 'someOrg'
+            organisation: 'someorganisation'
         }, {
             id: 2,
             name: 'Project 1 -bon',
-            org: 'someOrg'
+            organisation: 'someorganisation'
         }
     ],
     'Moyamba District': [
         {
             id: 1,
             name: 'Project 1',
-            org: 'someOrg'
+            organisation: 'someorganisation'
         }, {
             id: 2,
             name: 'Project 3 -mom',
-            org: 'someOrg2'
+            organisation: 'someOrg2'
         }
     ]
 };
