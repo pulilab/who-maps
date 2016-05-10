12d7a41bd177a80f46186d34dffb641d711927cd
Fekete Gyorgy <f@pulilab.com>
2016-05-10 11:26:25 +0200

    controller for searchbar

--- frontend/src/Common/Searchbar/SearchbarController.js
+++ frontend/src/Common/Searchbar/SearchbarController.js
@@ -1,29 +1,94 @@
-import { trendingProjects } from '../../LandingPage/landingMock';
+import _ from 'lodash';
+import SearchbarService from './SearchbarService';
 
 class SearchbarController {
 
-    constructor() {
+    constructor($state, $scope) {
         const vm = this;
-        vm.filters = {
-            'Location': false,
-            'Project name': true,
-            'Health topic': false,
-            'Technology platform': false,
-            'Organization': false
-        };
-        vm.projects = trendingProjects;
-        vm.resultNr = 12;
+        vm.EE = window.EE;
+        vm.scope = $scope;
+        vm.state = $state;
+        vm.ss = new SearchbarService();
+        vm.$onInit = vm.initialisation.bind(vm);
+        vm.filters = [
+            {
+                name: 'location',
+                displayName: 'Location',
+                value: false
+            },
+            {
+                name: 'project_name',
+                displayName: 'Project name',
+                value: true
+            },
+            {
+                name: 'health_topic',
+                displayName: 'Health topic',
+                value: false
+            },
+            {
+                name: 'technology_platform',
+                displayName: 'Technology platform',
+                value: false
+            },
+            {
+                name: 'organisation',
+                displayName: 'Organization',
+                value: false
+            }
+        ];
+        vm.searchStr = '';
+
+        vm.scope.$watch(() => {
+            return vm.searchStr;
+        }, tmpStr => {
+            vm.search(tmpStr);
+        });
+    }
+
+    initialisation() {
+        this.showSearch = false
+    }
+
+    toggleSearch() {
+        this.showSearch = !this.showSearch;
+    }
+
+    search(tmpStr) {
+        const vm = this;
+        if (!tmpStr || tmpStr.length === 0) {
+            return false;
+        }
+        if (tmpStr === vm.searchStr) {
+            if (_.some(vm.filters, {value: true})) {
+                this.ss.searchProject(vm.searchStr, vm.filters).then(results => {
+                    vm.projects = results;
+                    vm.resultNr = _.min([results.length, 5]);
+                    vm.totalNr = results.length;
+                    vm.scope.$evalAsync();
+                });
+            }
+        }
+        return true;
+    }
+
+    close() {
+        this.showSearch = false;
+        this.searchStr = '';
+        this.projects = void 0;
+        this.totalNr = 0;
+        this.resultNr = 0;
     }
 
     static searchbarFactory() {
         require('./Searchbar.scss');
-        function searchbarCp() {
-            return new SearchbarController();
+        function searchController($state, $scope) {
+            return new SearchbarController($state, $scope);
         }
 
-        searchbarCp.$inject = [];
+        searchController.$inject = ['$state', '$scope'];
 
-        return searchbarCp;
+        return searchController;
     }
 
 }
