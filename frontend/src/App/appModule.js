4de804457322cc3789a1ec5cce435bac45627478
lordfuoco <lordfuoco@gmail.com>
2016-05-11 10:02:21 +0200

    fix2

--- frontend/src/App/appModule.js
+++ frontend/src/App/appModule.js
@@ -48,7 +48,7 @@ const config = ($stateProvider, $urlRouterProvider) => {
                             def.resolve();
                         });
 
-                    return def;
+                    return def.promise;
                 }]
             }
 
