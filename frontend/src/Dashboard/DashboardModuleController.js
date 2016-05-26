ed4b061640c73a5afccc752f89f6dbcee63bb1e6
Andras Takacs <att@pulilab.com>
2016-05-26 11:42:55 +0200

    dash - improve project / lessons&tips data mocked

--- frontend/src/Dashboard/DashboardModuleController.js
+++ frontend/src/Dashboard/DashboardModuleController.js
@@ -40,6 +40,27 @@ class DashboardModuleController extends Protected {
         this.commProjects = commProjects;
         this.resizeEvent();
         this.eventBinding();
+
+        this.pgArray = [
+            {
+                title: 'Use the existing evidence base to bolster interventions',
+                description: 'Project teams should remember that mHealth is a catalytic tool and not often a health Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.',
+                commentNr: 17,
+                imageURL: 'someURL'
+            },
+            {
+                title: 'Conduct formative work to understand your context',
+                description: 'Formative research is critical for local validation and contextualization of mHealth Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod',
+                commentNr: 6,
+                imageURL: 'someURL'
+            },
+            {
+                title: 'Lorem ipsum dolor sit amet, consectetur adipisici elit',
+                description: 'A short description!',
+                commentNr: 0,
+                imageURL: 'someURL'
+            }
+        ];
     }
 
     onDestroy() {
