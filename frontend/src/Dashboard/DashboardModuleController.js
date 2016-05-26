f1865435efb89e7f0ea40921e528d099adf0a972
Andras Takacs <att@pulilab.com>
2016-05-26 11:23:29 +0200

    dash - improve project resource sitebuilt

--- frontend/src/Dashboard/DashboardModuleController.js
+++ frontend/src/Dashboard/DashboardModuleController.js
@@ -447,6 +447,7 @@ class DashboardModuleController extends Protected {
         if (data.every(dom => dom.completion === 0)) {
 
             return Math.floor(Math.random() * 17);
+            // INVESTIGATE 16 problem
         }
         return data.filter(dom => dom.completion > 0)
             .reduce((res, act) => act.percentage < res.percentage ? act : res, { percentage: 200 }).id - 1;
