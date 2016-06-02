589c0d28750c61e5752fad6ccea2a69448ab5102
Fekete Gyorgy <f@pulilab.com>
2016-06-02 17:28:21 +0200

    permission tests

--- django/project/tests.py
+++ django/project/tests.py
@@ -529,3 +529,118 @@ class ProjectTests(APITestCase):
         self.assertTrue("viewers" in response.json())
         self.assertTrue(owner_id not in response.json()['team'])
         self.assertEqual(response.json()['team'], [user_profile_id])
+
+
+class PermissionTests(ProjectTests):
+
+    def test_team_member_can_update_project_groups(self):
+        url = reverse("project-groups", kwargs={"pk": self.project_id})
+
+        user_profile_id = UserProfile.objects.first().id
+        groups = {
+            "team": [user_profile_id],
+            "viewers": [user_profile_id]
+        }
+        response = self.test_user_client.put(url, groups)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()['team'], [user_profile_id])
+        self.assertEqual(response.json()['viewers'], [user_profile_id])
+
+    def test_team_viewer_cannot_update_project_groups(self):
+        url = reverse("project-groups", kwargs={"pk": self.project_id})
+        response = self.test_user_client.get(url)
+        self.assertEqual(response.status_code, 200)
+        owner_id = response.json()['team'][0]['id']
+
+        # Create a test user with profile.
+        url = reverse("rest_register")
+        data = {
+            "email": "test_user2@gmail.com",
+            "password1": "123456",
+            "password2": "123456"}
+        response = self.client.post(url, data)
+
+        # Log in the user.
+        url = reverse("api_token_auth")
+        data = {
+            "username": "test_user2@gmail.com",
+            "password": "123456"}
+        response = self.client.post(url, data)
+        test_user_key = response.json().get("token")
+        test_user_client = APIClient(HTTP_AUTHORIZATION="Token {}".format(test_user_key), format="json")
+
+        # Create profile.
+        org = Organisation.objects.create(name="org2")
+        url = reverse("userprofile-list")
+        data = {
+            "name": "Test Name 2",
+            "organisation": org.id,
+            "country": "test_country"}
+        response = test_user_client.post(url, data)
+
+        user_profile_id = response.json()['id']
+
+        url = reverse("project-groups", kwargs={"pk": self.project_id})
+
+        groups = {
+            "team": [owner_id],
+            "viewers": [user_profile_id]
+        }
+        response = self.test_user_client.put(url, groups)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()['team'], [owner_id])
+        self.assertEqual(response.json()['viewers'], [user_profile_id])
+
+        # try to update it with the viewer
+        response = test_user_client.put(url, groups)
+        self.assertEqual(response.status_code, 403)
+        self.assertEqual(response.json()['detail'], 'You do not have permission to perform this action.')
+
+    def test_authenticated_users_can_list_project_groups(self):
+        # Create a test user with profile.
+        url = reverse("rest_register")
+        data = {
+            "email": "test_user2@gmail.com",
+            "password1": "123456",
+            "password2": "123456"}
+        response = self.client.post(url, data)
+
+        # Log in the user.
+        url = reverse("api_token_auth")
+        data = {
+            "username": "test_user2@gmail.com",
+            "password": "123456"}
+        response = self.client.post(url, data)
+        test_user_key = response.json().get("token")
+        test_user_client = APIClient(HTTP_AUTHORIZATION="Token {}".format(test_user_key), format="json")
+
+        # Create profile.
+        org = Organisation.objects.create(name="org2")
+        url = reverse("userprofile-list")
+        data = {
+            "name": "Test Name 2",
+            "organisation": org.id,
+            "country": "test_country"}
+        response = test_user_client.post(url, data)
+        user_profile_id = response.json()['id']
+
+        url = reverse("project-groups", kwargs={"pk": self.project_id})
+
+        response = self.test_user_client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue("team" in response.json())
+        self.assertTrue("viewers" in response.json())
+        self.assertTrue(user_profile_id not in response.json()['team'])
+        self.assertTrue(user_profile_id not in response.json()['viewers'])
+
+    def test_not_authenticated_cannot_list_project_groups(self):
+        test_user_client = APIClient(format="json")
+
+        url = reverse("project-groups", kwargs={"pk": self.project_id})
+
+        response = test_user_client.get(url)
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.json()['detail'], 'Authentication credentials were not provided.')
