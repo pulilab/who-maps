6702c71cacccf2c6dda2215636c8545899ced56a
Fekete Gyorgy <f@pulilab.com>
2016-06-06 16:25:50 +0200

    get_permission for retrieve and filter project data

--- django/project/urls.py
+++ django/project/urls.py
@@ -5,11 +5,11 @@ from . import views
 
 urlpatterns = [
     url(r"^projects/$", view=views.ProjectCRUDViewSet.as_view({'get': 'retrieve', 'post': 'create', 'put': 'update'}), name="project-crud"),
+    url(r"^projects/(?P<pk>\d+)/$", view=views.ProjectCRUDViewSet.as_view({'get': 'retrieve', 'put': 'update'}), name="project-detail"),
     url(r"^projects/member-of/$", view=views.ProjectListViewSet.as_view({'get': 'list'}), name="project-list"),
     url(r"^projects/by-view/map/(?P<country_id>\d+)/$", view=views.ProjectPublicViewSet.as_view({'get': 'by_district'}), name="project-by-district"),
     url(r"^projects/by-view/list/(?P<country_id>\d+)/$", view=views.ProjectPublicViewSet.as_view({'get': 'list_all'}), name="project-country-list"),
     url(r"^projects/by-view/list/$", view=views.ProjectPublicViewSet.as_view({'get': 'list_all'}), name="project-all-list"),
-    url(r"^projects/(?P<pk>\d+)/$", view=views.ProjectCRUDViewSet.as_view({'get': 'retrieve', 'put': 'update'}), name="project-detail"),
     url(r"^projects/structure/$", view=views.get_project_structure, name="get-project-structure"),
     url(r"^projects/(?P<project_id>\d+)/file-list/$", view=views.FileListViewSet.as_view({'get': 'list'}), name="file-list"),
     url(r"^projects/(?P<project_id>\d+)/files/$", view=views.FilePostViewSet.as_view({'post': 'create'}), name="project-files"),
