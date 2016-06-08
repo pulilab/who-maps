5f32e5ab8d351d402161e4920cd24b9e108cc519
Zoltan Ilcsik <ilcsik@gmail.com>
2016-06-08 16:18:19 +0200

    HSS refactor for new version

--- django/hss/urls.py
+++ django/hss/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r"^projects/(?P<project_id>\d+)/hss/continuum/$", view=views.ContinuumView.as_view({"post": "create"}), name="hss-continuum"),
     url(r"^projects/(?P<project_id>\d+)/hss/constraints/$", view=views.ConstraintView.as_view({"post": "create"}), name="hss-constraints"),
     url(r"^projects/(?P<project_id>\d+)/hss/interventions/$", view=views.InterventionView.as_view({"post": "create"}), name="hss-interventions"),
+    url(r"^projects/(?P<project_id>\d+)/hss/ageranges/$", view=views.AgeRangeView.as_view({"post": "create"}), name="hss-ageranges"),
     url(r"^projects/(?P<project_id>\d+)/hss/taxonomies/$", view=views.TaxonomyView.as_view({"post": "create"}), name="hss-taxonomies"),
     url(r"^projects/(?P<project_id>\d+)/hss/data/$", view=views.hss_data, name="hss-data"),
     url(r"^projects/hss/structure/$", view=views.hss_structure, name="hss-structure"),
