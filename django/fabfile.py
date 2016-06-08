de54f2b347ea0861af59bb9685e3b0376c467f8a
Zoltan Ilcsik <ilcsik@gmail.com>
2016-06-08 18:26:34 +0200

    Fabfile multiple compose files - failing CI build

--- django/fabfile.py
+++ django/fabfile.py
@@ -35,7 +35,14 @@ def deploy():
         run('git checkout %s' % env.branch)
         run('git pull origin %s' % env.branch)
         time.sleep(20)
-        run('docker-compose restart')
+
+        if env.name == 'dev':
+            run('docker-compose -f docker-compose.yml -f docker-compose.dev.yml restart')
+        elif env.name == 'staging':
+            run('docker-compose -f docker-compose.yml -f docker-compose.test.yml restart')
+        else:
+            run('docker-compose restart')
+
         time.sleep(5)
 
         # handle backend
@@ -44,9 +51,15 @@ def deploy():
             # backup DB
             _backup_db()
             # build
-            run('docker-compose build')
-            run('docker-compose down')
-            run('docker-compose up -d')
+            if env.name == 'dev':
+                options = "-f ../docker-compose.yml -f ../docker-compose.dev.yml "
+            elif env.name == 'staging':
+                options = "-f ../docker-compose.yml -f ../docker-compose.test.yml "
+            else:
+                options = ""
+            run('docker-compose {}build'.format(options))
+            run('docker-compose {}down'.format(options))
+            run("docker-compose {}up -d".format(options))
 
             # drop & create DB
             time.sleep(10)
