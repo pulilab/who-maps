2a4372641055caee4d4925cc1c4bdd7db9ead148
lordfuoco <lordfuoco@gmail.com>
2016-06-12 13:43:40 +0200

    Badly fixes: #214, needs refactor in v2

--- frontend/src/LandingPage/LandingPageController.js
+++ frontend/src/LandingPage/LandingPageController.js
@@ -1,10 +1,14 @@
+import _ from 'lodash';
 import { trendingProjects } from './landingMock';
 import { Storage } from '../Common/';
+import SignupService from '../Common/Signup/SignupService';
 
 class LandingPageModuleController {
 
     constructor($scope, $location, $anchorScroll) {
         this.storage = new Storage();
+        this.ss = new SignupService();
+        this.EE = window.EE;
         this.trendingProjects = trendingProjects;
         this.$location = $location;
         this.$anchorScroll = $anchorScroll;
@@ -28,6 +32,38 @@ class LandingPageModuleController {
     scrollTo(idString) {
         this.$location.hash(idString);
         this.$anchorScroll();
+    }
+
+    signup(signupForm) {
+        if (signupForm.$valid) {
+            this.inProgress = true;
+            this.ss.signup(this.register)
+                .then(result => {
+                    if (result) {
+                        this.processRegistrationResult(result, signupForm);
+                    }
+                });
+        }
+    }
+
+    processRegistrationResult(result, signupForm) {
+        this.inProgress = false;
+        if (result.success) {
+            this.registered = true;
+            const user = {
+                username: this.register.email
+            };
+            this.storage.set('token', result.data.key);
+            this.storage.set('user_profile_id', result.data.user_profile_id);
+            this.storage.set('user', user);
+            this.EE.emit('login');
+        }
+        else {
+            _.forEach(result.data, (item, key) => {
+                signupForm[key].customError = item;
+                signupForm[key].$setValidity('custom', false);
+            });
+        }
     }
 
     static landingControllerFactory() {
