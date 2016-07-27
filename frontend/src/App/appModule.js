50c9405c2da76d52358f415fc90ef463f851426e
Fekete Gyorgy <f@pulilab.com>
2016-07-27 15:41:01 +0200

    fix scrolltotop

--- frontend/src/App/appModule.js
+++ frontend/src/App/appModule.js
@@ -184,17 +184,18 @@ const config = ($stateProvider, $urlRouterProvider, $locationProvider) => {
 };
 
 function handleStateChange(...args) {
-    console.debug(`Ui route state change ${this} :`, args);
+    if (DEBUG) {
+        console.debug(`Ui route state change ${this} :`, args);
+    }
     if (this === 'success') {
         document.body.scrollTop = document.documentElement.scrollTop = 0;
     }
 }
 
 const run = ($rootScope, $state) => {
-    if (DEBUG) {
-        $rootScope.$on('$stateChangeError', handleStateChange.bind('error'));
-        $rootScope.$on('$stateChangeSuccess', handleStateChange.bind('success'));
-    }
+    $rootScope.$on('$stateChangeError', handleStateChange.bind('error'));
+    $rootScope.$on('$stateChangeSuccess', handleStateChange.bind('success'));
+
 
     const checkXHR = (event) => {
         $rootScope.progress = event.detail.progression;
