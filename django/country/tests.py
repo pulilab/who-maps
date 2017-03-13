721b0de1065ff435a7c8faf551ae2285a0148262
Gyorgy Fekete <f@pulilab.com>
2017-03-13 16:46:46 +0100

    remove api endpoints for geodata and tests

--- django/country/tests.py
+++ django/country/tests.py
@@ -34,68 +34,16 @@ class CountryTests(APITestCase):
         self.test_user_key = response.json().get("token")
         self.test_user_client = APIClient(HTTP_AUTHORIZATION="Token {}".format(self.test_user_key))
 
-        geodata = {
-            "admin_level_5": {
-                "objects": {
-                    "admin_level_5": {
-                        "geometries": [
-                            {
-                                "properties": {
-                                    "name": "Some District",
-                                    "admin_level": "5"
-                                }
-                            }
-                        ]
-                    }
-                }
-            }
-        }
-
-        geodata2 = {
-            "admin_level_4": {
-                "objects": {
-                    "admin_level_4": {
-                        "geometries": [
-                            {
-                                "properties": {
-                                    "name": "Some District",
-                                    "admin_level": "4"
-                                }
-                            }
-                        ]
-                    }
-                }
-            }
-        }
-
-        country, _ = Country.objects.get_or_create(name="sierra-leone", geodata=geodata)
+        country, _ = Country.objects.get_or_create(name="sierra-leone")
         country.save()
         self.country_id = country.id
 
-        country, _ = Country.objects.get_or_create(name="kenya", geodata=geodata2)
+        country, _ = Country.objects.get_or_create(name="kenya")
         country.save()
         self.country_id2 = country.id
-
-    def test_get_geodata(self):
-        url = reverse("get-geodata", kwargs={"country_id": self.country_id})
-        response = self.test_user_client.get(url)
-        self.assertEqual(response.status_code, 200)
-        self.assertIn("admin_level_5", response.json().keys())
 
     def test_get_countries(self):
         url = reverse("country-list")
         response = self.test_user_client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertIn("name", response.json()[0].keys())
-
-    def test_get_districts_lvl5(self):
-        url = reverse("get-districts", kwargs={"country_id": self.country_id})
-        response = self.test_user_client.get(url)
-        self.assertEqual(response.status_code, 200)
-        self.assertIn("Some District", response.json())
-
-    def test_get_districts_lvl4(self):
-        url = reverse("get-districts", kwargs={"country_id": self.country_id2})
-        response = self.test_user_client.get(url)
-        self.assertEqual(response.status_code, 200)
-        self.assertIn("Some District", response.json())
--- django/country/views.py
+++ django/country/views.py
@@ -1,57 +1,6 @@
-from django.conf import settings
-
 from rest_framework import generics
-from rest_framework.response import Response
-from rest_framework.authentication import TokenAuthentication
-from rest_framework.decorators import api_view, authentication_classes
-from rest_framework.decorators import permission_classes
-from rest_framework.permissions import IsAuthenticated
-
-from core.views import TokenAuthMixin, get_object_or_400
 from .models import Country
 from .serializers import CountryListSerializer
-
-
-@api_view(['GET'])
-def get_geodata(request, country_id):
-    """
-    Retrieves geodata based on country_id.
-
-    Args:
-        country_id: ID for the given country.
-
-    Returns:
-        json: geodata for the given country in JSON.
-    """
-    country = get_object_or_400(Country, "No such country.", id=country_id)
-    return Response(country.geodata)
-
-
-@api_view(['GET'])
-def get_districts(request, country_id):
-    """
-    Retrieves districts based on country_id.
-
-    Args:
-        country_id: ID for the given country.
-
-    Returns:
-        json: districts for the given country in JSON.
-    """
-    country = get_object_or_400(Country, "No such country.", id=country_id)
-    admin_level_name = settings.LEVELS_FOR_DISTRICTS.get(country.name, "")
-    admin_level = country.geodata.get(admin_level_name, None)
-    if admin_level:
-        districts = []
-        for item in admin_level["objects"][admin_level_name]["geometries"]:
-            if "properties" in item.keys():
-                if "admin_level" in item["properties"].keys():
-                    name = item["properties"].get("name:en", None) or item["properties"].get("name")
-                    districts.append(name)
-        return Response(set(districts))
-    else:
-        # No admin_level for the given country.
-        return Response(list())
 
 
 class CountryListAPIView(generics.ListAPIView):
