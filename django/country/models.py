e3ab5de766381f50890b25a1829f6608140dd099
Gyorgy Fekete <f@pulilab.com>
2017-03-13 16:29:34 +0100

    remove geodata field from country

--- django/country/models.py
+++ django/country/models.py
@@ -7,7 +7,6 @@ from core.models import NameByIDMixin, ExtendedModel
 class Country(NameByIDMixin, ExtendedModel):
     name = models.CharField(max_length=255, unique=True)
     code = models.CharField(max_length=4, default="NULL", help_text="ISO3166-1 country code")
-    geodata = JSONField(default=dict)
 
     def __str__(self):
         return self.name
