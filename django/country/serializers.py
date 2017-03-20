4b97044a5c9ae6ae37fa2970585ce2124818d838
Gyorgy Fekete <f@pulilab.com>
2017-03-20 10:58:51 +0100

    Landing page serializer

--- django/country/serializers.py
+++ django/country/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Country
+from .models import Country, PartnerLogo
 
 
 class CountryListSerializer(serializers.ModelSerializer):
@@ -8,3 +8,15 @@ class CountryListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Country
         fields = ("id", "name", "code")
+
+
+class LandingPageSerializer(serializers.ModelSerializer):
+    partner_logos = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Country
+        fields = ("id", "name", "code", "logo", "cover", "cover_text", "footer_title", "footer_text", "partner_logos")
+
+    @staticmethod
+    def get_partner_logos(obj):
+        return [p.image_url for p in PartnerLogo.objects.filter(country=obj)]
