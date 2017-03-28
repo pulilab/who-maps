df3ffbd14808153c1dcaac943f7c830629f806c0
Gyorgy Fekete <f@pulilab.com>
2017-03-28 16:08:55 +0200

    search test coverage

--- django/search/tests.py
+++ django/search/tests.py
@@ -56,7 +56,7 @@ class ProjectTests(APITestCase):
             "organisation": self.org.id,
             "contact_name": "name1",
             "contact_email": "a@a.com",
-            "implementation_overview": "phrase2",
+            "implementation_overview": "overview",
             "implementation_dates": "2016",
             "health_focus_areas": ["area1", "area2"],
             "geographic_scope": "somewhere",
@@ -144,3 +144,27 @@ class ProjectTests(APITestCase):
         response = self.test_user_client.post(url, data, format="json")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.json()), 0)
+
+    def test_search_not_in_main_categories(self):
+        url = reverse("search-project")
+        data = {
+            "query": "overview"
+        }
+        response = self.test_user_client.post(url, data, format="json")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json()), 2)
+
+    def test_search_not_in_main_categories_wiki(self):
+        url = reverse("search-project")
+        data = {
+            "query": "wiki.org"
+        }
+        response = self.test_user_client.post(url, data, format="json")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json()), 2)
+
+    def test_search_no_query(self):
+        url = reverse("search-project")
+        response = self.test_user_client.post(url, {}, format="json")
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json()['query'][0], 'This field is required.')
