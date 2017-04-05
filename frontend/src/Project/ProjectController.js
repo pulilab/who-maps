9f1fe9c757d5b2e0a5527557d0640ec039d218ae
lordfuoco <lordfuoco@gmail.com>
2017-04-05 17:50:56 +0200

    added more toast messages around the app

--- frontend/src/Project/ProjectController.js
+++ frontend/src/Project/ProjectController.js
@@ -241,6 +241,7 @@ class ProjectController extends ProjectDefinition {
 
 
     focusInvalidField() {
+        this.showToast('Validation error');
         this.timeout(()=>{
             const firstInvalid = document.getElementById('npf').querySelector('.ng-invalid');
             if (firstInvalid) {
@@ -259,7 +260,7 @@ class ProjectController extends ProjectDefinition {
                 if (response && response.success) {
                     // update cached project data with the one from the backend
                     this.cs.updateProject(response.data, this.projectId);
-                    this.confirmationToast();
+                    this.showToast('Project Updated!');
                     this.postUpdateActions();
                 }
                 else {
@@ -276,7 +277,7 @@ class ProjectController extends ProjectDefinition {
                     this.cs.addProjectToCache(response.data);
                     this.putGroups().then(() => {
                         this.postSaveActions();
-                        this.confirmationToast();
+                        this.showToast('Project Saved!');
                     });
                 }
                 else {
@@ -364,10 +365,10 @@ class ProjectController extends ProjectDefinition {
         }
     }
 
-    confirmationToast() {
+    showToast(text) {
         this.toast.show(
             this.toast.simple()
-                .textContent('Project Saved!')
+                .textContent(text)
                 .position('bottom right')
                 .hideDelay(3000)
         );
