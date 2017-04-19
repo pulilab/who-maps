78523d2ea25b1f2a23d8c4001f00f04285d12a72
lordfuoco <lordfuoco@gmail.com>
2017-04-20 00:12:01 +0200

    Experience list: enabled adding of new experiences

--- frontend/src/Cms/ExperiencesList/ExperiencesListController.js
+++ frontend/src/Cms/ExperiencesList/ExperiencesListController.js
@@ -1,30 +1,58 @@
 class ExperienceListController {
 
-    constructor() {
+    constructor($scope) {
+        this.scope = $scope;
         this.$onInit = this.onInit.bind(this);
     }
 
     onInit() {
-        const mockData = require('../resources/mockData');
+        this.cs = require('../CmsService');
+        this.commonServices = require('../../Common/CommonServices');
         this.domains = require('../resources/domains');
         const axisId = parseInt(this.axisId, 10);
         const domainId = parseInt(this.domainId, 10);
-        const domain = this.domains[axisId].domains[domainId];
-        this.experiences = mockData.experiences.filter(exp => {
-            return exp.domain === domain;
+        this.domain = this.domains[axisId].domains[domainId];
+        this.data = [];
+        this.cs.getData().then(data => {
+            this.data = data;
         });
+
+
         this.newExperience = {
-            value: null,
-            valid: false
+            body: null,
+            valid: false,
+            name: this.commonServices.userProfile.name,
+            domain: this.domain.id,
+            type: 3
         };
+
+        this.watchers();
+    }
+
+    watchers() {
+        this.scope.$watchCollection(() => {
+            return this.data;
+        }, data => {
+            this.experiences = data.filter(exp => {
+                return exp.domain === this.domain.id && exp.type === 3;
+            });
+        });
+    }
+
+    saveExperience() {
+        this.cs.addContent(this.newExperience).then(() => {
+            this.scope.$evalAsync(() => {
+                this.newExperience.body = false;
+            });
+        });
     }
 
     static factory() {
         require('./ExperiencesList.scss');
-        function experienceListController() {
-            return new ExperienceListController();
+        function experienceListController($scope) {
+            return new ExperienceListController($scope);
         }
-        experienceListController().$inject = [];
+        experienceListController().$inject = ['$scope'];
         return experienceListController;
     }
 }
