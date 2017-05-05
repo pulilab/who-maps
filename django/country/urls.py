96f536a5171840c1a7156dc9ae4a6d245c91d4e6
Gyorgy Fekete <f@pulilab.com>
2017-05-05 13:03:17 +0200

    country field urls

--- django/country/urls.py
+++ django/country/urls.py
@@ -10,4 +10,7 @@ urlpatterns = router.urls
 
 urlpatterns += [
     url(r"^countries/$", view=views.CountryListAPIView.as_view(), name="country-list"),
+    url(r"^country-fields/(?P<country_id>\d+)/$", view=views.CountryFieldsListView.as_view(), name="country-fields-list"),
+    url(r"^country-fields/(?P<country_id>\d+)/(?P<project_id>\d+)/$", view=views.CountryFieldsUpdateView.as_view(), name="country-fields-update"),
+    url(r"^country-fields/$", view=views.CountryFieldsCreateView.as_view(), name="country-fields-create"),
 ]
