18c0d1fcf0dbe9e1d93ba55f17b089ab2a36c36d
Gyorgy Fekete <f@pulilab.com>
2017-05-05 13:00:49 +0200

    country field serializers

--- django/country/serializers.py
+++ django/country/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
-from .models import Country, PartnerLogo
+from .models import Country, PartnerLogo, CountryField
 
 
 class CountryListSerializer(serializers.ModelSerializer):
@@ -20,3 +21,58 @@ class LandingPageSerializer(serializers.ModelSerializer):
     @staticmethod
     def get_partner_logos(obj):
         return [p.image_url for p in PartnerLogo.objects.filter(country=obj)]
+
+
+class CountryFieldsListSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = CountryField
+        fields = ("country", "type", "question")
+
+
+class CountryFieldsSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = CountryField
+        fields = ("country", "type", "question", "answer", "project")
+
+    def validate(self, attrs):
+        if "project" not in attrs:
+            raise ValidationError("Project cannot be null")
+        return attrs
+
+    @staticmethod
+    def validate_project(value):
+        if not value:
+            raise ValidationError("Project cannot be null")
+        return value
+
+
+class CountryFieldsCreateSerializer(serializers.Serializer):
+    fields = CountryFieldsSerializer(many=True, required=True, allow_null=False)
+
+    def create(self, validated_data):
+        return [CountryField.objects.create(**field) for field in validated_data['fields']]
+
+    def to_representation(self, instances):
+        return {"fields": [instance.to_representation() for instance in instances]}
+
+
+class CountryFieldsUpdateSerializer(serializers.Serializer):
+    fields = CountryFieldsSerializer(many=True, required=True, allow_null=False)
+
+    # def create(self, validated_data):
+    #     return [CountryField.objects.create(**field) for field in validated_data['fields']]
+    #
+    def to_representation(self, instances):
+        # return {"fields": [instance.to_representation() for instance in instances]}
+        return {}
+
+    def update(self, instances, validated_data):
+        updated_fields = validated_data['fields']
+        for updated_field in updated_fields:
+            for instance in instances:
+                if instance.question == updated_field['question'] and instance.type == updated_field['type']:
+                    instance.answer = updated_field['answer']
+                    instance.save()
+        return instances
