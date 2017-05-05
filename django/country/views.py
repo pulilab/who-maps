90ef8ba499dc94f34d3a3372c0ae256d77350114
Gyorgy Fekete <f@pulilab.com>
2017-05-05 13:03:28 +0200

    country field views

--- django/country/views.py
+++ django/country/views.py
@@ -1,6 +1,9 @@
-from rest_framework import generics, mixins, viewsets
-from .models import Country
-from .serializers import CountryListSerializer, LandingPageSerializer
+from rest_framework import generics, mixins, viewsets, status
+from rest_framework.response import Response
+
+from .models import Country, CountryField
+from .serializers import CountryListSerializer, LandingPageSerializer, CountryFieldsListSerializer, \
+    CountryFieldsCreateSerializer, CountryFieldsUpdateSerializer
 
 
 class CountryListAPIView(generics.ListAPIView):
@@ -13,3 +16,36 @@ class RetrieveLandingPageViewSet(mixins.RetrieveModelMixin,
     queryset = Country.objects.all()
     serializer_class = LandingPageSerializer
     lookup_field = "code"
+
+
+class CountryFieldsListView(generics.ListAPIView):
+    serializer_class = CountryFieldsListSerializer
+
+    def get_queryset(self):
+        return CountryField.objects.filter(project=None, enabled=True)
+
+
+class CountryFieldsCreateView(generics.CreateAPIView):
+    serializer_class = CountryFieldsCreateSerializer
+
+
+class CountryFieldsUpdateView(generics.GenericAPIView):
+    serializer_class = CountryFieldsUpdateSerializer
+
+    def get_queryset(self):
+        return CountryField.objects.filter(enabled=True)
+
+    def put(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        country_id = self.kwargs.get('country_id')
+        project_id = self.kwargs.get('project_id')
+        instances = queryset.filter(country_id=country_id, project_id=project_id)
+
+        # May raise a permission denied
+        self.check_object_permissions(self.request, instances[0])
+
+        serializer = self.get_serializer(instances, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data)
