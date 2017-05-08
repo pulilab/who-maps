2d29e380633a31641306db41cdabac6ee937e04c
lordfuoco <lordfuoco@gmail.com>
2017-05-08 17:53:21 +0200

    reverted to avoid circleci errors

--- frontend/test-context.js
+++ frontend/test-context.js
@@ -1,6 +1,2 @@
-const testsContext = require.context('./src', true, /-spec\.js$/);
-testsContext.keys().forEach(testsContext);
-
-const componentsContext = require.context('./src/', true, /^((?!-spec).)*.js$/);
-
-componentsContext.keys().forEach(componentsContext);
+const context = require.context('./src', true, /-spec\.js$/);
+context.keys().forEach(context);
