b5b6e8391ef47295c34ef3848d8f31ff2b64cc94
Gyorgy Fekete <f@pulilab.com>
2017-05-09 18:03:33 +0200

    state filter lookups and choices

--- django/cms/admin.py
+++ django/cms/admin.py
@@ -2,7 +2,29 @@ from django.contrib import admin
 from django.contrib.admin import SimpleListFilter
 from cms.models import Post, State
 
-# Register your models here.
+
+class StateFilter(SimpleListFilter):
+    title = 'State'
+
+    parameter_name = 'state'
+
+    def lookups(self, request, model_admin):
+        return ((0, "All"),) + State.STATE_CHOICES
+
+    def choices(self, cl):  # pragma: no cover
+        for lookup, title in self.lookup_choices:
+            try:
+                selected = int(self.value()) == lookup
+            except TypeError:
+                selected = State.FLAGGED == lookup
+
+            yield {
+                'selected': selected,
+                'query_string': cl.get_query_string({
+                    self.parameter_name: lookup,
+                }, []),
+                'display': title,
+            }
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('name', 'type', 'domain', 'state', 'modified', 'author')
