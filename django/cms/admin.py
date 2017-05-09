5090cb95b5391296bbffcdd66bfe902a64514225
Gyorgy Fekete <f@pulilab.com>
2017-05-09 18:03:09 +0200

    post admin to show content, disabled adding from here

--- django/cms/admin.py
+++ django/cms/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.admin import SimpleListFilter
+from cms.models import Post, State
 
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'domain', 'state', 'modified', 'author')
+    list_filter = (StateFilter, 'type')
+    search_fields = ('name', 'body', 'author__name', 'author__user__email')
+    ordering = ('name',)
+
+    def has_add_permission(self, request):
+        return False
