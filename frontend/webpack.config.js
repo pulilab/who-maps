1e0670aff2268cbbab4dd31188c011125c62520a
lordfuoco <lordfuoco@gmail.com>
2017-05-10 13:08:07 +0200

    minor fix for changed api on webpack-dev-server

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -118,6 +118,8 @@ module.exports = {
         ]
     },
     devServer: {
+        host: '0.0.0.0',
+        disableHostCheck: true,
         historyApiFallback: true,
         proxy: {
             '/api/*': {
