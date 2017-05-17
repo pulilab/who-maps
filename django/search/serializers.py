f0041dcda9b30b106de1ec5101f8bf554ee926bb
Gyorgy Fekete <f@pulilab.com>
2017-05-17 18:07:49 +0200

    all serializer field for search

--- django/search/serializers.py
+++ django/search/serializers.py
@@ -6,6 +6,6 @@ class SearchSerializer(serializers.Serializer):
     query = serializers.CharField()
     location = serializers.BooleanField(required=False)
     project_name = serializers.BooleanField(required=False)
-    health_topic = serializers.BooleanField(required=False)
     technology_platform = serializers.BooleanField(required=False)
     organisation = serializers.BooleanField(required=False)
+    all = serializers.BooleanField(required=False)
