c97581634799267a9b3ba0a60fe4baf4f3770b7d
Gyorgy Fekete <f@pulilab.com>
2017-06-19 16:47:48 +0200

    fab new production server, tags settings

--- django/fabfile.py
+++ django/fabfile.py
@@ -17,10 +17,10 @@ def dev():
 
 def production():
     """Configure prod"""
-    env.hosts = ['whomaps@test.whomaps.pulilab.com']
+    env.hosts = ['whomaps@207.154.215.126']
     env.name = 'production'
     env.port = 22
-    env.branch = "master"
+    env.branch = "tags/2.0"
     env.project_root = '/home/whomaps/who-maps'
     env.backend_root = 'django'
     env.frontend_root = 'frontend'
@@ -31,7 +31,7 @@ def staging():
     env.hosts = ['whomaps@139.59.148.238']
     env.name = 'staging'
     env.port = 22
-    env.branch = "tags/2.0-rc6"
+    env.branch = "tags/2.0-rc7"
     env.project_root = '/home/whomaps/who-maps'
     env.backend_root = 'django'
     env.frontend_root = 'frontend'
