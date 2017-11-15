50d54b804dfb3e1d0dec3360942b23b8e8ef644a
lordfuoco <lordfuoco@gmail.com>
2017-11-15 10:42:08 +0100

    hooked landingPageController to the store and removed custom stuff

--- frontend/src/LandingPage/LandingPageController.js
+++ frontend/src/LandingPage/LandingPageController.js
@@ -1,57 +1,35 @@
-import { Storage } from '../Common/';
-import { CustomCountryService } from '../Common/';
+import { getSubDomain } from '../Utilities';
 import * as ProjectModule from '../store/modules/projects';
-import * as UserModule from '../store/modules/user';
+import * as CountryModule from '../store/modules/countries';
 
 class LandingPageModuleController {
 
     constructor($scope, $location, $anchorScroll, $ngRedux) {
-        this.storage = new Storage();
-        this.ccs = CustomCountryService;
         this.scope = $scope;
         this.EE = window.EE;
         this.$location = $location;
         this.$anchorScroll = $anchorScroll;
         this.$onInit = this.onInit.bind(this);
         this.$onDestroy = this.onDestroy.bind(this);
-        this.unsubscribeUsers = $ngRedux.connect(this.mapUsers, UserModule)(this);
-        this.unsubscribeProjects = $ngRedux.connect(this.mapProjects, ProjectModule)(this);
+        this.unsubscribe = $ngRedux.connect(this.mapState, CountryModule)(this);
     }
 
-    mapUsers(state) {
+    mapState(state) {
         return {
-            user: state.user
-        };
-    }
-
-    mapProjects(state) {
-        return {
-            projects: ProjectModule.getPublishedProjects(state)
+            user: state.user,
+            projects: ProjectModule.getPublishedProjects(state),
+            countryData: CountryModule.getCountryCoverPage(state),
+            countryCover: CountryModule.getCountryCoverPicture(state)
         };
     }
 
     onInit() {
-        const vm = this;
-        const subDomain = this.ccs.getSubDomain();
-        this.ccs.getCountryData(subDomain).then(data => {
-            vm.scope.$evalAsync(() => {
-                vm.countryData = data;
-                vm.countryCover = null;
-                vm.countryData.partners = data.default_partners.concat(data.partner_logos);
-                if (data.cover) {
-                    vm.countryCover = {
-                        background: `url(${data.cover}) 0 0`,
-                        'background-size': 'cover',
-                        'background-repeat': 'no-repeat'
-                    };
-                }
-            });
-        });
+        const subDomain = getSubDomain();
+        this.setCurrentCountryFromCode(subDomain);
     }
 
     onDestroy() {
-        this.unsubscribeUsers();
-        this.unsubscribeProjects();
+        this.unsubscribe();
     }
 
     scrollTo(idString) {
