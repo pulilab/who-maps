423717b3391683ee2e6a5f6a5d4a199401b24a81
lordfuoco <lordfuoco@gmail.com>
2017-11-16 11:45:26 +0100

    countryTopBar completely ported to store

--- frontend/src/Common/CountryTopBar/CountryTopBarController.js
+++ frontend/src/Common/CountryTopBar/CountryTopBarController.js
@@ -1,5 +1,5 @@
 import TopBarBehaviour  from '../TopBarBheaviour';
-import { getSubDomain } from '../../Utilities';
+import * as CountryModule from '../../store/modules/countries';
 
 class CountryTopBarController extends TopBarBehaviour {
 
@@ -7,31 +7,34 @@ class CountryTopBarController extends TopBarBehaviour {
         super($state, $scope, $ngRedux);
         this.EE = window.EE;
         this.timeout = $timeout;
-        this.ccs = require('../CustomCountryService');
         this.$onInit = this.onInit.bind(this);
+        // this has a different name because TopBarBehaviour implement is own state
+        this.unsubscribeCountry = $ngRedux.connect(this.mapCountryState, CountryModule)(this);
+    }
+
+    mapCountryState(state) {
+        const countryData = CountryModule.getCountryCoverPage(state);
+        const showCountryNameAndFlag = countryData && countryData.name && countryData.name !== 'WHO';
+        return {
+            countryData,
+            countryFlag: CountryModule.getCurrentCountry(state).flag,
+            pageLoaded: !!countryData.name,
+            showCountryNameAndFlag,
+            logoClass: showCountryNameAndFlag ? 'has-country-logo' : ''
+        };
     }
 
     onInit() {
-        const vm = this;
         this.pageLoaded = false;
         this.watchers();
         this.commonInit();
-        if (this.user) {
-            this.cs.loadedPromise.then(vm.setProfileData.bind(vm));
-        }
-        const subDomain = getSubDomain();
-        this.countryFlag = this.ccs.getCountryFlag(subDomain);
-        this.ccs.getCountryData(subDomain).then(data => {
-            this.scope.$evalAsync(() => {
-                vm.countryData = data;
-                vm.pageLoaded = true;
-                vm.showCountryNameAndFlag = vm.countryData && vm.countryData.name && vm.countryData.name !== 'WHO';
-                vm.logoClass = vm.showCountryNameAndFlag ? 'has-country-logo' : '';
-            });
-        });
 
         window.onscroll = this.scrollEventHandler.bind(this);
         document.addEventListener('scroll', this.scrollEventHandler.bind(this), true);
+    }
+
+    onDestroy() {
+        this.unsubscribeCountry();
     }
 
     watchers() {
@@ -42,14 +45,6 @@ class CountryTopBarController extends TopBarBehaviour {
             stateName = stateName.replace(/ /g, '-');
             self.pageClass = `page-${stateName}`;
         });
-    }
-
-    setProfileData() {
-        this.userProfile = this.cs.userProfile;
-        if (this.userProfile) {
-            this.profileDataReady = true;
-        }
-        this.scope.$evalAsync();
     }
 
     scrollEventHandler(e) {
