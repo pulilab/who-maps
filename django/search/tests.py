2114ffcbe61c81ee3d5cf0c088a526e41ff40aee
Gyorgy Fekete <f@pulilab.com>
2017-11-28 16:21:45 +0100

    fix search tests

--- django/search/tests.py
+++ django/search/tests.py
@@ -11,17 +11,26 @@ class SearchTests(SetupTests):
     def setUp(self):
         super(SearchTests, self).setUp()
         project = Project.objects.get(id=self.project_id)
-        project.name = "phrase1 phrase2"
-        project.data['implementation_overview'] = "overview"
-        project.save()
-
         country = Country.objects.create(name="phrase2", code="PP2")
 
-        url = reverse("project-crud")
+        project.name = "phrase1 phrase2"
+        project.data['implementation_overview'] = "overview"
+        project.data['country'] = country.id
+        project.save()
+
+        # create draft
+        url = reverse("project-create")
         project_data2 = copy.deepcopy(self.project_data)
         project_data2.update(name="phrase3 phrase5")
         project_data2.update(country=country.id)
-        self.test_user_client.post(url, project_data2, format="json")
+        response = self.test_user_client.post(url, project_data2, format="json")
+        self.assertEqual(response.status_code, 201)
+        project_id = response.json()['id']
+
+        # publish it
+        url = reverse("project-publish", kwargs=dict(pk=project_id))
+        response = self.test_user_client.put(url, project_data2, format="json")
+        self.assertEqual(response.status_code, 200)
 
     def test_search_two_fields(self):
         url = reverse("search-project")
