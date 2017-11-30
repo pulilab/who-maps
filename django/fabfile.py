0f2114409f7e4ed60833f389829706441b875bc9
Gyorgy Fekete <f@pulilab.com>
2017-11-30 11:46:40 +0100

    include linting in fab cov

--- django/fabfile.py
+++ django/fabfile.py
@@ -158,6 +158,7 @@ def test(app=""):
 
 
 def cov():
+    lint()
     local(
         "docker-compose exec django py.test --cov --cov-report html --cov-fail-under 100 --cov-report term-missing"
         " --cov-config .coveragerc"
