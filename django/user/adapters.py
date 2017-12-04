9c8d665dbdd80850dc655c084553b64c8ff89336
Simon Sagi <github@simonsagi.com>
2017-12-04 14:14:30 +0100

    - fix celery instance picking

--- django/user/adapters.py
+++ django/user/adapters.py
@@ -2,6 +2,9 @@ from allauth.account.adapter import DefaultAccountAdapter
 from django.conf import settings
 from django.contrib.sites.models import Site
 
+# This has to stay here to use the proper celery instance with the djcelery_email package
+import scheduler.celery # noqa
+
 
 class DefaultAccountAdapterCustom(DefaultAccountAdapter):
     ACTIVATE_BASE_URL = 'http://' + Site.objects.get(id=settings.SITE_ID).domain + '/'
