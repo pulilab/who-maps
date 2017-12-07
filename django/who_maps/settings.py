0f01dcb321620b2787fe7de6aff87d1fa0106e07
Simon Sagi <github@simonsagi.com>
2017-12-07 17:32:05 +0100

    - fix static

--- django/who_maps/settings.py
+++ django/who_maps/settings.py
@@ -142,6 +142,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/usr/share/django/static'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
