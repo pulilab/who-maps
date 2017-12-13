276612269911ae797875587076a26dafed281740
lordfuoco <lordfuoco@gmail.com>
2017-12-13 10:00:49 +0100

    added proper controller and translation support to disclaimer component

--- frontend/src/Common/Disclaimer/disclaimerComponent.js
+++ frontend/src/Common/Disclaimer/disclaimerComponent.js
@@ -1,9 +1,12 @@
 import _template from './disclaimer.html';
 import './disclaimer.scss';
+import DisclaimerController from './DisclaimerController';
 
 const donorsComponent = {
     template: _template,
-    name: 'disclaimer'
+    name: 'disclaimer',
+    controllerAs: 'vm',
+    controller: DisclaimerController.factory()
 };
 
 export default donorsComponent;
