d5eddd134cb083b325978f22644253891c588ea2
lordfuoco <lordfuoco@gmail.com>
2017-12-13 09:08:13 +0100

    more translations for landing page and fix for countryTpBar

--- frontend/src/LandingPage/LandingPageController.js
+++ frontend/src/LandingPage/LandingPageController.js
@@ -1,6 +1,7 @@
 import { getSubDomain } from '../Utilities';
 import * as ProjectModule from '../store/modules/projects';
 import * as CountryModule from '../store/modules/countries';
+import * as LanguageModule from '../store/modules/language';
 
 class LandingPageModuleController {
 
@@ -12,10 +13,12 @@ class LandingPageModuleController {
         this.$anchorScroll = $anchorScroll;
         this.$onInit = this.onInit.bind(this);
         this.$onDestroy = this.onDestroy.bind(this);
+        this.mapState = this.mapState.bind(this);
         this.unsubscribe = $ngRedux.connect(this.mapState, CountryModule)(this);
     }
 
     mapState(state) {
+        this.translate = LanguageModule.translate.bind(this, state);
         return {
             user: state.user,
             projects: ProjectModule.getUserProjects(state),
