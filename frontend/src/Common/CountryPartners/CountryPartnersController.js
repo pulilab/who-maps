4f8a2387323e81df29b55805d989e9293f37db32
lordfuoco <lordfuoco@gmail.com>
2017-12-13 09:51:35 +0100

    Added translations system to countryPartners

--- frontend/src/Common/CountryPartners/CountryPartnersController.js
+++ frontend/src/Common/CountryPartners/CountryPartnersController.js
@@ -1,14 +1,27 @@
+import * as LanguageModule from '../../store/modules/language';
 
 class CountryPartnersController {
 
-    constructor($scope, $state) {
-        const vm = this;
-        vm.scope = $scope;
-        vm.state = $state;
-        vm.$onInit = vm.onInit.bind(vm);
+    constructor($scope, $state, $ngRedux) {
+        this.scope = $scope;
+        this.state = $state;
+        this.$ngRedux = $ngRedux;
+        this.$onInit = this.onInit.bind(this);
+        this.$onDestroy = this.onDestroy.bind(this);
+        this.mapState = this.mapState.bind(this);
+    }
+
+    mapState(state) {
+        this.translate = LanguageModule.translate.bind(this, state);
+        return {};
     }
 
     onInit() {
+        this.unsubscribe = this.$ngRedux.connect(this.mapState, null)(this);
+    }
+
+    onDestroy() {
+        this.unsubscribe();
     }
 
     computeLogoStyle(logo) {
@@ -17,11 +30,11 @@ class CountryPartnersController {
 
     static countryPartnersFactory() {
         require('./countryPartners.scss');
-        function countryPartners($scope, $state) {
-            return new CountryPartnersController($scope, $state);
+        function countryPartners($scope, $state, $ngRedux) {
+            return new CountryPartnersController($scope, $state, $ngRedux);
         }
 
-        countryPartners.$inject = ['$scope', '$state'];
+        countryPartners.$inject = ['$scope', '$state', '$ngRedux'];
 
         return countryPartners;
     }
