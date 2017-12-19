a07b48911359479b7db6e2310a4db71b16202e72
lordfuoco <lordfuoco@gmail.com>
2017-12-19 11:50:37 +0100

    modified thematic_overview and domains

--- django/core/data/thematic_overview.py
+++ django/core/data/thematic_overview.py
@@ -1,30 +1,41 @@
 from django.utils.translation import ugettext_lazy as _
 
+CATEGORIES = [
+    {'id': 1,
+     'name': _('About')},
+    {'id': 2,
+     'name': _('Introduction')}
+]
 
-THEMATIC_OVERVIEW = [
-    {'id': 17,
-     'axis': 7,
+SUB_CATEGORIES = [
+    {'id': 11,
+     'category': 1,
      'name': _('Foreword')},
-    {'id': 18,
-     'axis': 7,
+    {'id': 12,
+     'category': 1,
      'name': _('Acknowledgements')},
     {'id': 19,
-     'axis': 7,
+     'category': 1,
      'name': _('Executive summary')},
     {'id': 20,
-     'axis': 7,
+     'category': 1,
      'name': _('Acronyms and abbreviations')},
     {'id': 21,
-     'axis': 7,
+     'category': 1,
      'name': _('Key terminology')},
 
     {'id': 22,
-     'axis': 8,
+     'category': 2,
      'name': _('Background: scaling up mHealth')},
     {'id': 23,
-     'axis': 8,
+     'category': 2,
      'name': _('The MAPS Toolkit')},
     {'id': 24,
-     'axis': 8,
+     'category': 2,
      'name': _('How to use MAPS')}
 ]
+
+THEMATIC_OVERVIEW = {
+    'categories': CATEGORIES,
+    'sub_categories': SUB_CATEGORIES
+}
