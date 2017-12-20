edf22d2e7150e3bc3e64925bfc59b5ed19298fdb
Simon Sagi <github@simonsagi.com>
2017-12-20 10:39:58 +0100

    - fix innermost level translation

--- django/project/tests.py
+++ django/project/tests.py
@@ -1493,3 +1493,61 @@ class TestModelTranslations(TestCase):
         response = self.test_user_client.get(url, HTTP_ACCEPT_LANGUAGE='fr')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json()['technology_platforms'][0]['name'], 'French name')
+
+    def test_innermost_translation(self):
+        DigitalStrategy.objects.all().delete()
+        HealthFocusArea.objects.all().delete()
+        HealthCategory.objects.all().delete()
+        cache.clear()
+
+        strategy = DigitalStrategy.objects.create(name='Test strategy', group=DigitalStrategy.GROUP_CHOICES[0][0])
+        strategy.name_en = 'English name'
+        strategy.name_fr = 'French name'
+        strategy.save()
+
+        child_strategy = DigitalStrategy.objects.create(name='Child strategy', parent=strategy,
+                                                        group=DigitalStrategy.GROUP_CHOICES[0][0])
+        child_strategy.name_en = 'Child name'
+        child_strategy.name_fr = 'Omlette du fromage'
+        child_strategy.save()
+
+        health_category = HealthCategory.objects.create(name='Parent category')
+        health_category.name_en = 'English name'
+        health_category.name_fr = 'French name'
+        health_category.save()
+
+        health_focus_area = HealthFocusArea.objects.create(name='Health focus area', health_category=health_category)
+        health_focus_area.name_en = 'English area'
+        health_focus_area.name_fr = 'French area'
+        health_focus_area.save()
+
+        url = reverse('get-project-structure')
+        # Getting the english version
+        response = self.test_user_client.get(url, HTTP_ACCEPT_LANGUAGE='en')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()['strategies'][0],
+                         {'name': 'Client',
+                          'subGroups': [{'id': strategy.id,
+                                         'name': 'English name',
+                                         'strategies': [{'id': child_strategy.id,
+                                                         'name': 'Child name'}]}]})
+        self.assertEqual(response.json()['health_focus_areas'][0],
+                         {'id': health_category.id,
+                          'name': 'English name',
+                          'health_focus_areas': [{'id': health_focus_area.id,
+                                                  'name': 'English area'}]})
+
+        # Getting the french version
+        response = self.test_user_client.get(url, HTTP_ACCEPT_LANGUAGE='fr')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()['strategies'][0],
+                         {'name': 'Client',
+                          'subGroups': [{'id': strategy.id,
+                                         'name': 'French name',
+                                         'strategies': [{'id': child_strategy.id,
+                                                         'name': 'Omlette du fromage'}]}]})
+        self.assertEqual(response.json()['health_focus_areas'][0],
+                         {'id': health_category.id,
+                          'name': 'French name',
+                          'health_focus_areas': [{'id': health_focus_area.id,
+                                                  'name': 'French area'}]})
