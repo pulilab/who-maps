4dc16d96b7a5f9ff46304f22307027b8232a2b64
lordfuoco <lordfuoco@gmail.com>
2017-12-21 13:59:06 +0100

    activate field change on click and not on mouseover

--- frontend/src/Common/CountryMap/CountryMapController.js
+++ frontend/src/Common/CountryMap/CountryMapController.js
@@ -160,34 +160,12 @@ class CountryMapController {
               .attr('d', path)
               .classed('d3district', true)
               .classed('global', this.showNationalLevelCoverage)
-              .classed(`name-${districtsName}`, true).on('mouseover', () => {
-                  if (!this.big) {
-                      this.activeDistrict = {
-                          name: districtsName,
-                          data: self.svgLib[districtsName] ? self.svgLib[districtsName].districtData : null
-                      };
-                      this.scope.$evalAsync();
-                  }
-              })
-              .attr('data-justtocatchdomelement', function setDistrictActiveIfHoveredLonger() {
-                  // this bound as DOM iteratee (current district)
-                  if (!self.big) {
-                      return '';
-                  }
-                  d3.select(this).on('mouseover', () => {
-                      window.setTimeout((e) => {
-                          const stillHovered = (e.parentElement.querySelector(':hover') === e);
-                          if (stillHovered) {
-                              self.scope.$evalAsync(() => {
-                                  self.activeDistrict = {
-                                      name: districtsName,
-                                      data: self.svgLib[districtsName] ? self.svgLib[districtsName].districtData : null
-                                  };
-                              });
-                          }
-                      }, 1000, this);
-                  });
-                  return '';
+              .classed(`name-${districtsName}`, true).on('click', () => {
+                  this.activeDistrict = {
+                      name: districtsName,
+                      data: self.svgLib[districtsName] ? self.svgLib[districtsName].districtData : null
+                  };
+                  this.scope.$evalAsync();
               });
         });
 
