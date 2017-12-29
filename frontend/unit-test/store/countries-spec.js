c319c53cf55f315d88ff9653ca1167234a4c6511
lordfuoco <lordfuoco@gmail.com>
2017-12-29 12:54:49 +0100

    fully covered countries store module

--- frontend/unit-test/store/countries-spec.js
+++ frontend/unit-test/store/countries-spec.js
@@ -220,6 +220,195 @@ describe('Countries Store Module', () => {
 
         }));
 
+        it('loadCountryFields', A(async () => {
+            spyOn(axios, 'get').and.returnValue(defaultAxiosSuccess);
+            const state = getState({
+                countries: {
+                    countryFields: [{ country: 1 }]
+                }
+            });
+            dispatch.calls.reset();
+            await CountriesModule.loadCountryFields(1)(dispatch, state);
+            expect(axios.get).not.toHaveBeenCalled();
+            expect(dispatch).not.toHaveBeenCalled();
+
+            await CountriesModule.loadCountryFields(2)(dispatch, state);
+            expect(axios.get).toHaveBeenCalledWith('/api/country-fields/2/');
+            expect(dispatch).toHaveBeenCalledWith({ type: 'UPDATE_COUNTRY_FIELDS_LIST', fields: 1 });
+
+        }));
+
+        it('loadCountryMapDataAndDistricts', A(async () => {
+            spyOn(CountriesModule, 'getCurrentCountry').and.returnValue({ code: 'hu' });
+            const getStateSpy = getState({});
+            const data = {
+                objects: {
+                    1: {
+                        geometries: [{
+                            properties: {
+                                name: 'name',
+                                'name:en': 'name_en',
+                                'name:es': 'name_es',
+                                'name:fr': 'name_fr',
+                                'name:pt': 'name_pt'
+                            }
+                        }]
+                    }
+                }
+            };
+            spyOn(axios, 'get').and.returnValue(Promise.resolve({ data }));
+            CountriesModule.mapData.hu = undefined;
+            await CountriesModule.loadCountryMapDataAndDistricts()(dispatch, getStateSpy);
+            expect(getStateSpy).toHaveBeenCalled();
+            expect(axios.get).toHaveBeenCalledWith('/static/country-geodata/hu.json');
+            expect(dispatch)
+              .toHaveBeenCalledWith({ type: 'SET_CURRENT_COUNTRY_DISTRICTS', districts: jasmine.any(Object) });
+
+
+            axios.get.calls.reset();
+            CountriesModule.mapData.hu = data;
+            await CountriesModule.loadCountryMapDataAndDistricts()(dispatch, getStateSpy);
+            expect(axios.get).not.toHaveBeenCalled();
+            expect(dispatch)
+              .toHaveBeenCalledWith({ type: 'SET_CURRENT_COUNTRY_DISTRICTS', districts: jasmine.any(Object) });
+
+        }));
+
+        it('loadCountryLandingPageInfo', A(async () => {
+            const getStateSpy = getState({});
+            spyOn(axios, 'get').and.returnValue(defaultAxiosSuccess);
+            const spy = spyOn(CountriesModule, 'getCurrentCountry');
+
+            dispatch.calls.reset();
+            await CountriesModule.loadCountryLandingPageInfo()(dispatch, getStateSpy);
+            expect(getStateSpy).toHaveBeenCalled();
+            expect(dispatch).not.toHaveBeenCalled();
+            expect(axios.get).not.toHaveBeenCalled();
+
+            spy.and.returnValue({ code: 'hu' });
+            await CountriesModule.loadCountryLandingPageInfo()(dispatch, getStateSpy);
+            expect(getStateSpy).toHaveBeenCalled();
+            expect(axios.get).toHaveBeenCalledWith('/api/landing/HU/');
+            expect(dispatch).toHaveBeenCalledWith({ type: 'SET_COUNTRY_COVER_DATA', cover: 1 });
+        }));
+
+        it('loadCurrentCountryDistrictsProject', A(async () => {
+            const spy = spyOn(CountriesModule, 'getCurrentCountry');
+            const getStateSpy = getState({});
+            spyOn(axios, 'get').and.returnValue(defaultAxiosSuccess);
+
+            dispatch.calls.reset();
+
+            await CountriesModule.loadCurrentCountryDistrictsProject()(dispatch, getStateSpy);
+            expect(getStateSpy).toHaveBeenCalled();
+            expect(axios.get).not.toHaveBeenCalled();
+            expect(dispatch).not.toHaveBeenCalled();
+
+            spy.and.returnValue({ id: 1 });
+            await CountriesModule.loadCurrentCountryDistrictsProject()(dispatch, getStateSpy);
+            expect(getStateSpy).toHaveBeenCalled();
+            expect(axios.get).toHaveBeenCalledWith('/api/projects/by-view/map/1/');
+            expect(dispatch).toHaveBeenCalledWith({ type: 'SET_CURRENT_COUNTRY_DISTRICT_PROJECTS', projects: 1 });
+
+        }));
+
+        it('loadCountryProjectsOrAll', A(async () => {
+            spyOn(axios, 'get').and.returnValue(defaultAxiosSuccess);
+
+            await CountriesModule.loadCountryProjectsOrAll()(dispatch);
+            expect(axios.get).toHaveBeenCalledWith('/api/projects/by-view/list/');
+            expect(dispatch).toHaveBeenCalledWith({ type: 'SET_CURRENT_COUNTRY_PROJECTS', projects: 1 });
+
+            await CountriesModule.loadCountryProjectsOrAll(1)(dispatch);
+            expect(axios.get).toHaveBeenCalledWith('/api/projects/by-view/list/1/');
+            expect(dispatch).toHaveBeenCalledWith({ type: 'SET_CURRENT_COUNTRY_PROJECTS', projects: 1 });
+        }));
+
+        it('loadCurrentCountryProjects', A(async () => {
+            const getStateSpy = getState({});
+            const currentSpy = spyOn(CountriesModule, 'getCurrentCountry');
+            spyOn(CountriesModule, 'loadCountryProjectsOrAll');
+            await CountriesModule.loadCurrentCountryProjects()(dispatch, getStateSpy);
+            expect(getStateSpy).toHaveBeenCalled();
+            expect(currentSpy).toHaveBeenCalled();
+            expect(CountriesModule.loadCountryProjectsOrAll).not.toHaveBeenCalled();
+
+            currentSpy.and.returnValue({ id: 1 });
+
+            await CountriesModule.loadCurrentCountryProjects()(dispatch, getStateSpy);
+            expect(getStateSpy).toHaveBeenCalled();
+            expect(currentSpy).toHaveBeenCalled();
+            expect(CountriesModule.loadCountryProjectsOrAll).toHaveBeenCalledWith(1);
+        }));
+
+        it('setCurrentCountry', A(async () => {
+            spyOn(CountriesModule, 'loadCountryFields');
+            spyOn(CountriesModule, 'loadCountryMapDataAndDistricts');
+            spyOn(CountriesModule, 'loadCountryLandingPageInfo');
+            spyOn(CountriesModule, 'loadCurrentCountryDistrictsProject');
+            let getStateSpy = getState({ countries: {
+                currentCountry: null
+            } });
+
+            dispatch.calls.reset();
+
+            await CountriesModule.setCurrentCountry()(dispatch, getStateSpy);
+            expect(getStateSpy).toHaveBeenCalled();
+            expect(dispatch).not.toHaveBeenCalled();
+            expect(CountriesModule.loadCountryFields).not.toHaveBeenCalled();
+            expect(CountriesModule.loadCountryMapDataAndDistricts).not.toHaveBeenCalled();
+            expect(CountriesModule.loadCountryLandingPageInfo).not.toHaveBeenCalled();
+            expect(CountriesModule.loadCurrentCountryDistrictsProject).not.toHaveBeenCalled();
+
+            getStateSpy = getState({ countries: {
+                currentCountry: 1
+            } });
+
+            await CountriesModule.setCurrentCountry(1)(dispatch, getStateSpy);
+            expect(getStateSpy).toHaveBeenCalled();
+            expect(dispatch).not.toHaveBeenCalled();
+            expect(CountriesModule.loadCountryFields).not.toHaveBeenCalled();
+            expect(CountriesModule.loadCountryMapDataAndDistricts).not.toHaveBeenCalled();
+            expect(CountriesModule.loadCountryLandingPageInfo).not.toHaveBeenCalled();
+            expect(CountriesModule.loadCurrentCountryDistrictsProject).not.toHaveBeenCalled();
+
+
+            await CountriesModule.setCurrentCountry(2, ['countryFields'])(dispatch, getStateSpy);
+            expect(getStateSpy).toHaveBeenCalled();
+            expect(dispatch).toHaveBeenCalledWith({ type: 'SET_CURRENT_COUNTRY', country: 2 });
+            expect(CountriesModule.loadCountryFields).toHaveBeenCalled();
+            expect(CountriesModule.loadCountryMapDataAndDistricts).toHaveBeenCalled();
+            expect(CountriesModule.loadCountryLandingPageInfo).toHaveBeenCalled();
+            expect(CountriesModule.loadCurrentCountryDistrictsProject).toHaveBeenCalled();
+
+        }));
+
+
+        it('setCurrentCountryFromCode', A(async () => {
+            spyOn(CountriesModule, 'setCurrentCountry');
+            const getStateSpy = getState({ countries: {
+                list: [{ code: 'hu', id: 1 }]
+            } });
+
+            await CountriesModule.setCurrentCountryFromCode('en')(dispatch, getStateSpy);
+            expect(getStateSpy).toHaveBeenCalled();
+            expect(CountriesModule.setCurrentCountry).not.toHaveBeenCalled();
+            expect(dispatch).toHaveBeenCalledWith({ type: 'UNSET_CURRENT_COUNTRY' });
+
+
+            await CountriesModule.setCurrentCountryFromCode('hu')(dispatch, getStateSpy);
+            expect(getStateSpy).toHaveBeenCalled();
+            expect(dispatch).toHaveBeenCalled();
+            expect(CountriesModule.setCurrentCountry).toHaveBeenCalledWith(1, ['landingPage']);
+        }));
+
+        it('csvExport', A(async () => {
+            spyOn(axios, 'post').and.returnValue(defaultAxiosSuccess);
+            const result = await CountriesModule.csvExport(1);
+            expect(axios.post).toHaveBeenCalledWith('/api/projects/csv-export/', 1);
+            expect(result).toBe(1);
+
+        }));
 
     });
 
