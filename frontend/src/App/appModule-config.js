c4c6daa77ed2329c4b6c721b5a1957aa7b60541f
András T. Takács <maurjin@gmail.com>
2018-01-02 11:02:25 +0100

    appModule breakdown, and testing

---
+++ frontend/src/App/appModule-config.js
@@ -0,0 +1,170 @@
+import { setDateLocaleProvider } from '../plugins/language';
+import _appTemplate from './app.html';
+import * as ProjectsModule from '../store/modules/projects';
+import * as UserModule from '../store/modules/user';
+import * as SystemModule from '../store/modules/system';
+import * as CountriesModule from '../store/modules/countries';
+import Storage from '../Common/Storage';
+import { reducers, middleware } from '../store/index';
+
+const storage = new Storage();
+
+async function baseUserResolver($ngRedux) {
+    await $ngRedux.dispatch(UserModule.loadProfile());
+    await $ngRedux.dispatch(SystemModule.loadStaticData());
+    const countries = $ngRedux.dispatch(CountriesModule.loadCountries());
+    const structure = $ngRedux.dispatch(ProjectsModule.loadProjectStructure());
+    return Promise.all([countries, structure]);
+}
+
+async function shareUserResolver($ngRedux) {
+    return $ngRedux.dispatch(UserModule.loadProfile());
+}
+
+async function appDataResolver($ngRedux) {
+    await $ngRedux.dispatch(UserModule.loadProfile());
+    await $ngRedux.dispatch(SystemModule.loadStaticData());
+    const projects = $ngRedux.dispatch(ProjectsModule.loadUserProjects());
+    const structure = $ngRedux.dispatch(ProjectsModule.loadProjectStructure());
+    const profiles = $ngRedux.dispatch(SystemModule.loadUserProfiles());
+    const countries = $ngRedux.dispatch(CountriesModule.loadCountries());
+    return Promise.all([projects, profiles, countries, structure]);
+}
+
+async function publicDataResolver($ngRedux) {
+    await $ngRedux.dispatch(UserModule.loadProfile());
+    await $ngRedux.dispatch(SystemModule.loadStaticData());
+    const projects = $ngRedux.dispatch(ProjectsModule.loadUserProjects());
+    const structure = $ngRedux.dispatch(ProjectsModule.loadProjectStructure());
+    const countries = $ngRedux.dispatch(CountriesModule.loadCountries());
+    return Promise.all([projects, countries, structure]);
+}
+
+function appNameMapper($ngRedux) {
+    const state = $ngRedux.getState();
+    return ProjectsModule.getUserDefaultProject(state);
+}
+
+const config = ($stateProvider, $urlRouterProvider, $locationProvider,
+                $anchorScrollProvider, $ngReduxProvider, $mdDateLocaleProvider) => {
+
+    setDateLocaleProvider($mdDateLocaleProvider);
+
+    $stateProvider
+        .state('base', {
+            url: '',
+            template: _appTemplate,
+            controller: 'systemController',
+            controllerAs: 'vm',
+            abstract: true,
+            resolve: {
+                user: ['$ngRedux', baseUserResolver]
+            }
+        })
+        .state('share', {
+            url: '/project/:projectUUID',
+            template: '<uuid-load />',
+            controllerAs: 'vm',
+            resolve: {
+                user: ['$ngRedux', shareUserResolver]
+            }
+        })
+        .state('app', {
+            url: '/app/:appName',
+            template: '<app layout="column" layout-fill></app>',
+            resolve: {
+                data: ['$ngRedux', appDataResolver]
+            },
+            params: {
+                appName: {
+                    value: ['$ngRedux', appNameMapper]
+                }
+            }
+        })
+        .state('public', {
+            url: '/public/:appName',
+            template: '<app layout="column" view-mode="true"></app>',
+            resolve: {
+                data: ['$ngRedux', publicDataResolver]
+            }
+        })
+        .state('login', {
+            url: '/login',
+            parent: 'base',
+            views: {
+                main: {
+                    template: '<login></login>'
+                }
+            }
+        })
+        .state('reset', {
+            url: '/reset',
+            parent: 'base',
+            views: {
+                main: {
+                    template: '<reset></reset>'
+                }
+            }
+        })
+        .state('signup', {
+            url: '/signup',
+            parent: 'base',
+            views: {
+                main: {
+                    template: '<signup></signup>'
+                }
+            }
+        })
+        .state('editProfile', {
+            url: '/edit-profile',
+            parent: 'app',
+            views: {
+                main: {
+                    template: '<edit-profile ></edit-profile>'
+                }
+            }
+        })
+        .state('emailConfirmation', {
+            url: '/email-confirmation/:key',
+            parent: 'base',
+            views: {
+                main: {
+                    template: '<email-confirmation></email-confirmation>'
+                }
+            }
+        })
+        .state('terms-of-use', {
+            url: '/terms-of-use',
+            parent: 'base',
+            views: {
+                main: {
+                    template: '<terms-of-use></terms-of-use>'
+
+                }
+            }
+        });
+
+    $urlRouterProvider.otherwise('/landing');
+    $locationProvider.html5Mode(true);
+    $anchorScrollProvider.disableAutoScrolling();
+
+    const initialState = {
+        user: {
+            token: storage.get('token') || undefined
+        }
+    };
+    const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__;
+    const storeExtension = reduxDevTools ? [reduxDevTools()] : undefined;
+    $ngReduxProvider.createStoreWith(reducers, middleware, storeExtension, initialState);
+};
+
+config.$inject = [
+    '$stateProvider',
+    '$urlRouterProvider',
+    '$locationProvider',
+    '$anchorScrollProvider',
+    '$ngReduxProvider',
+    '$mdDateLocaleProvider'
+];
+
+export { baseUserResolver, shareUserResolver, appDataResolver, publicDataResolver, appNameMapper, config };
---
+++ frontend/unit-test/App/appModule-run-spec.js
@@ -0,0 +1,120 @@
+/* global define, it, describe, beforeEach, afterEach, expect, jasmine, spyOn, Promise, fdescribe */
+const spyScope = {};
+
+import { $state, toast, dialog, $ngRedux, $timeout } from '../testUtilities';
+const $rootScope = {};
+import { run, handleStateChange, checkProfile, setAxiosBaseTokenIfInStorage } from '../../src/App/appModule-run';
+spyScope.setAxiosBaseTokenIfInStorage = setAxiosBaseTokenIfInStorage;
+import 'axios';
+
+
+let onStartFn, onSuccessFn, onErrorFn, onFinishFn;
+
+const $transitions = {
+    onStart: jasmine.createSpy('onStart').and.callFake((obj, fn) => {
+        onStartFn = fn;
+    }),
+    onSuccess: jasmine.createSpy('onSuccess').and.callFake((obj, fn) => {
+        onSuccessFn = fn;
+    }),
+    onError: jasmine.createSpy('onError').and.callFake((obj, fn) => {
+        onErrorFn = fn;
+    }),
+    onFinish: jasmine.createSpy('onFinish').and.callFake((obj, fn) => {
+        onFinishFn = fn;
+    })
+};
+
+describe('AppModule - run submodule', () => {
+
+    beforeEach(() => {
+        $ngRedux.dispatch.and.stub();
+        $ngRedux.getState.and.callFake(() => ({
+            user: {
+                profile: {
+                    name: 'NAME',
+                    country: 'COUNTRY',
+                    organisation_id: 'ORGANISATION_ID'
+                }
+            }
+        }));
+    });
+
+    afterEach(() => {});
+
+    it('is defined, and importable', () => {
+        expect(typeof run).toBe('function');
+    });
+
+    it('scrolls to top on stateChange', () => {
+        handleStateChange('success', { name: 'a' }, { name: 'b' });
+        handleStateChange('failure_or_something');
+
+        const mainContent = document.createElement('div');
+        mainContent.className = 'main-content';
+        document.body.appendChild(mainContent);
+        mainContent.scrollTop = 10;
+
+        handleStateChange('success', { name: 'a' },  { name: 'b' });
+        expect(mainContent.scrollTop).toBe(0);
+        mainContent.scrollTop = 10;
+
+        handleStateChange('success', { name: 'editProject' },  { name: 'editProject' });
+        expect(mainContent.scrollTop).toBe(0);
+        mainContent.scrollTop = 10;
+    });
+
+    it('handles checking profile', () => {
+        const profile = { name: 'NAME', country: 'COUNTRY', organisation_id: 'ORGANISATION_ID' };
+        const t = { router: { stateService: { target: () => {} } } };
+        spyOn(t.router.stateService, 'target').and.callThrough(str => Promise.resolve(str));
+
+        checkProfile({}, t);
+        expect(t.router.stateService.target).toHaveBeenCalledWith('editProfile');
+
+        const ret = checkProfile(profile, t);
+        expect(t.router.stateService.target).toHaveBeenCalledTimes(1);
+        expect(typeof ret.then).toBe('function');
+    });
+
+    it('sets axios base token if finds it in the storage', () => {
+        const axios = { setAuthToken: jasmine.createSpy('setAuthToken') };
+        const storage = { get: (a) => a };
+        setAxiosBaseTokenIfInStorage(storage, axios);
+        expect(axios.setAuthToken).toHaveBeenCalledWith('token');
+    });
+
+    it('has run fn', () => {
+        run($rootScope, $state, toast, dialog, $ngRedux, $timeout, $transitions, { /* gettextCatalog */ });
+
+        // bind fn to outmost test scope with spies
+        expect($transitions.onStart).toHaveBeenCalledWith({}, jasmine.any(Function));
+        expect(typeof onStartFn).toBe('function');
+        const onStartResult = onStartFn();
+        expect(typeof onStartResult.then).toBe('function');
+
+        expect($transitions.onSuccess).toHaveBeenCalledWith({}, jasmine.any(Function));
+        expect(typeof onSuccessFn).toBe('function');
+        const onSuccessResult = onSuccessFn({ from: () => 'a', to: () => 'b' });
+        expect(typeof onSuccessResult.then).toBe('function');
+
+        expect($transitions.onError).toHaveBeenCalledWith({}, jasmine.any(Function));
+        expect(typeof onErrorFn).toBe('function');
+        const onErrorResult = onErrorFn();
+        expect(typeof onErrorResult.then).toBe('function');
+
+        expect($transitions.onFinish).toHaveBeenCalledWith({}, jasmine.any(Function));
+        expect(typeof onFinishFn).toBe('function');
+        const onFinishResult = onFinishFn({
+            to: () => ({ profileRequired: true }),
+            params: () => ({ appname: 'asdf' })
+        });
+        expect(typeof onFinishResult.then).toBe('function');
+
+        const onFinishResult2 = onFinishFn({
+            to: () => {},
+            params: () => ({ appname: 'asdf' })
+        });
+        expect(onFinishResult2).not.toBe(onFinishResult);
+    });
+});
