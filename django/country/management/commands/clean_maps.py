211519f06dc94fc260aa4bd0098f19eb8c638d60
lordfuoco <lordfuoco@gmail.com>
2018-01-09 17:42:56 +0100

    django management command to slim down the maps based on the country admin selected first_sub_level

---
+++ django/country/management/commands/clean_maps.py
@@ -0,0 +1,32 @@
+import os
+import shutil
+import json
+
+from django.core.management.base import BaseCommand
+from django.conf import settings
+
+import geodata_config
+from country.models import Country
+
+
+class Command(BaseCommand):
+    help = "Removed unused features from geojson."
+
+    def add_arguments(self, parser):
+        parser.add_argument('code', nargs='*', type=str)
+
+    def handle(self, *args, **options):
+        country_code = options['code']
+
+        if country_code:
+            country_code = country_code[0]
+            self.stdout.write('{}'.format(country_code))
+            with open(os.path.join(settings.BASE_DIR, 'media/uploaded_maps', '{}.geojson'.format(country_code))) as f:
+                content = f.read()
+                json_content = json.loads(content)
+                c = Country.objects.get(code=country_code.upper())
+                level = c.map_data['first_sub_level']['name']
+                json_content['features'] = [f for f in json_content['features']
+                                            if f['properties']['wof:placetype'] == level]
+                with open(os.path.join(settings.BASE_DIR, 'media/uploaded_maps', '{}_slim.geojson'.format(country_code)), 'w') as out:
+                    json.dump(json_content, out)
