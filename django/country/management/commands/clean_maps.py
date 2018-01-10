f03f683b33c2436589d82e6bbe7000829fab88b2
lordfuoco <lordfuoco@gmail.com>
2018-01-10 09:45:08 +0100

    Adjusted management command to use the new models and folder hierarchy

--- django/country/management/commands/clean_maps.py
+++ django/country/management/commands/clean_maps.py
@@ -5,6 +5,9 @@ from django.core.management.base import BaseCommand
 from django.conf import settings
 
 from country.models import Country
+from country.models import MapFile
+from pathlib import Path
+
 
 
 class Command(BaseCommand):
@@ -18,14 +21,17 @@ class Command(BaseCommand):
 
         if country_code:
             country_code = country_code[0]
+            c = Country.objects.get(code=country_code.upper())
+            map_file = MapFile.objects.get(country_id=c.id)
             self.stdout.write('{}'.format(country_code))
-            with open(os.path.join(settings.BASE_DIR, 'media/uploaded_maps', '{}.geojson'.format(country_code))) as f:
+            with open(os.path.join(settings.MEDIA_ROOT, '{}'.format(map_file.map_file))) as f:
                 content = f.read()
                 json_content = json.loads(content)
-                c = Country.objects.get(code=country_code.upper())
+
                 level = c.map_data['first_sub_level']['name']
                 json_content['features'] = [f for f in json_content['features']
                                             if f['properties']['wof:placetype'] == level]
-                with open(os.path.join(settings.BASE_DIR, 'media/uploaded_maps',
-                                       '{}_slim.geojson'.format(country_code)), 'w') as out:
+                folder = os.path.join(settings.MEDIA_ROOT, 'processed_maps/')
+                Path(folder).mkdir(parents=True, exist_ok=True)
+                with open(os.path.join(folder,'{}_slim.geojson'.format(country_code)), 'w') as out:
                     json.dump(json_content, out)
