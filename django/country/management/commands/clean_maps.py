30407dd5a377b90aebe127da632a7595188beaa1
lordfuoco <lordfuoco@gmail.com>
2018-01-10 09:54:55 +0100

    Improved management command

--- django/country/management/commands/clean_maps.py
+++ django/country/management/commands/clean_maps.py
@@ -1,6 +1,7 @@
 import os
 import json
 
+from django.core.exceptions import ObjectDoesNotExist
 from django.core.management.base import BaseCommand
 from django.conf import settings
 
@@ -9,9 +10,8 @@ from country.models import MapFile
 from pathlib import Path
 
 
-
 class Command(BaseCommand):
-    help = "Removed unused features from geojson."
+    help = "Remove unused features from geojson."
 
     def add_arguments(self, parser):
         parser.add_argument('code', nargs='*', type=str)
@@ -21,17 +21,24 @@ class Command(BaseCommand):
 
         if country_code:
             country_code = country_code[0]
-            c = Country.objects.get(code=country_code.upper())
-            map_file = MapFile.objects.get(country_id=c.id)
-            self.stdout.write('{}'.format(country_code))
-            with open(os.path.join(settings.MEDIA_ROOT, '{}'.format(map_file.map_file))) as f:
-                content = f.read()
-                json_content = json.loads(content)
+            c = None
+            map_file = None
+            try:
+                c = Country.objects.get(code=country_code.upper())
+                map_file = MapFile.objects.get(country_id=c.id)
+            except ObjectDoesNotExist:
+                self.stdout.write('Selected country does not exist or it does not have an associated MapFile')
 
-                level = c.map_data['first_sub_level']['name']
-                json_content['features'] = [f for f in json_content['features']
-                                            if f['properties']['wof:placetype'] == level]
-                folder = os.path.join(settings.MEDIA_ROOT, 'processed_maps/')
-                Path(folder).mkdir(parents=True, exist_ok=True)
-                with open(os.path.join(folder,'{}_slim.geojson'.format(country_code)), 'w') as out:
-                    json.dump(json_content, out)
+            if c and map_file:
+                self.stdout.write('Removing unused features {} from geojson'.format(c.name))
+                with open(os.path.join(settings.MEDIA_ROOT, '{}'.format(map_file.map_file))) as f:
+                    content = f.read()
+                    json_content = json.loads(content)
+
+                    level = c.map_data['first_sub_level']['name']
+                    json_content['features'] = [f for f in json_content['features']
+                                                if f['properties']['wof:placetype'] == level]
+                    folder = os.path.join(settings.MEDIA_ROOT, 'processed_maps/')
+                    Path(folder).mkdir(parents=True, exist_ok=True)
+                    with open(os.path.join(folder,'{}_slim.geojson'.format(country_code)), 'w') as out:
+                        json.dump(json_content, out)
