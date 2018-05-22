ad976f2caab039a9eebe9c2c3df214199039a732
lordfuoco <lordfuoco@gmail.com>
2018-05-22 15:10:45 +0200

    feat: migrate toolkit test to jest

--- frontend/test/store/toolkit.test.js
+++ frontend/test/store/toolkit.test.js
@@ -3,11 +3,12 @@ import * as SystemModule from '../../src/store/modules/system';
 import { A, defaultAxiosSuccess, dispatch, getState } from '../testUtilities';
 import axios from '../../src/plugins/axios';
 
-/* global it, describe, expect, beforeEach, afterEach, jasmine, spyOn, Promise, console */
-
 describe('TOOLKIT Store Module', () => {
   describe('GETTERS', () => {
-    it('getToolkitData', () => {
+    beforeEach(() => {
+      jest.restoreAllMocks();
+    });
+    test('getToolkitData', () => {
       const state = {
         toolkit: {
           toolkitData: []
@@ -18,31 +19,31 @@ describe('TOOLKIT Store Module', () => {
       expect(result).not.toBe(state.toolkit.toolkitData);
     });
 
-    it('getStructure', () => {
+    test('getStructure', () => {
       const state = {};
-      spyOn(SystemModule, 'getAxis').and.returnValue([{ id: 1 }]);
-      spyOn(SystemModule, 'getDomains').and.returnValue([{ axis: 1, id: 1 }]);
-      spyOn(SystemModule, 'getQuestions').and.returnValue([{ domain: 1, question_id: 'a' }]);
+      jest.spyOn(SystemModule, 'getAxis').mockReturnValue([{ id: 1 }]);
+      jest.spyOn(SystemModule, 'getDomains').mockReturnValue([{ axis: 1, id: 1 }]);
+      jest.spyOn(SystemModule, 'getQuestions').mockReturnValue([{ domain: 1, question_id: 'a' }]);
       const result = ToolkitModule.getStructure(state);
       expect(result[0].domains[0].id).toBe(1);
     });
 
-    it('getDomainStructure', () => {
+    test('getDomainStructure', () => {
       const state = {};
-      const spy = spyOn(ToolkitModule, 'getStructure');
+      const spy = jest.spyOn(ToolkitModule, 'getStructure').mockReturnValue(undefined);
       let result = ToolkitModule.getDomainStructure(state, 0, 0);
       expect(result).toEqual({});
 
-      spy.and.returnValue([{ domains: [{ id: 1 }] }]);
+      spy.mockReturnValue([{ domains: [{ id: 1 }] }]);
       result = ToolkitModule.getDomainStructure(state, 0, 0);
       expect(result.id).toBe(1);
     });
 
-    it('getAxisDetail', () => {
-      spyOn(SystemModule, 'getAxis').and.returnValue([{ id: 1, a: 1, c: 3 }]);
-      spyOn(SystemModule, 'getDomains').and.returnValue([{ id: 1, a: 1, c: 1 }]);
-      spyOn(ToolkitModule, 'getToolkitData')
-        .and.returnValue([{ id: 1, a: 2, domains: [{ id: 1, b: 2, c: 3 }] }]);
+    test('getAxisDetail', () => {
+      jest.spyOn(SystemModule, 'getAxis').mockReturnValue([{ id: 1, a: 1, c: 3 }]);
+      jest.spyOn(SystemModule, 'getDomains').mockReturnValue([{ id: 1, a: 1, c: 1 }]);
+      jest.spyOn(ToolkitModule, 'getToolkitData')
+        .mockReturnValue([{ id: 1, a: 2, domains: [{ id: 1, b: 2, c: 3 }] }]);
       const result = ToolkitModule.getAxisDetail({}, 1);
       expect(result.a).toBe(2);
       expect(result.c).toBe(3);
@@ -57,9 +58,12 @@ describe('TOOLKIT Store Module', () => {
   });
 
   describe('ACTIONS', () => {
-    it('loadToolkitData', A(async () => {
-      dispatch.calls.reset();
-      spyOn(axios, 'get').and.returnValue(defaultAxiosSuccess);
+    beforeEach(() => {
+      jest.restoreAllMocks();
+    });
+    test('loadToolkitData', A(async () => {
+      dispatch.mockClear();
+      jest.spyOn(axios, 'get').mockReturnValue(defaultAxiosSuccess);
       const state = {
         projects: {
           currentProject: null
@@ -73,16 +77,18 @@ describe('TOOLKIT Store Module', () => {
       expect(axios.get).toHaveBeenCalledWith('/api/projects/1/toolkit/data/');
       expect(dispatch).toHaveBeenCalledWith({ type: 'SET_TOOLKIT_DATA', data: 1 });
 
+      jest.spyOn(console, 'log').mockImplementation(() => {});
+
       try {
         await ToolkitModule.loadToolkitData()(dispatch, getState({}));
       } catch (e) {
-        expect(e).toEqual(jasmine.any(Error));
+        expect(e).toEqual(expect.any(Error));
       }
     }));
 
-    it('saveAnswer', A(async () => {
-      dispatch.calls.reset();
-      spyOn(axios, 'post').and.returnValue(defaultAxiosSuccess);
+    test('saveAnswer', A(async () => {
+      dispatch.mockClear();
+      jest.spyOn(axios, 'post').mockReturnValue(defaultAxiosSuccess);
       const state = {
         projects: {
           currentProject: null
@@ -97,23 +103,28 @@ describe('TOOLKIT Store Module', () => {
       expect(dispatch).toHaveBeenCalledWith({ type: 'UPDATE_TOOLKIT_DATA', data: 1 });
       expect(axios.post).toHaveBeenCalledWith('/api/projects/1/toolkit/score/', 1);
 
+      jest.spyOn(console, 'log').mockImplementation(() => {});
+
       try {
         await ToolkitModule.saveAnswer(1)(dispatch, getState({}));
       } catch (e) {
-        expect(e).toEqual(jasmine.any(Error));
+        expect(e).toEqual(expect.any(Error));
       }
     }));
   });
 
   describe('REDUCERS', () => {
-    it('SET_TOOLKIT_DATA', () => {
+    beforeEach(() => {
+      jest.restoreAllMocks();
+    });
+    test('SET_TOOLKIT_DATA', () => {
       let state = {};
       const action = { type: 'SET_TOOLKIT_DATA', data: 1 };
       state = ToolkitModule.default(state, action);
       expect(state.toolkitData).toBe(1);
     });
 
-    it('UPDATE_TOOLKIT_DATA', () => {
+    test('UPDATE_TOOLKIT_DATA', () => {
       let state = {
         toolkitData: [
           {
@@ -135,7 +146,7 @@ describe('TOOLKIT Store Module', () => {
       expect(state.toolkitData[0].domains[0].questions[0].answers[0]).toBe(1);
     });
 
-    it('CLEAR_TOOLKIT_DATA', () => {
+    test('CLEAR_TOOLKIT_DATA', () => {
       let state = {
         toolkitData: 1
       };
