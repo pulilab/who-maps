d3b59a744544dc4197ba33356dd3dfd2b5ed849c
lordfuoco <lordfuoco@gmail.com>
2018-05-22 15:16:32 +0200

    feat: migrate user store test to jest

--- frontend/test/store/user.test.js
+++ frontend/test/store/user.test.js
@@ -3,15 +3,18 @@ import * as ProjectModule from '../../src/store/modules/projects';
 import * as SystemModule from '../../src/store/modules/system';
 import * as CountryModule from '../../src/store/modules/countries';
 import Storage from '../../src/Storage';
+import * as language from '../../src/plugins/language';
 import { A, defaultAxiosSuccess, dispatch, getState } from '../testUtilities';
 import axios from '../../src/plugins/axios';
 
-/* global it, describe, expect, beforeEach, afterEach, jasmine, spyOn, Promise, console */
-
 describe('USER Store Module', () => {
   describe('GETTERS', () => {
-    it('getProfile', () => {
-      const countrySpy = spyOn(CountryModule, 'getCountriesList');
+    beforeEach(() => {
+      jest.restoreAllMocks();
+      jest.spyOn(language, 'setLanguage').mockReturnValue(undefined);
+    });
+    test('getProfile', () => {
+      const countrySpy = jest.spyOn(CountryModule, 'getCountriesList').mockReturnValue(undefined);
       const state = {
         user: {
           profile: {
@@ -24,12 +27,12 @@ describe('USER Store Module', () => {
       expect(result.id).toBe(1);
       expect(countrySpy).toHaveBeenCalled();
 
-      countrySpy.and.returnValue([]);
+      countrySpy.mockReturnValue([]);
       result = UserModule.getProfile(state);
       expect(result).toEqual({ id: 1, country: undefined });
       expect(countrySpy).toHaveBeenCalled();
 
-      countrySpy.and.returnValue([{ id: 1, name: 'a' }]);
+      countrySpy.mockReturnValue([{ id: 1, name: 'a' }]);
       result = UserModule.getProfile(state);
       expect(result).toEqual({ id: 1, country: { id: 1, name: 'a' } });
       expect(countrySpy).toHaveBeenCalled();
@@ -38,8 +41,8 @@ describe('USER Store Module', () => {
       expect(result).toEqual(undefined);
     });
 
-    it('getUserLanguage', () => {
-      const spy = spyOn(SystemModule, 'getLanguages').and.returnValue([]);
+    test('getUserLanguage', () => {
+      const spy = jest.spyOn(SystemModule, 'getLanguages').mockReturnValue([]);
       const state = {};
 
       let result = UserModule.getUserLanguage(state);
@@ -58,14 +61,14 @@ describe('USER Store Module', () => {
       expect(SystemModule.getLanguages).toHaveBeenCalled();
 
       state.user.profile.language = 1;
-      spy.and.returnValue([{ code: 1, name: 'a' }]);
+      spy.mockReturnValue([{ code: 1, name: 'a' }]);
 
       result = UserModule.getUserLanguage(state);
       expect(result).toEqual({ code: 1, name: 'a' });
       expect(SystemModule.getLanguages).toHaveBeenCalled();
     });
 
-    it('isSuperUser', () => {
+    test('isSuperUser', () => {
       const state = {
         user: {
           profile: {
@@ -79,9 +82,13 @@ describe('USER Store Module', () => {
   });
 
   describe('ACTIONS', () => {
-    it('storeData', () => {
-      spyOn(Storage.prototype, 'set');
-      spyOn(axios, 'setAuthToken');
+    beforeEach(() => {
+      jest.restoreAllMocks()
+      jest.spyOn(language, 'setLanguage').mockReturnValue(undefined);
+    });
+    test('storeData', () => {
+      jest.spyOn(Storage.prototype, 'set').mockReturnValue(undefined);
+      jest.spyOn(axios, 'setAuthToken').mockReturnValue(undefined);
       const data = {
         token: 1,
         user_profile_id: 2,
@@ -96,8 +103,8 @@ describe('USER Store Module', () => {
       expect(axios.setAuthToken).toHaveBeenCalledWith(1);
     });
 
-    it('handleProfile', () => {
-      spyOn(Storage.prototype, 'get');
+    test('handleProfile', () => {
+      jest.spyOn(Storage.prototype, 'get').mockReturnValue(undefined);
       let result = UserModule.handleProfile({ id: 1 });
       expect(result.organisation).toEqual(null);
       result = UserModule.handleProfile({ organisation: 1 });
@@ -113,17 +120,17 @@ describe('USER Store Module', () => {
       });
     });
 
-    it('loadProfile', A(async () => {
-      const spy = spyOn(Storage.prototype, 'get');
-      dispatch.calls.reset();
+    test('loadProfile', A(async () => {
+      const spy = jest.spyOn(Storage.prototype, 'get').mockReturnValue(undefined);
+      dispatch.mockClear();
 
-      spyOn(axios, 'get').and.returnValue(defaultAxiosSuccess);
-      spyOn(UserModule, 'handleProfile').and.returnValue(1);
+      jest.spyOn(axios, 'get').mockReturnValue(defaultAxiosSuccess);
+      jest.spyOn(UserModule, 'handleProfile').mockReturnValue(1);
 
       await UserModule.loadProfile()(dispatch, getState({ user: {} }));
       expect(dispatch).not.toHaveBeenCalled();
 
-      spy.and.returnValue(true);
+      spy.mockReturnValue(true);
 
       await UserModule.loadProfile()(dispatch, getState({ user: { profile: 1 } }));
       expect(dispatch).not.toHaveBeenCalled();
@@ -137,10 +144,10 @@ describe('USER Store Module', () => {
       expect(dispatch).toHaveBeenCalledWith({ type: 'SET_PROFILE', profile: 1 });
     }));
 
-    it('doSignup', A(async () => {
-      const spy = spyOn(axios, 'post').and.returnValue(
+    test('doSignup', A(async () => {
+      const spy = jest.spyOn(axios, 'post').mockReturnValue(
         Promise.resolve({ data: { key: 'a', is_superuser: false } }));
-      spyOn(UserModule, 'storeData');
+      jest.spyOn(UserModule, 'storeData').mockReturnValue(undefined);
       const signup = { account_type: 1, password1: 2, password2: 2, email: 3 };
 
       await UserModule.doSignup(signup)(dispatch);
@@ -149,7 +156,7 @@ describe('USER Store Module', () => {
       expect(UserModule.storeData).toHaveBeenCalledWith({ token: 'a', key: 'a', is_superuser: false }, 3);
       const error = new Error();
       error.response = { data: 1 };
-      spy.and.returnValue(Promise.reject(error));
+      spy.mockReturnValue(Promise.reject(error));
 
       try {
         await UserModule.doSignup(signup)(dispatch);
@@ -158,11 +165,11 @@ describe('USER Store Module', () => {
       }
     }));
 
-    it('doLogin', A(async () => {
-      const spy = spyOn(axios, 'post').and.returnValue(defaultAxiosSuccess);
-      spyOn(UserModule, 'storeData');
-      spyOn(UserModule, 'loadProfile');
-      spyOn(ProjectModule, 'loadUserProjects');
+    test('doLogin', A(async () => {
+      const spy = jest.spyOn(axios, 'post').mockReturnValue(defaultAxiosSuccess);
+      jest.spyOn(UserModule, 'storeData').mockReturnValue(undefined);
+      jest.spyOn(UserModule, 'loadProfile').mockReturnValue(undefined);
+      jest.spyOn(ProjectModule, 'loadUserProjects').mockReturnValue(undefined);
 
       await UserModule.doLogin({ username: 1, password: 2 })(dispatch);
       expect(axios.post).toHaveBeenCalledWith('/api/api-token-auth/', { username: 1, password: 2 });
@@ -171,7 +178,7 @@ describe('USER Store Module', () => {
       expect(UserModule.loadProfile).toHaveBeenCalled();
       const error = new Error();
       error.response = { data: 1 };
-      spy.and.returnValue(Promise.reject(error));
+      spy.mockReturnValue(Promise.reject(error));
       try {
         await UserModule.doLogin({ username: 1, password: 2 })(dispatch);
       } catch (e) {
@@ -179,11 +186,11 @@ describe('USER Store Module', () => {
       }
     }));
 
-    it('saveProfile', A(async () => {
-      spyOn(Storage.prototype, 'get').and.returnValue(null);
-      spyOn(UserModule, 'handleProfile').and.returnValue(1);
-      spyOn(axios, 'put').and.returnValue(defaultAxiosSuccess);
-      spyOn(axios, 'post').and.returnValue(defaultAxiosSuccess);
+    test('saveProfile', A(async () => {
+      jest.spyOn(Storage.prototype, 'get').mockReturnValue(null);
+      jest.spyOn(UserModule, 'handleProfile').mockReturnValue(1);
+      jest.spyOn(axios, 'put').mockReturnValue(defaultAxiosSuccess);
+      jest.spyOn(axios, 'post').mockReturnValue(defaultAxiosSuccess);
       const state = { user: {} };
 
       await UserModule.saveProfile({ organisation: { id: 1 }, country: { id: 1 } })(dispatch, getState(state));
@@ -198,9 +205,9 @@ describe('USER Store Module', () => {
       expect(dispatch).toHaveBeenCalledWith({ type: 'SET_PROFILE', profile: 1 });
     }));
 
-    it('doLogout', () => {
-      spyOn(Storage.prototype, 'clear');
-      spyOn(console, 'warn');
+    test('doLogout', () => {
+      jest.spyOn(Storage.prototype, 'clear').mockReturnValue(undefined);
+      jest.spyOn(console, 'warn').mockReturnValue(undefined);
       UserModule.doLogout()(dispatch);
       expect(Storage.prototype.clear).toHaveBeenCalled();
       expect(dispatch).toHaveBeenCalledWith({ type: 'CLEAR_USER_PROJECTS' });
@@ -212,7 +219,7 @@ describe('USER Store Module', () => {
       expect(console.warn).toHaveBeenCalled();
     });
 
-    it('updateTeamViewers', () => {
+    test('updateTeamViewers', () => {
       const state = {
         user: {
           profile: {
@@ -225,15 +232,15 @@ describe('USER Store Module', () => {
       expect(dispatch).toHaveBeenCalledWith({ type: 'UPDATE_TEAM_VIEWER', member: [1, 3], viewer: [2, 4] });
     });
 
-    it('verifyEmail', A(async () => {
-      spyOn(axios, 'post').and.returnValue(defaultAxiosSuccess);
+    test('verifyEmail', A(async () => {
+      jest.spyOn(axios, 'post').mockReturnValue(defaultAxiosSuccess);
       const result = await UserModule.verifyEmail('a');
       expect(axios.post).toHaveBeenCalledWith('/api/rest-auth/registration/verify-email/', 'a');
       expect(result).toBe(1);
     }));
 
-    it('resetPassword', A(async () => {
-      spyOn(axios, 'post').and.returnValue(defaultAxiosSuccess);
+    test('resetPassword', A(async () => {
+      jest.spyOn(axios, 'post').mockReturnValue(defaultAxiosSuccess);
       const result = await UserModule.resetPassword('a');
       expect(axios.post).toHaveBeenCalledWith('/api/rest-auth/password/reset/', 'a');
       expect(result).toBe(1);
@@ -241,14 +248,18 @@ describe('USER Store Module', () => {
   });
 
   describe('REDUCERS', () => {
-    it('SET_USER', () => {
+    beforeEach(() => {
+      jest.restoreAllMocks()
+      jest.spyOn(language, 'setLanguage').mockReturnValue(undefined);
+    });
+    test('SET_USER', () => {
       let state = {};
       const action = { type: 'SET_USER', user: { id: 1 } };
       state = UserModule.default(state, action);
       expect(state.id).toBe(1);
     });
 
-    it('SET_PROFILE', () => {
+    test('SET_PROFILE', () => {
       let state = {};
       let action = { type: 'SET_PROFILE', profile: { id: 1 } };
       state = UserModule.default(state, action);
@@ -262,7 +273,7 @@ describe('USER Store Module', () => {
       expect(state.profile.id).toBe(1);
     });
 
-    it('UPDATE_TEAM_VIEWER', () => {
+    test('UPDATE_TEAM_VIEWER', () => {
       let state = {};
       const action = { type: 'UPDATE_TEAM_VIEWER', member: [1], viewer: [2] };
       state = UserModule.default(state, action);
@@ -270,7 +281,7 @@ describe('USER Store Module', () => {
       expect(state.profile.viewer).toEqual([2]);
     });
 
-    it('UNSET_USER', () => {
+    test('UNSET_USER', () => {
       let state = {};
       const action = { type: 'UNSET_USER' };
       state = UserModule.default(state, action);
