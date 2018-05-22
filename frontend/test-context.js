97912a21e5bc3dec196086053e24012238abc4d5
lordfuoco <lordfuoco@gmail.com>
2018-05-22 11:13:35 +0200

    chore: remove karma and add jest

--- frontend/test-context.js
+++
@@ -1,7 +0,0 @@
-// const context = require.context('./src', true, /-spec\.js$/);
-// context.keys().forEach(context);
-const tests = require.context('./test/', true, /\.js$/);
-tests.keys().forEach(tests);
-
-const sources = require.context('./src/', true, /\.js$/);
-sources.keys().forEach(sources);
