f7057d244b282d53853bec5707658c936858f741
lordfuoco <lordfuoco@gmail.com>
2018-05-22 11:26:36 +0200

    chore: add basic jest config and remove unused script from package json

---
+++ frontend/jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  verbose: true,
+  bail: true,
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/*.js'
+  ],
+  coverageDirectory: 'coverage',
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  },
+  moduleNameMapper: {
+    '~(.*)$': '<rootDir>/$1'
+  }
+};
