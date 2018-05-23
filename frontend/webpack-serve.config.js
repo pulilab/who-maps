c7998d3791e45117d6c13fe1f1a3f4e9f2fbf45e
lordfuoco <lordfuoco@gmail.com>
2018-05-23 13:30:22 +0200

    style: linting of webpack-serve

--- frontend/webpack-serve.config.js
+++ frontend/webpack-serve.config.js
@@ -5,14 +5,14 @@ const history = require('connect-history-api-fallback');
 
 webpackConfig.mode = 'development';
 webpackConfig.serve = {
-    host: '0.0.0.0',
-    add: (app, middleware, options) => {
-        app.use(convert(proxy('/api', { target: 'http://localhost', secure: false })));
-        app.use(convert(proxy('/media', { target: 'http://localhost', secure: false })));
-        app.use(convert(proxy('/static', { target: 'http://localhost', secure: false })));
-        app.use(convert(proxy('/translation', { target: 'http://localhost', secure: false })));
-        app.use(convert(history()));
-    }
-}
+  host: '0.0.0.0',
+  add: (app, middleware, options) => {
+    app.use(convert(proxy('/api', { target: 'http://localhost', secure: false })));
+    app.use(convert(proxy('/media', { target: 'http://localhost', secure: false })));
+    app.use(convert(proxy('/static', { target: 'http://localhost', secure: false })));
+    app.use(convert(proxy('/translation', { target: 'http://localhost', secure: false })));
+    app.use(convert(history()));
+  }
+};
 
 module.exports = webpackConfig;
