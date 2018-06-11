87ab5bce3d3f25782aad4af799e1f7cc1639970e
lordfuoco <lordfuoco@gmail.com>
2018-06-11 19:24:43 +0200

    chore: bump tag version

--- django/fabfile.py
+++ django/fabfile.py
@@ -23,7 +23,7 @@ def production():
     env.host_string = PROD_HOST_STRING
     env.name = 'production'
     env.port = 22
-    env.branch = "tags/3.5.21"
+    env.branch = "tags/3.5.22"
     env.project_root = '/home/whomaps/who-maps'
     env.backend_root = 'django'
     env.frontend_root = 'frontend'
@@ -35,7 +35,7 @@ def staging():
     env.host_string = 'whomaps@139.59.148.238'
     env.name = 'staging'
     env.port = 22
-    env.branch = "tags/3.5.21"
+    env.branch = "tags/3.5.222"
     env.project_root = '/home/whomaps/who-maps'
     env.backend_root = 'django'
     env.frontend_root = 'frontend'
