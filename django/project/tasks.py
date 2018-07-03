d0343523a3cddb7d1af9d12e8c1ca92c9f845894
lordfuoco <lordfuoco@gmail.com>
2018-07-03 16:51:14 +0200

    feat: more parsing, added error 'cleaning' dry

--- django/project/tasks.py
+++ django/project/tasks.py
@@ -2,12 +2,14 @@ from collections import defaultdict
 from django.conf import settings
 from django.utils.translation import ugettext, override
 
-from django.core import mail, exceptions
+from django.core import mail, exceptions, validators
 from django.template import loader
 from django.utils import timezone
 from celery.utils.log import get_task_logger
 from django.contrib.auth.models import User
+from rest_framework.exceptions import ValidationError
 
+from user.models import Organisation
 from country.models import Country
 from toolkit.models import Toolkit
 
@@ -59,29 +61,97 @@ def send_project_approval_digest():
 
 @app.task(name="sync_from_odk_server")
 def sync_from_odk_server():
+    default_nld = {
+        'clients': 0,
+        'facilities': 0,
+        'health_workers': 0
+    }
+
     def uuid_parser(value):
         return value.replace('uuid:', '')
 
     def escaped_int_converter(value):
         return int(value.replace("'", ''))
 
+    def list_parser(list):
+        return json.loads(list)
+
+    def string_to_list(text):
+        return text.split(',')
+
     def first_level_converter(value, type, project):
         if type == 'name':
             project['name'] = value
         elif type == 'organization':
-            project['organisation'] = escaped_int_converter(value)
+            try:
+                project['organisation'] = escaped_int_converter(value)
+            except ValueError:
+                (org, success) = Organisation.objects.get_or_create(name=value)
+                project['organisation'] = org.id
         elif 'platforms' in type:
-                project['platforms'].append({'id': escaped_int_converter(value)})
+                platforms = project.get('platforms', [])
+                project['platforms'] = platforms + [{'id': escaped_int_converter(value)}]
+        elif type == 'clients':
+            nld = project.get('national_level_deployment', dict(default_nld))
+            nld['clients'] = escaped_int_converter(value)
+            project['national_level_deployment'] = nld
+        elif type == 'facilities':
+            nld = project.get('national_level_deployment', dict(default_nld))
+            nld['facilities'] = escaped_int_converter(value)
+            project['national_level_deployment'] = nld
+        elif type == 'health_workers':
+            nld = project.get('national_level_deployment', dict(default_nld))
+            nld['health_workers'] = escaped_int_converter(value)
+            project['national_level_deployment'] = nld
+        elif type == 'contact_email':
+            project['contact_email'] = value
+        elif type == 'contact_name':
+            project['contact_name'] = value
+        elif type == 'donors':
+            project['donors'] = string_to_list(value)
+        elif type == 'end_date':
+            project['end_date'] = value
+        elif type == 'geographic_scope':
+            project['geographic_scope'] = value
+        elif type == 'government_investor':
+            project['government_investor'] = escaped_int_converter(value)
+        elif type == 'health_focus_areas':
+            as_list = list_parser(value)
+            project['health_focus_areas'] = list(map(lambda s: escaped_int_converter(s), as_list))
+        elif type == 'health_information_systems':
+            as_list = list_parser(value)
+            project['his_bucket'] = list(map(lambda s: escaped_int_converter(s), as_list))
+        elif type == 'health_system_challenges':
+            as_list = list_parser(value)
+            project['hsc_challenges'] = list(map(lambda s: escaped_int_converter(s), as_list))
+        elif type == 'implementation_dates':
+            project['implementation_dates'] = value
+        elif type == 'implementation_overview':
+            project['implementation_overview'] = value
+        elif type == 'implementing_partners':
+            project['implementing_partners'] = string_to_list(value)
+        elif type == 'licenses':
+            as_list = list_parser(value)
+            project['licenses'] = list(map(lambda s: escaped_int_converter(s), as_list))
+        elif type == 'mobile_application':
+            project['mobile_application'] = value
+        elif type == 'repository':
+            project['repository'] = value
+        elif type == 'start_date':
+            project['start_date'] = value
+        elif type == 'wiki':
+            project['wiki'] = value
 
     def second_level_converter(value, type, project):
         if 'dhi' in type:
             index = int(type.replace('dhi', '')) - 1
             platform = project['platforms'][index]
             if platform:
-                platform['strategies'] = list(map(lambda s: escaped_int_converter(s), value))
+                as_list = list_parser(value)
+                platform['strategies'] = list(map(lambda s: escaped_int_converter(s), as_list))
 
     def odk_columns_parser(columns):
-        project = dict(platforms=[])
+        project = dict()
         for column in columns:
             type = column.get('column', None)
             value = column.get('value', None)
@@ -103,34 +173,40 @@ def sync_from_odk_server():
                     traceback.print_exc()
         return project
 
-    def serialize_odk_data(row, odk_etag, odk_id, odk_extra_data, existing):
+    def parse_odk_data(row, odk_etag, odk_id, odk_extra_data):
         p = odk_columns_parser(row.get('orderedColumns'))
         p['odk_etag'] = odk_etag
         p['odk_id'] = odk_id
         p['odk_extra_data'] = odk_extra_data
+        return p
+
+    def new_or_updated_serializer(data, existing=None):
         if existing:
-            return ProjectDraftSerializer(existing, data=p)
-        return ProjectDraftSerializer(data=p)
+            return ProjectDraftSerializer(existing, data=data, context={'preserve_etag': True})
+        else:
+            return ProjectDraftSerializer(data=data)
 
-    def save_new_project(serialized_project, email):
+    def serialize_and_save(row, odk_etag, odk_id, odk_extra_data, existing, user_email):
+        parsed = parse_odk_data(row, odk_etag, odk_id, odk_extra_data)
+        serialized = new_or_updated_serializer(parsed, existing)
         try:
-            u = User.objects.get(email=email)
-            project = serialized_project.save(owner=u.userprofile)
-            Toolkit.objects.get_or_create(project_id=project.id, defaults=dict(data=toolkit_default))
-            ProjectApproval.objects.create(project=project)
-        except exceptions.ObjectDoesNotExist:
-            print('No user with following email: {}'.format(email))
-
-    def serialized_and_save(row, odk_etag, odk_id, odk_extra_data, existing, user_email):
-        serialized = serialize_odk_data(row, odk_etag, odk_id, odk_extra_data, existing)
-        try:
-            serialized.is_valid(raise_exception=True)
+            if not serialized.is_valid():
+                for error in serialized.errors:
+                    parsed.pop(error, None)
+                serialized = new_or_updated_serializer(parsed, existing)
+                serialized.is_valid(raise_exception=True)
             if(existing):
                 serialized.save()
             else:
-                save_new_project(serialized, user_email)
-        except exceptions.ValidationError:
-            print('Validation error on project: {}'.format(row))
+                u = User.objects.get(email=user_email)
+                project = serialized.save(owner=u.userprofile)
+                project.post_save_initializations(Toolkit)
+
+        except exceptions.ObjectDoesNotExist:
+            print('No user with following email: {}'.format(user_email))
+        except ValidationError:
+            print('Validation error on project: {}'.format(odk_id))
+            print(serialized.errors)
 
     def save_or_update_project(row, user_email, odk_etag, odk_id, odk_extra_data):
         existing = None
@@ -139,9 +215,9 @@ def sync_from_odk_server():
         except exceptions.ObjectDoesNotExist:
             pass
         if not existing:
-            serialized_and_save(row, odk_etag, odk_id, odk_extra_data, None, user_email)
+            serialize_and_save(row, odk_etag, odk_id, odk_extra_data, None, user_email)
         elif existing.odk_etag and existing.odk_etag != odk_etag:
-            serialized_and_save(row, odk_etag, odk_id, odk_extra_data, existing, None)
+            serialize_and_save(row, odk_etag, odk_id, odk_extra_data, existing, None)
         else:
             print('present, same version: nothing to do ')
 
