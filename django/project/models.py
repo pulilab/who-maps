0d066160061290f66dfe331c338fdf9b421cb386
lordfuoco <lordfuoco@gmail.com>
2018-07-03 16:50:00 +0200

    refactor: move toolkit  and ProjectApproval init in models

--- django/project/models.py
+++ django/project/models.py
@@ -11,6 +11,7 @@ from core.models import ExtendedModel, ExtendedNameOrderedSoftDeletedModel, Acti
 from country.models import Country, CountryField
 from project.cache import InvalidateCacheMixin
 from user.models import UserProfile, Organisation
+from toolkit.toolkit_data import toolkit_default
 
 
 class ProjectManager(models.Manager):
@@ -156,6 +157,12 @@ class Project(SoftDeleteModel, ExtendedModel):
         project_country = Country.objects.filter(id=country_id).first()
         if project_country:
             self.public_id = project_country.code + str(uuid.uuid1()).split('-')[0]
+
+    def post_save_initializations(self, Toolkit):
+        # Add default Toolkit structure for the new project.
+        Toolkit.objects.get_or_create(project_id=self.id, defaults=dict(data=toolkit_default))
+        # Add approval
+        ProjectApproval.objects.create(project=self)
 
 
 class ProjectApproval(ExtendedModel):
