21557bd5c0add4ba3712d1d7795958f9a1bc8a55
Gyorgy Fekete <f@pulilab.com>
2018-07-04 12:00:43 +0200

    refine querysets

--- django/user/tasks.py
+++ django/user/tasks.py
@@ -3,19 +3,25 @@ import paramiko
 from django.conf import settings
 from celery.utils.log import get_task_logger
 from scheduler.celery import app
-from .models import UserProfile
 
 logger = get_task_logger(__name__)
 
 
 @app.task(name="sync_users_to_odk")
-def sync_users_to_odk():
+def sync_user_to_odk(user_profile_id):
+    from .models import UserProfile
+
     ssh = paramiko.SSHClient()
     ssh.load_system_host_keys()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(username=settings.ODK_SERVER_USER, hostname=settings.ODK_SERVER_HOST)
 
-    for profile in UserProfile.objects.all():
+    if not user_profile_id:
+        queryset = UserProfile.objects.exclude(odk_sync=True)
+    else:
+        queryset = UserProfile.objects.filter(id=user_profile_id)
+
+    for profile in queryset:
         user_line = "{} '{}'".format(profile.user.email, profile.user.password)
         ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(
             "python odk_sync_user.py {}".format(user_line))
