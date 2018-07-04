5ae1acf5921f537d541a9514acb9b74e50c249ff
lordfuoco <lordfuoco@gmail.com>
2018-07-04 17:17:43 +0200

    fix: more sturdy parsing in odk sync

--- django/project/tasks.py
+++ django/project/tasks.py
@@ -69,7 +69,7 @@ def sync_project_from_odk():
     import_url = urljoin(base_url, form_url)
     s = requests.Session()
     s.post(login_url, data=settings.ODK_CREDENTIALS)
-    res = s.get(import_url)
+    # res = s.get(import_url)
 
     interoperability_links = InteroperabilityLink.objects.all()
 
@@ -85,124 +85,127 @@ def sync_project_from_odk():
         return json.loads(list)
 
     def string_to_list(text):
-        return text.split(',')
+        return list(filter(lambda s: s, text.split(',')))
 
-    def first_level_converter(value, type, project, int_link_collection):
+    def first_level_converter(value, column_type, project, int_link_collection):
         default_nld = {
             'clients': 0,
             'facilities': 0,
             'health_workers': 0
         }
-        if type == 'name':
+        if column_type == 'name':
             project['name'] = value
-        elif type == 'organization':
+        elif column_type == 'organization':
             try:
                 project['organisation'] = escaped_int_converter(value)
             except ValueError:
                 (org, success) = Organisation.objects.get_or_create(name=value)
                 project['organisation'] = org.id
-        elif type == 'country':
+        elif column_type == 'country':
             project['country'] = escaped_int_converter(value)
-        elif 'platforms' in type:
+        elif 'platforms' in column_type:
                 platforms = project.get('platforms', [])
-                project['platforms'] = platforms + [{'id': escaped_int_converter(value)}]
-        elif type == 'clients':
+                value = escaped_int_converter(value)
+                project['platforms'] = platforms + [{'id': value}]
+        elif column_type == 'clients':
             nld = project.get('national_level_deployment', dict(default_nld))
             nld['clients'] = escaped_int_converter(value)
             project['national_level_deployment'] = nld
-        elif type == 'facilities':
+        elif column_type == 'facilities':
             nld = project.get('national_level_deployment', dict(default_nld))
             nld['facilities'] = escaped_int_converter(value)
             project['national_level_deployment'] = nld
-        elif type == 'health_workers':
+        elif column_type == 'health_workers':
             nld = project.get('national_level_deployment', dict(default_nld))
             nld['health_workers'] = escaped_int_converter(value)
             project['national_level_deployment'] = nld
-        elif type == 'contact_email':
+        elif column_type == 'contact_email':
             project['contact_email'] = value
-        elif type == 'contact_name':
+        elif column_type == 'contact_name':
             project['contact_name'] = value
-        elif type == 'donors':
+        elif column_type == 'donors':
             project['donors'] = string_to_list(value)
-        elif type == 'end_date':
+        elif column_type == 'end_date':
             project['end_date'] = value
-        elif type == 'geographic_scope':
+        elif column_type == 'geographic_scope':
             project['geographic_scope'] = value
-        elif type == 'government_investor':
+        elif column_type == 'government_investor':
             project['government_investor'] = escaped_int_converter(value)
-        elif type == 'health_focus_areas':
+        elif column_type == 'health_focus_areas':
             as_list = list_parser(value)
             project['health_focus_areas'] = list(map(lambda s: escaped_int_converter(s), as_list))
-        elif type == 'health_information_systems':
+        elif column_type == 'health_information_systems':
             as_list = list_parser(value)
             project['his_bucket'] = list(map(lambda s: escaped_int_converter(s), as_list))
-        elif type == 'health_system_challenges':
+        elif column_type == 'health_system_challenges':
             as_list = list_parser(value)
             project['hsc_challenges'] = list(map(lambda s: escaped_int_converter(s), as_list))
-        elif type == 'implementation_dates':
+        elif column_type == 'implementation_dates':
             project['implementation_dates'] = value
-        elif type == 'implementation_overview':
+        elif column_type == 'implementation_overview':
             project['implementation_overview'] = value
-        elif type == 'implementing_partners':
+        elif column_type == 'implementing_partners':
             project['implementing_partners'] = string_to_list(value)
-        elif type == 'licenses':
+        elif column_type == 'licenses':
             as_list = list_parser(value)
             project['licenses'] = list(map(lambda s: escaped_int_converter(s), as_list))
-        elif type == 'mobile_application':
+        elif column_type == 'mobile_application':
             project['mobile_application'] = value
-        elif type == 'repository':
+        elif column_type == 'repository':
             project['repository'] = value
-        elif type == 'start_date':
+        elif column_type == 'start_date':
             project['start_date'] = value
-        elif type == 'wiki':
+        elif column_type == 'wiki':
             project['wiki'] = value
-        elif type == 'interoperability_standards':
+        elif column_type == 'interoperability_standards':
             as_list = list_parser(value)
             project['interoperability_standards'] = list(map(lambda s: escaped_int_converter(s), as_list))
-        elif 'interoperability_link_' in type and '_url' not in type:
+        elif 'interoperability_link_' in column_type and '_url' not in column_type:
             id = None
             try:
                 id = escaped_int_converter(value)
             except ValueError:
+                logging.error('Value error, received: {}'.format(value))
                 pass
             if id:
                 il_obj = next((item for item in int_link_collection if item["id"] == id), None)
                 il_obj['selected'] = True
-                il_obj['odk_name'] = '{}_url'.format(type)
+                il_obj['odk_name'] = '{}_url'.format(column_type)
 
-    def second_level_converter(value, type, project, int_link_collection):
-        dhi_index = 0
-        if 'dhi' in type:
-            platform = project['platforms'][dhi_index]
+    def second_level_converter(value, column_type, project, context):
+        if 'dhi' in column_type:
+            platform = project['platforms'][context["dhi_index"]]
             if platform:
                 as_list = list_parser(value)
                 platform['strategies'] = list(map(lambda s: escaped_int_converter(s), as_list))
-            dhi_index += 1
-        elif 'interoperability_link_' in type and '_url' in type:
-            il_obj = next((item for item in int_link_collection if item["odk_name"] == type), None)
+            context["dhi_index"] += 1
+        elif 'interoperability_link_' in column_type and '_url' in column_type:
+            il_obj = next((item for item in context['int_link_collection'] if item["odk_name"] == column_type), None)
             if il_obj:
-                il_obj['link'] = value
+                il_obj['link'] = value.lower()
 
-    def odk_columns_parser(columns):
+    def odk_columns_parser(columns, interoperability_links):
         int_link_collection = list(map(lambda il: {'id': il.id, 'odk_name': None}, interoperability_links))
         project = dict()
+        context = dict(int_link_collection=int_link_collection, dhi_index=0)
         for column in columns:
-            type = column.get('column', None)
+            column_type = column.get('column', None)
             value = column.get('value', None)
-            if type and value:
+            if column_type and value:
                 try:
-                    first_level_converter(value, type, project, int_link_collection)
+                    first_level_converter(value, column_type, project, int_link_collection)
                 except Exception:
                     traceback.print_exc()
 
         for column in columns:
-            type = column.get('column', None)
+            logging.error(context["dhi_index"])
+            column_type = column.get('column', None)
             value = column.get('value', None)
-            if type and value:
+            if column_type and value:
                 try:
-                    second_level_converter(value, type, project, int_link_collection)
+                    second_level_converter(value, column_type, project, context)
                 except IndexError:
-                    logging.error('{} has invalid / missing value: {}'.format(type, value))
+                    logging.error('{} has invalid / missing value: {}'.format(column_type, value))
                 except Exception:
                     traceback.print_exc()
 
@@ -211,8 +214,8 @@ def sync_project_from_odk():
         project['interoperability_links'] = int_link_collection
         return project
 
-    def parse_odk_data(row, odk_etag, odk_id, odk_extra_data):
-        p = odk_columns_parser(row.get('orderedColumns'))
+    def parse_odk_data(row, odk_etag, odk_id, odk_extra_data, interoperability_links):
+        p = odk_columns_parser(row.get('orderedColumns'), interoperability_links)
         p['odk_etag'] = odk_etag
         p['odk_id'] = odk_id
         p['odk_extra_data'] = odk_extra_data
@@ -224,11 +227,12 @@ def sync_project_from_odk():
         else:
             return ProjectDraftSerializer(data=data)
 
-    def serialize_and_save(row, odk_etag, odk_id, odk_extra_data, existing, user_email):
-        parsed = parse_odk_data(row, odk_etag, odk_id, odk_extra_data)
-        serialized = new_or_updated_serializer(parsed, existing)
+    def serialize_and_save(row, odk_etag, odk_id, odk_extra_data, existing, user_email, interoperability_links):
+        parsed = parse_odk_data(row, odk_etag, odk_id, odk_extra_data, interoperability_links)
+        serialized = new_or_updated_serializer(parsed, existing,)
         try:
             if not serialized.is_valid():
+                logging.error(serialized.errors)
                 for error in serialized.errors:
                     parsed.pop(error, None)
                 serialized = new_or_updated_serializer(parsed, existing)
@@ -246,7 +250,7 @@ def sync_project_from_odk():
             logging.warning('Validation error/s:')
             logging.warning(serialized.errors)
 
-    def save_or_update_project(row, user_email, odk_etag, odk_id, odk_extra_data):
+    def save_or_update_project(row, user_email, odk_etag, odk_id, odk_extra_data, interoperability_links):
         existing = None
         try:
             existing = Project.objects.get(odk_id=odk_id)
@@ -254,22 +258,22 @@ def sync_project_from_odk():
             pass
         if not existing:
             logging.error('Does not exist in DHA database: importing')
-            serialize_and_save(row, odk_etag, odk_id, odk_extra_data, None, user_email)
+            serialize_and_save(row, odk_etag, odk_id, odk_extra_data, None, user_email, interoperability_links)
         elif existing.odk_etag is None:
             logging.error('Overridden by ui: nothing to do')
         elif existing.odk_etag != odk_etag:
             logging.error('Exist in DHA database, but new version found in ODK: updating')
-            serialize_and_save(row, odk_etag, odk_id, odk_extra_data, existing, None)
+            serialize_and_save(row, odk_etag, odk_id, odk_extra_data, existing, None, interoperability_links)
         else:
             logging.error('Already present and same version: nothing to do')
 
-    def start_sync(rows):
+    def start_sync(rows, interoperability_links):
         logging.error('ODK IMPORT TASK START: {}'.format(datetime.now()))
         logging.error('\n')
         for row in rows:
             odk_etag = uuid_parser(row.get('rowETag'))
             odk_id = uuid_parser(row.get('id'))
-            savepoint_type = row.get('savepointType', 'INCOMPLETE')
+            savepoint_column_type = row.get('savepointType', 'INCOMPLETE')
             deleted = row.get('deleted', True)
             user_email = row.get('createUser', None)
             odk_extra_data = {
@@ -281,14 +285,14 @@ def sync_project_from_odk():
                 'filterScope': row.get('filterScope', None)
             }
             logging.error('Processing odk_id {} with odk_etag {} START'.format(odk_id, odk_etag))
-            if savepoint_type.lower() == 'complete' and deleted != 'true':
-                save_or_update_project(row, user_email, odk_etag, odk_id, odk_extra_data)
+            if savepoint_column_type.lower() == 'complete' and deleted != 'true':
+                save_or_update_project(row, user_email, odk_etag, odk_id, odk_extra_data, interoperability_links)
             else:
                 logging.error('Incomplete or deleted: nothing to do')
             logging.error('\n')
         logging.error('ODK IMPORT TASK END: {}'.format(datetime.now()))
 
-    # with open('project/static-json/odk.json') as odk_file:
-    #     rows = json.load(odk_file)
-    #     start_sync(rows)
-    start_sync(res.json())
+    with open('project/static-json/odk.json') as odk_file:
+        rows = json.load(odk_file)
+        start_sync(rows, interoperability_links)
+    # start_sync(res.json())
