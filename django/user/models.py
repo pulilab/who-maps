0486344f365323a6d8645a3a2f21136fcd2a0a91
Gyorgy Fekete <f@pulilab.com>
2018-07-04 12:00:26 +0200

    sync odk on userprofile created

--- django/user/models.py
+++ django/user/models.py
@@ -1,9 +1,12 @@
-from django.db import models
+from django.db import models, transaction
 from django.conf import settings
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.utils.translation import ugettext_lazy as _
 
 from core.models import NameByIDMixin, ExtendedModel
+from .tasks import sync_user_to_odk
 
 
 class Organisation(NameByIDMixin, ExtendedModel):
@@ -39,3 +42,9 @@ class UserProfile(ExtendedModel):
 
     def __str__(self):
         return "{} <{}>".format(self.name, self.user.email) if self.name else ""
+
+
+@receiver(post_save, sender=UserProfile)
+def odk_sync_on_save(sender, instance, created, **kwargs):
+    if created:
+        transaction.on_commit(lambda: sync_user_to_odk.apply_async(args=(instance.pk,)))
