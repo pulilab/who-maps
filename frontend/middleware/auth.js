4977750c4d8f254b53bcb7787137124917d5d825
lordfuoco <lordfuoco@gmail.com>
2018-07-11 12:14:28 +0200

    fix: check if tokens exist before checking inner attributes

--- frontend/middleware/auth.js
+++ frontend/middleware/auth.js
@@ -2,7 +2,7 @@ import { getTokenFromCookie, getTokenFromLocalStorage } from '../utilities/auth'
 
 export default async function ({store, req}) {
   const tokens = process.server ? getTokenFromCookie(req) : getTokenFromLocalStorage();
-  if (tokens.jwt && tokens.profileId) {
+  if (tokens && tokens.jwt && tokens.profileId) {
     await store.dispatch('user/setToken', tokens);
     return true;
   }
