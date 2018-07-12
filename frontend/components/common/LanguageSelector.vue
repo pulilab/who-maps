a2ff1894c72f7b576b2b745c16cacea1ef0b9ef5
lordfuoco <lordfuoco@gmail.com>
2018-07-12 09:26:57 +0200

    chore: minimal flex / layout for topbar

--- frontend/components/common/LanguageSelector.vue
+++ frontend/components/common/LanguageSelector.vue
@@ -1,12 +1,15 @@
 <template>
-  <div class="LanguageSelector">
+  <ediv
+    class="LanguageSelector"
+  >
     <div
       v-for="locale in $i18n.locales"
-      :key="locale.code">
-
+      :key="locale.code"
+      class="LanguageLink"
+    >
       <nuxt-link :to="switchLocalePath(locale.code)">{{ locale.name }}</nuxt-link>
     </div>
-  </div>
+  </ediv>
 </template>
 
 <script>
@@ -15,6 +18,20 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
+
+.LanguageSelector {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+
+  .LanguageLink {
+    margin: 0 4px;
+
+    .nuxt-link-exact-active {
+      color: red;
+    }
+  }
+}
 
 </style>
--- frontend/components/common/TopBar.vue
+++ frontend/components/common/TopBar.vue
@@ -2,9 +2,13 @@
   <div class="TopBar">
     <language-selector />
     <div class="separator" />
-    <div>
-      <nuxt-link :to="localePath('index-signup')">Signup</nuxt-link>
-      <nuxt-link :to="localePath('index-login')">Login</nuxt-link>
+    <div class="AuthLinks">
+      <div>
+        <nuxt-link :to="localePath('index-signup')">Signup</nuxt-link>
+      </div>
+      <div>
+        <nuxt-link :to="localePath('index-login')">Login</nuxt-link>
+      </div>
     </div>
   </div>
 </template>
@@ -18,6 +22,27 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
+  .TopBar {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
 
+    .separator {
+      width: 1px;
+      height: 18px;
+      background-color: gray;
+      margin: 0 4px;
+    }
+
+    .AuthLinks {
+      display: flex;
+      justify-content: flex-end;
+      align-items: center;
+
+      div {
+        margin: 0 4px;
+      }
+    }
+  }
 </style>
