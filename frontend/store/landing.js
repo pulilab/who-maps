cde04c1fe11cfa7cd8b18d5e1d989b4326a27b15
lordfuoco <lordfuoco@gmail.com>
2018-07-12 16:35:00 +0200

    feat: add the geojson library, getters, action to load the library and handle the selected countries

--- frontend/store/landing.js
+++ frontend/store/landing.js
@@ -1,10 +1,27 @@
 export const state = () => ({
   countries: [],
-  projects: []
+  projects: [],
+  geoJsonLibrary: {},
+  selectedCountries: []
 });
 
 export const getters = {
-
+  getCountries (state) {
+    return [...state.countries.map(c => ({...c}))];
+  },
+  getLandingPagePins (state, getters) {
+    const polyLabeled = getters.getCountries.filter(c => c.map_data.polylabel);
+    return polyLabeled.map(c => ({
+      id: c.id,
+      latlng: {...c.map_data.polylabel}
+    }));
+  },
+  getGeoJsonLibrary (state) {
+    return state.geoJsonLibrary;
+  },
+  getSelectedCountries (state) {
+    return state.selectedCountries;
+  }
 };
 export const actions = {
   async loadMapData ({commit}) {
@@ -14,6 +31,23 @@ export const actions = {
   async loadPublicProjectList ({commit}) {
     const { data } = await this.$axios.get('/api/projects/map/');
     commit('SET_PROJECT_LIST', data);
+  },
+  async toggleCountry ({commit, dispatch, getters}, id) {
+    const index = getters.getSelectedCountries.indexOf(id);
+    if (index === -1) {
+      await dispatch('loadGeoJSON', id);
+      commit('ADD_SELECTED_COUNTRY', id);
+    } else {
+      commit('RM_SELECTED_COUNTRY', index);
+    }
+  },
+  async loadGeoJSON ({commit, getters}, id) {
+    if (!getters.getGeoJsonLibrary[id]) {
+      const country = getters.getCountries.find(c => c.id === id);
+      const { data } = await this.$axios.get(`/static/country-geodata/${country.code.toLowerCase()}.json?version=${country.map_version}`);
+      Object.freeze(data);
+      commit('UPDATE_JSON_LIBRARY', {id, data});
+    }
   }
 };
 export const mutations = {
@@ -22,5 +56,14 @@ export const mutations = {
   },
   SET_PROJECT_LIST: (state, list) => {
     state.projects = list;
+  },
+  ADD_SELECTED_COUNTRY: (state, c) => {
+    state.selectedCountries.push(c);
+  },
+  UPDATE_JSON_LIBRARY: (state, {id, data}) => {
+    state.geoJsonLibrary[id] = data;
+  },
+  RM_SELECTED_COUNTRY: (state, index) => {
+    state.selectedCountries.splice(index, 1);
   }
 };
