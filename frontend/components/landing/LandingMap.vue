58a5d2f8f7c984cfd1ef8deba94805c1b871dd5f
lordfuoco <lordfuoco@gmail.com>
2018-07-12 13:24:38 +0200

    feat: landing page map component basic

--- frontend/components/landing/LandingMap.vue
+++ frontend/components/landing/LandingMap.vue
@@ -1,10 +1,65 @@
 <template>
-  <div class="LandingMap" />
+  <div class="LandingMap">
+    <no-ssr>
+      <l-map
+        ref="mainMap"
+        :zoom="zoom"
+        :world-copy-jump="true"
+        :options="mapOptions"
+      >
+        <l-tilelayer
+          url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}'"
+          @loading="mapReady"
+        />
+
+        <v-marker-cluster
+          ref="markerCluster"
+          :options="clusterOptions"
+        >
+          <map-marker
+            v-for="pin in pins"
+            :key="pin.id"
+            :pin="pin"
+          />
+        </v-marker-cluster>
+
+        <l-control-zoom
+          position="bottomright"
+        />
+      </l-map>
+    </no-ssr>
+  </div>
 </template>
 
 <script>
+import NoSSR from 'vue-no-ssr';
 export default {
-
+  components: {
+    'no-ssr': NoSSR
+  },
+  data () {
+    return {
+      zoom: 3,
+      mapOptions: { zoomControl: false, attributionControl: false }
+    };
+  },
+  computed: {
+    clusterOptions () {
+      return {
+        disableClusteringAtZoom: 8,
+        spiderfyOnMaxZoom: false,
+        polygonOptions: {
+          stroke: false,
+          fillColor: '#42B883'
+        }
+      };
+    }
+  },
+  methods: {
+    mapReady () {
+      console.log('map ready');
+    }
+  }
 };
 </script>
 
