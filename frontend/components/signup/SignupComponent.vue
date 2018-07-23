f60ed579ceb6447bb11344fb9b35bb0aa9eae4ff
András T. Takács <maurjin@gmail.com>
2018-07-23 17:50:53 +0200

    Signup mixin made, Zolis initial selection todo on signup page solved

--- frontend/components/signup/SignupComponent.vue
+++ frontend/components/signup/SignupComponent.vue
@@ -11,7 +11,6 @@
         <fieldset class="Type">
           <div class="Legend">1. Select your role</div>
           <!-- TODO -->
-          <!-- Add class '.active' one of these initially -->
           <div
             :class="['AccountTypeSelector', {'active': accountType === 'I', 'inactive': accountType && accountType !== 'I'}]"
             @click="accountType = 'I'">
@@ -126,50 +125,15 @@
 </template>
 
 <script>
-import { mapActions } from 'vuex';
+import SignupMixin from '../mixins/SignupMixin.js';
 
 export default {
+  mixins: [SignupMixin],
 
   data () {
     return {
-      showSuccess: false,
-      accountType: '',
-      signupForm: {
-        email: '',
-        password: '',
-        password2: ''
-      }
+      accountType: 'I'
     };
-  },
-
-  computed: {
-    inputsFilledOkay () {
-      // eslint-disable-next-line
-      const validatorRegex = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-      const validEmail = validatorRegex.test(this.signupForm.email);
-      const matchingPasswords = Boolean(this.signupForm.password === this.signupForm.password2);
-
-      return this.accountType && this.signupForm.password && validEmail && matchingPasswords;
-    }
-  },
-
-  methods: {
-    ...mapActions({
-      'doSignup': 'user/doSignup'
-    }),
-
-    async signup () {
-      await this.doSignup({
-        account_type: this.accountType,
-        password1: this.signupForm.password,
-        password2: this.signupForm.password2,
-        email: this.signupForm.email
-      });
-      this.showSuccess = true;
-      setTimeout(() => {
-        this.$router.push(this.localePath('index-edit-profile'));
-      }, 5000);
-    }
   }
 };
 </script>
