f69944998536466f7133e23b3cec0eef2124e29b
András T. Takács <maurjin@gmail.com>
2018-07-26 15:09:03 +0200

    User type selector

--- frontend/components/landing/SignupBox.vue
+++ frontend/components/landing/SignupBox.vue
@@ -12,11 +12,76 @@
         </h6>
       </el-col>
     </el-row>
+
+    <!-- User type selector -->
+    <div v-if="!showForm">
+      <el-row
+        type="flex"
+        align="start"
+        justify="space-between"
+        class="AccountTypeSelectors">
+        <el-col class="mright">
+          <div
+            :class="['AccountTypeSelector', {'active': accountType === 'I', 'inactive': accountType && accountType !== 'I'}]"
+            @click="accountType = 'I'">
+            <div class="Introduction">I'm an <br><span>"Implementer"</span></div>
+            <div class="TypeDetails">How can I better scale-up my implementation? Are there tips and resources that I should consider to improve my implementation? Sign up to complete the digital version of the MAPS toolkit and track the performance of your implementation.</div>
+          </div>
+        </el-col>
+        <el-col class="mright">
+          <div
+            :class="['AccountTypeSelector', {'active': accountType === 'D', 'inactive': accountType && accountType !== 'D'}]"
+            @click="accountType = 'D'">
+            <div class="Introduction">I'm a <br><span>"Financial Investor"</span></div>
+            <div class="TypeDetails">What are the different projects within your portfolio? Sign up to access a visual dashboard displaying the performance metrics of projects within your portfolio.</div>
+          </div>
+        </el-col>
+        <el-col>
+          <div
+            :class="['AccountTypeSelector', {'active': accountType === 'G', 'inactive': accountType && accountType !== 'G'}]"
+            @click="accountType = 'G'">
+            <div class="Introduction">I work within <br><span>"Government"</span></div>
+            <div class="TypeDetails">Who is implementing digital health interventions in your country? Sign up to access interactive maps and performance metrics on the different implementation in your country.</div>
+          </div>
+        </el-col>
+      </el-row>
+
+      <el-row
+        type="flex"
+        align="start"
+        justify="space-between"
+        class="NextStepHolder">
+
+        <el-col class="mright"/>
+        <el-col class="mright">
+          <el-button
+            :disabled="!accountType"
+            class="NextStepBtn"
+            @click="showForm = true">Next step</el-button>
+        </el-col>
+        <el-col/>
+
+      </el-row>
+    </div>
+
+    <!-- Form -->
+    <div v-if="showForm">
+      FORM
+    </div>
   </div>
 </template>
 
 <script>
+import SignupMixin from '../mixins/SignupMixin.js';
+
 export default {
+  mixins: [SignupMixin],
+
+  data () {
+    return {
+      showForm: false
+    };
+  }
 
 };
 </script>
@@ -58,6 +123,34 @@ export default {
         color: @colorTextPrimary;
         margin: 0;
       }
+    }
+
+    .AccountTypeSelectors {
+      padding: 40px 30px 30px;
+
+      .AccountTypeSelector {
+        background: white;
+        height: 300px;
+
+        &.active {
+          transform: scale(1.05);
+        }
+        &.inactive {
+          opacity: 0.8;
+        }
+      }
+    }
+
+    .NextStepHolder {
+      padding: 0 30px;
+
+      .NextStepBtn {
+        width: 100%;
+      }
+    }
+
+    .mright {
+      margin-right: 20px;
     }
   }
 </style>
