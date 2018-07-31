71bd64bce6d48afacaeceb073b732d8433aac647
lordfuoco <lordfuoco@gmail.com>
2018-07-31 16:01:39 +0200

    feat: page hierarchy and api fetching for projects / assesment

---
+++ frontend/pages/index/projects/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="MyProjects">
+    <h3> My Projects </h3>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  components: {
+  },
+  computed: {
+    ...mapGetters({
+    })
+  }
+};
+</script>
+
+<style lang="less">
+</style>
