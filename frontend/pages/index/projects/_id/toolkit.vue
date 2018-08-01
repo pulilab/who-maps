2dbad54f966679af464ffe2afab3af37c3da7fa1
lordfuoco <lordfuoco@gmail.com>
2018-08-01 16:28:26 +0200

    feat: basic pages to provide the routing for the toolkit section

---
+++ frontend/pages/index/projects/_id/toolkit.vue
@@ -0,0 +1,23 @@
+<template>
+
+  <div id="toolkitjs"/>
+</template>
+
+<script>
+
+export default {
+  components: {
+  },
+  async fetch ({store}) {
+    await store.dispatch('projects/loadUserProjects');
+  },
+  mounted () {
+    const mapsToolkitFactory = require('../../../../angular/MapsToolkit/mapsToolkitFactory');
+    mapsToolkitFactory.factory();
+  }
+};
+</script>
+
+<style>
+
+</style>
