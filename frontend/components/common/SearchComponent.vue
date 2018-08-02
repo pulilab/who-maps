49d97f36626167f6e5b014ba2623ff6ef8e01f65
lordfuoco <lordfuoco@gmail.com>
2018-08-02 14:22:35 +0200

    feat: search component mocked up

--- frontend/components/common/SearchComponent.vue
+++ frontend/components/common/SearchComponent.vue
@@ -1,12 +1,97 @@
 <template>
   <div class="SearchDropdown">
-    <i class="el-icon-search bold" />
+    <el-popover
+      :visible-arrow="false"
+      v-model="shown"
+      placement="left"
+      popper-class="SearchPopper"
+      width="400"
+      trigger="click"
+    >
+      <el-row type="flex">
+        <el-col :span="24">
+          <el-input
+            v-model="searchString"
+            placeholder="Type something">
+
+            <i
+              slot="prepend"
+              class="el-icon-search"
+            />
+            <template slot="append">
+              <el-button
+                icon="el-icon-close"
+                @click="hideSearch"
+              />
+              <el-button
+                icon="el-icon-arrow-right"
+                @click="search"
+              />
+            </template>
+          </el-input>
+        </el-col>
+      </el-row>
+      <el-row type="flex">
+        <el-col v-show="hasResults">
+          {{ results.length }} results
+        </el-col>
+        <el-col>
+          <nuxt-link to="dashboard"> Advanced search > </nuxt-link>
+        </el-col>
+      </el-row>
+
+      <el-row v-show="!hasResults">
+        <el-col >
+          <p class="TipText">
+            Short tip text about advenacd seacrh lorem ipsum dolor sit ametncidunt ut labore et dolore magna aliqua.
+          </p>
+        </el-col>
+      </el-row>
+      <el-row
+        v-for="project in results"
+        v-show="hasResults"
+        :key="project"
+      >
+        <el-col>
+          <project-card />
+        </el-col>
+      </el-row>
+      <el-button
+        slot="reference"
+        icon="el-icon-search"
+      />
+    </el-popover>
   </div>
 </template>
 
 <script>
-export default {
+import ProjectCard from './ProjectCard';
 
+export default {
+  components: {
+    ProjectCard
+  },
+  data () {
+    return {
+      shown: false,
+      searchString: null,
+      results: []
+    };
+  },
+  computed: {
+    hasResults () {
+      return this.results.length > 0;
+    }
+  },
+  methods: {
+    hideSearch () {
+      this.shown = false;
+      this.results = [];
+    },
+    search () {
+      this.results = [1, 2, 3];
+    }
+  }
 };
 </script>
 
@@ -21,5 +106,8 @@ export default {
       font-size: 24px;
       color: @colorWhite;
     }
+  }
+  .SearchPopper {
+    transform: translate(75px, 50px);
   }
 </style>
