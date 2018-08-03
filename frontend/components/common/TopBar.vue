f56fd0dbe23b7495f29438738ab724ffae9a72f8
lordfuoco <lordfuoco@gmail.com>
2018-08-03 15:28:13 +0200

    feat: top bar responds to user logged in state, added proper links

--- frontend/components/common/TopBar.vue
+++ frontend/components/common/TopBar.vue
@@ -9,16 +9,19 @@
     >
 
       <el-col class="LogoHolder">
-        <div>
+        <nuxt-link :to="localePath('index')">
           <img
             :src="countrySpecific ? countryLogoURL : '/logo-who-blue.svg'"
             :alt="countrySpecific ? 'Country logo' : 'WHO logo'"
             class="Logo">
-        </div>
+        </nuxt-link>
       </el-col>
 
       <el-col class="RightPart">
+        <!-- ANON MODE -->
         <el-row
+          v-if="!user"
+          class="AnonView"
           type="flex"
           justify="end">
           <el-col
@@ -40,13 +43,13 @@
             <div>
               <nuxt-link
                 :to="localePath('index-signup')"
-                class="HeaderBtn">Signup</nuxt-link>
+                class="HeaderBtn HideOnActive">Signup</nuxt-link>
             </div>
 
             <div>
               <nuxt-link
                 :to="localePath('index-login')"
-                class="HeaderBtn">Login</nuxt-link>
+                class="HeaderBtn HideOnActive">Login</nuxt-link>
             </div>
           </el-col>
 
@@ -62,21 +65,75 @@
             </div>
           </el-col>
         </el-row>
+        <!-- LOGGED IN MODE -->
+        <el-row
+          v-if="user"
+          class="LoggedInView"
+          type="flex"
+          justify="end">
+          <el-col class="AuthLinks">
+            <div>
+              <nuxt-link
+                :to="localePath('index-dashboard')"
+                class="HeaderBtn"
+              >
+                Dashboard
+              </nuxt-link>
+            </div>
+            <div>
+              <nuxt-link
+                :to="localePath('index-projects')"
+                class="HeaderBtn"
+              >
+                My Projects
+              </nuxt-link>
+            </div>
+            <div>
+              <nuxt-link
+                :to="localePath('index-cms')"
+                class="HeaderBtn"
+              >
+                Planning and Guidance
+              </nuxt-link>
+            </div>
+            <div>
+              <el-button
+                type="text"
+                class="HeaderBtn"
+              >
+                Toolkit
+              </el-button>
+            </div>
+            <div>
+              <nuxt-link
+                :to="localePath('index-projects-create')"
+                class="HeaderBtn">
+                <i class="el-icon-plus" />
+                New Project
+              </nuxt-link>
+            </div>
+            <user-dropdown class="HeaderBtn" />
+          </el-col>
+        </el-row>
       </el-col>
     </el-row>
   </div>
 </template>
 
 <script>
-import LanguageSelector from './LanguageSelector';
 import VueScrollClass from 'vue-scroll-class';
+import { mapGetters } from 'vuex';
+
+import LanguageSelector from './LanguageSelector';
+import UserDropdown from './UserDropdown';
 
 export default {
   directives: {
     'scroll-class': VueScrollClass
   },
   components: {
-    LanguageSelector
+    LanguageSelector,
+    UserDropdown
   },
   props: {
     countrySpecific: {
@@ -87,6 +144,11 @@ export default {
       type: String,
       default: '/mock/placeholder-sl.png'
     }
+  },
+  computed: {
+    ...mapGetters({
+      user: 'user/getProfile'
+    })
   }
 };
 </script>
@@ -118,6 +180,12 @@ export default {
 
       > .el-row > .el-col {
         width: auto;
+      }
+    }
+
+    .HideOnActive {
+      &.nuxt-link-active {
+        display: none;
       }
     }
 
