9e08883046c86a65a32d46992f1467af43c8be02
lordfuoco <lordfuoco@gmail.com>
2018-08-03 15:27:53 +0200

    feat: basic country and language item components

---
+++ frontend/components/common/CountryItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="CountryItem">
+    <country-flag
+      :code="country.code"
+      small />
+    {{ country.name }}
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+import CountryFlag from './CountryFlag';
+export default {
+  components: {
+    CountryFlag
+  },
+  props: {
+    id: {
+      type: Number,
+      required: true
+    }
+  },
+  computed: {
+    ...mapGetters({
+      getCountryDetails: 'countries/getCountryDetails'
+    }),
+    country () {
+      return this.getCountryDetails(this.id);
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
