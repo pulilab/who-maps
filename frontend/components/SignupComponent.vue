3f7ed16b7f1664a8b97b4ed058613652069f3a81
lordfuoco <lordfuoco@gmail.com>
2018-08-03 15:26:18 +0200

    feat: align signup to new requirements validate on blur submit on enter

---
+++ frontend/components/SignupComponent.vue
@@ -0,0 +1,164 @@
+<template>
+  <div class="SingupComponent">
+    <el-card :shadow="shadow">
+      <div class="Header">
+        Sign up for Digital Health Atlas
+      </div>
+      <div class="Legend">Fill out the form below</div>
+      <el-form
+        ref="signupForm"
+        :model="signupForm"
+        :rules="rules"
+        @submit.native.prevent="signup"
+      >
+
+        <el-form-item
+          label="Email address"
+          prop="email">
+          <el-input
+            v-model="signupForm.email"
+            type="email" />
+        </el-form-item>
+
+        <el-form-item
+          label="Password"
+          prop="password1">
+          <el-input
+            v-model="signupForm.password1"
+            type="password" />
+        </el-form-item>
+
+        <el-form-item
+          label="Password (Again)"
+          prop="password2">
+          <el-input
+            v-model="signupForm.password2"
+            type="password" />
+          <div
+            v-if="nonFieldErrors"
+            class="el-form-item__error ModifiedFormError">{{ nonFieldErrors }}
+          </div>
+        </el-form-item>
+        <el-row
+          type="flex"
+          align="middle"
+          class="CardActions">
+          <el-col
+            :span="6"
+            class="SecondaryAction LoginLink">
+            <h6>Already signed up?</h6>
+            <nuxt-link :to="localePath('index-login')">
+              Login here
+            </nuxt-link>
+          </el-col>
+          <el-col
+            :span="6"
+            class="PrimaryAction">
+            <el-button
+              type="primary"
+              size="medium"
+              native-type="submit">
+              Sign up now
+            </el-button>
+          </el-col>
+        </el-row>
+      </el-form>
+    </el-card>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+import FormAPIErrorsMixin from './mixins/FormAPIErrorsMixin';
+
+export default {
+  mixins: [FormAPIErrorsMixin],
+  props: {
+    shadow: {
+      type: String,
+      default: 'always'
+    }
+  },
+  data () {
+    return {
+      signupForm: {
+        email: '',
+        password1: '',
+        password2: ''
+      },
+      rules: {
+        email: [
+          { required: true, message: 'This field is required', trigger: 'blur' },
+          { type: 'email', message: 'Has to be a valid email address', trigger: 'blur' },
+          { validator: this.validatorGenerator('email'), trigger: 'blur' }
+        ],
+        password1: [
+          { required: true, message: 'This field is required', trigger: 'blur' },
+          { min: 8, message: 'This field should be at least 8 characters', trigger: 'blur' },
+          { validator: this.validatorGenerator('password1'), trigger: 'blur' }
+        ],
+        password2: [
+          { required: true, message: 'This field is required', trigger: 'blur' },
+          { validator: this.passwordMatching, trigger: 'blur' }
+        ]
+      }
+    };
+  },
+  methods: {
+    ...mapActions({
+      'doSignup': 'user/doSignup'
+    }),
+    passwordMatching (rule, value, callback) {
+      value === this.signupForm.password1 ? callback() : callback(Error('The password must match'));
+    },
+    async signup () {
+      console.log('signup');
+      this.deleteFormAPIErrors();
+      try {
+        await this.doSignup({
+          // TODO remove this when backend is ready
+          account_type: 'I',
+          password1: this.signupForm.password1,
+          password2: this.signupForm.password2,
+          email: this.signupForm.email
+        });
+        this.showSuccess = true;
+        setTimeout(() => {
+          this.$router.push(this.localePath('index-edit-profile'));
+        }, 5000);
+      } catch (e) {
+        this.setFormAPIErrors(e);
+        this.$refs.signupForm.validate(() => {});
+      }
+    }
+  }
+};
+</script>
+
+<style lang="less">
+  @import "../assets/style/variables.less";
+  @import "../assets/style/mixins.less";
+
+  .SingupComponent {
+    width: @cardSizeSmall;
+    margin: 0 auto;
+
+    .CardActions {
+      justify-content: space-between;
+      .SecondaryAction {
+        h6 {
+          margin: 0 0 2px;
+          font-size: @fontSizeSmall;
+          font-weight: 400;
+          color: @colorTextSecondary;
+        }
+
+        a {
+          font-size: @fontSizeSmall;
+          font-weight: 700;
+          color: @colorBrandPrimary;
+        }
+      }
+    }
+  }
+</style>
--- frontend/components/landing/SignupBox.vue
+++ frontend/components/landing/SignupBox.vue
@@ -12,143 +12,20 @@
         </h6>
       </el-col>
     </el-row>
-
-    <!-- User type selector -->
-    <div v-if="!showForm">
-      <el-row
-        type="flex"
-        align="start"
-        justify="space-between"
-        class="AccountTypeSelectors">
-        <el-col class="mright">
-          <div
-            :class="['AccountTypeSelector', {'active': accountType === 'I', 'inactive': accountType && accountType !== 'I'}]"
-            @click="accountType = 'I'">
-            <span class="IconRole">
-              <img src="/icon-role-implementer.svg">
-            </span>
-            <div class="Introduction">I'm an <br><span>"Implementer"</span></div>
-            <div class="TypeDetails">How can I better scale-up my implementation? Are there tips and resources that I should consider to improve my implementation? Sign up to complete the digital version of the MAPS toolkit and track the performance of your implementation.</div>
-          </div>
-        </el-col>
-        <el-col class="mright">
-          <div
-            :class="['AccountTypeSelector', {'active': accountType === 'D', 'inactive': accountType && accountType !== 'D'}]"
-            @click="accountType = 'D'">
-            <span class="IconRole">
-              <img src="/icon-role-investor.svg">
-            </span>
-            <div class="Introduction">I'm a <br><span>"Financial Investor"</span></div>
-            <div class="TypeDetails">What are the different projects within your portfolio? Sign up to access a visual dashboard displaying the performance metrics of projects within your portfolio.</div>
-          </div>
-        </el-col>
-        <el-col>
-          <div
-            :class="['AccountTypeSelector', {'active': accountType === 'G', 'inactive': accountType && accountType !== 'G'}]"
-            @click="accountType = 'G'">
-            <span class="IconRole">
-              <img src="/icon-role-government.svg">
-            </span>
-            <div class="Introduction">I work within <br><span>"Government"</span></div>
-            <div class="TypeDetails">Who is implementing digital health interventions in your country? Sign up to access interactive maps and performance metrics on the different implementation in your country.</div>
-          </div>
-        </el-col>
-      </el-row>
-
-      <el-row
-        type="flex"
-        align="start"
-        justify="space-between"
-        class="NextStepHolder">
-
-        <el-col class="mright"/>
-        <el-col class="mright">
-          <el-button
-            :disabled="!accountType"
-            type="primary"
-            size="medium"
-            class="NextStepBtn"
-            @click="showForm = true">Next step</el-button>
-        </el-col>
-        <el-col/>
-
-      </el-row>
-    </div>
-
-    <!-- Form -->
-    <div v-if="showForm && !showSuccess">
-      <el-form
-        ref="signupForm"
-        :model="signupForm"
-        :rules="rules"
-        @submit.native.prevent>
-
-        <el-form-item
-          label="Email address"
-          prop="email">
-          <el-input
-            v-model="signupForm.email"
-            type="email" />
-        </el-form-item>
-
-        <el-form-item
-          label="Password"
-          prop="password1">
-          <el-input
-            v-model="signupForm.password1"
-            type="password" />
-        </el-form-item>
-
-        <el-form-item
-          label="Password (Again)"
-          prop="password2">
-          <el-input
-            v-model="signupForm.password2"
-            type="password" />
-          <div
-            v-if="nonFieldErrors"
-            class="el-form-item__error ModifiedFormError">{{ nonFieldErrors }}
-          </div>
-        </el-form-item>
-      </el-form>
-
-      <el-row
-        type="flex"
-        align="start"
-        justify="space-between"
-        class="NextStepHolder">
-
-        <el-col class="mright"/>
-        <el-col class="mright">
-          <el-button
-            type="primary"
-            size="medium"
-            @click="signup">
-            Sign up now
-          </el-button>
-        </el-col>
-        <el-col/>
-
-      </el-row>
-    </div>
-
-    <div v-if="showSuccess">
-      <h4>Go to next step</h4>
-      <p>Your registration is successful, you will receive an email with the instructions to activate your account, you will be automatically logged in in 5 seconds...</p>
-    </div>
+    <el-row>
+      <el-col>
+        <signup-component shadow="never"/>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
-import SignupMixin from '../mixins/SignupMixin.js';
+import SignupComponent from '../SignupComponent';
 
 export default {
-  mixins: [SignupMixin],
-
-  data () {
-    return {
-      showForm: false
-    };
+  components: {
+    SignupComponent
   }
 };
 </script>
@@ -191,33 +68,29 @@ export default {
         margin: 0;
       }
     }
+    .SingupComponent {
+      .Header {
+        display: none;
+      }
 
-    .AccountTypeSelectors {
-      padding: 40px 30px 30px;
+      .Legend {
+        display: none;
+      }
 
-      .AccountTypeSelector {
-        background: white;
-        height: 300px;
-
-        &.active {
-          transform: scale(1.05);
-        }
-        &.inactive {
-          opacity: 0.8;
+      .LoginLink {
+        display: none;
+      }
+      .el-card {
+        border: 0;
+        border-radius: 0;
+        box-shadow: none !important;
+        .el-card__body {
+          background-color: @colorBrandPrimary;
         }
       }
-    }
-
-    .NextStepHolder {
-      padding: 0 30px;
-
-      .NextStepBtn {
-        width: 100%;
+      .CardActions {
+        justify-content: space-around;
       }
-    }
-
-    .mright {
-      margin-right: 20px;
     }
   }
 </style>
