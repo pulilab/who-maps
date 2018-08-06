50bc22199e06356787d753e656ec34dc7cd0730e
lordfuoco <lordfuoco@gmail.com>
2018-08-06 13:24:32 +0200

    fix: remove duplicated bars

--- frontend/pages/index/admin.vue
+++ frontend/pages/index/admin.vue
@@ -1,7 +1,5 @@
 <template>
   <div>
-    <top-bar />
-    <action-bar />
     <div :class="['Narrower', {'ForProfile': $route.path.split('/')[3] === 'edit-profile'} ]">
       <nuxt-child />
     </div>
@@ -11,13 +9,9 @@
 <script>
 // NOTE: This is only a router view wrapper, for common fetches
 import { mapGetters } from 'vuex';
-import TopBar from '../../components/common/TopBar.vue';
-import ActionBar from '../../components/common/ActionBar.vue';
 
 export default {
   components: {
-    TopBar,
-    ActionBar
   },
   middleware: ['isLoggedIn'],
   computed: {
