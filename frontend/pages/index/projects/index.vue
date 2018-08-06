83cc46cff089d4182bf09e4a4e16d7fda5a7ce47
lordfuoco <lordfuoco@gmail.com>
2018-08-06 13:41:00 +0200

    feat: enable the user project list in the my project page

--- frontend/pages/index/projects/index.vue
+++ frontend/pages/index/projects/index.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="MyProjects">
     <h3> My Projects </h3>
+    <user-project-list />
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
 
+import UserProjectList from '../../../components/common/UserProjectsList';
+
 export default {
   components: {
+    UserProjectList
   },
   computed: {
     ...mapGetters({
