4fb275e1117323ef0e5f35bb2aa4031a1224d60c
Gyorgy Fekete <f@pulilab.com>
2018-08-06 17:19:10 +0200

    search view list proto

--- django/search/views.py
+++ django/search/views.py
@@ -23,3 +23,14 @@ class SearchView(generics.ListAPIView):
     serializer_class = SearchSerializer
     filter_backends = (filters.OrderingFilter,)
     ordering_fields = ('project_name', 'contact_name')
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
