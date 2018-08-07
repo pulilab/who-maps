221ffcc1ebd56af332ecf22b64272aa0497b222a
lordfuoco <lordfuoco@gmail.com>
2018-08-07 16:25:47 +0200

    feat: organisation select component

---
+++ frontend/components/common/OrganisationSelect.vue
@@ -0,0 +1,61 @@
+<template>
+  <el-select
+    :value="value"
+    :allow-create="true"
+    filterable
+    reserve-keyword
+    class="Organisationelector"
+    popper-class="OrganisationelectorDropdown"
+    placeholder="Type and select a name"
+    @change="changeHandler"
+  >
+    <el-option
+      v-for="organisation in organisations"
+      :key="organisation.id"
+      :label="organisation.name"
+      :value="organisation.id"
+    />
+  </el-select>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+import OrganisationItem from '../common/OrganisationItem';
+
+export default {
+  components: {
+    OrganisationItem
+  },
+  model: {
+    prop: 'value',
+    event: 'change'
+  },
+  props: {
+    value: {
+      type: [String, Number],
+      default: null
+    }
+  },
+  computed: {
+    ...mapGetters({
+      organisations: 'system/getOrganisations'
+    })
+  },
+  methods: {
+    changeHandler (value) {
+      this.$emit('change', value);
+    }
+  }
+};
+</script>
+
+<style lang="less">
+.Organisationelector {
+  width: 100%;
+}
+.OrganisationelectorDropdown {
+   .OrganisationItem {
+    display: inline;
+  }
+}
+</style>
