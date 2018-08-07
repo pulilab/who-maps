0d4c1f25cce843eb5a8f3b1bf60018bcc45af964
Gyorgy Fekete <f@pulilab.com>
2018-08-07 17:39:20 +0200

    prepare new search view for queryset filtering

--- django/search/views.py
+++ django/search/views.py
@@ -5,17 +5,19 @@ from .models import ProjectSearch
 
 class SearchView(generics.ListAPIView):
     queryset = ProjectSearch.objects.all()
-    serializer_class = SearchSerializer
     filter_backends = (filters.OrderingFilter,)
     ordering_fields = ('project_name', 'contact_name')
 
     def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset())
+        queryset = ProjectSearch.search(self.get_queryset())
+
+        queryset = self.filter_queryset(queryset)
 
         page = self.paginate_queryset(queryset)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
 
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
+        data = {}  # TODO: make this
+
+        if page is not None:
+            return self.get_paginated_response(data)
+
+        return Response(data)
