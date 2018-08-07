9ce1efad9beee64bd75d4edf0f3fdfad42e2b12f
lordfuoco <lordfuoco@gmail.com>
2018-08-07 10:34:00 +0200

    fix: added placeholder item to avoid a flash of the button

--- frontend/components/common/ToolkitDialogWrapper.vue
+++ frontend/components/common/ToolkitDialogWrapper.vue
@@ -1,12 +1,27 @@
 <template>
-  <div id="thematicjs"/>
+  <div>
+    <div id="thematicjs"/>
+    <!-- Following A is just a placeholder to avoid a flash of content -->
+    <a
+      v-if="!initialised"
+      class="HeaderBtn"
+      href="#">
+      Toolkit
+    </a>
+  </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      initialised: false
+    };
+  },
   mounted () {
     const factory = require('../../angular/Thematic/thematicFactory');
-    factory.factory(0, 0, 'MAPS Toolkit', 'md-primary', 'Toolkit');
+    factory.factory(0, 0, 'MAPS Toolkit', 'HeaderBtn', 'Toolkit');
+    this.initialised = true;
   }
 };
 </script>
