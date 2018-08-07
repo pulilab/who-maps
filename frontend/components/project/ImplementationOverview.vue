ca1310ae27160ebd4b3a6d55847546488da236db
lordfuoco <lordfuoco@gmail.com>
2018-08-07 16:26:24 +0200

    feat: basic components for project view

---
+++ frontend/components/project/ImplementationOverview.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    id="implementation"
+    class="ImplementationOverview">
+    <collapsible-card title="Implementation Overview">
+      ImplementationOverview
+    </collapsible-card>
+  </div>
+</template>
+
+<script>
+import CollapsibleCard from './CollapsibleCard';
+
+export default {
+  components: {
+    CollapsibleCard
+  }
+};
+</script>
+
+<style>
+
+</style>
---
+++ frontend/components/project/TechnologyOverview.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    id="technology"
+    class="TechnologyOverview">
+    <collapsible-card title="Thecnology Overview">
+      TechnologyOverview
+    </collapsible-card>
+  </div>
+</template>
+
+<script>
+import CollapsibleCard from './CollapsibleCard';
+
+export default {
+  components: {
+    CollapsibleCard
+  }
+};
+</script>
+
+<style>
+
+</style>
