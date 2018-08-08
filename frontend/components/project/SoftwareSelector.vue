13b3f5e982483bde359388bdff88bedb1a6c1879
lordfuoco <lordfuoco@gmail.com>
2018-08-08 15:58:31 +0200

    feat: Software selector component

---
+++ frontend/components/project/SoftwareSelector.vue
@@ -0,0 +1,50 @@
+<template>
+  <el-select
+    :value="value"
+    popper-class="SoftwareSelectorDropdown"
+    class="SoftwareSelector"
+    placeholder="Select from list"
+    @change="changeHandler">
+
+    <el-option
+      v-for="paltform in technologyPlatforms"
+      :key="paltform.id"
+      :label="paltform.name"
+      :value="paltform.id"/>
+  </el-select>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+export default {
+  model: {
+    prop: 'value',
+    event: 'change'
+  },
+  props: {
+    value: {
+      type: Number,
+      default: null
+    }
+  },
+  computed: {
+    ...mapGetters({
+      technologyPlatforms: 'projects/getTechnologyPlatforms'
+    })
+  },
+  methods: {
+    changeHandler (value) {
+      this.$emit('change', value);
+    }
+  }
+};
+</script>
+
+<style lang="less">
+.SoftwareSelector {
+  width: 50%;
+}
+.SoftwareSelectorDropdown {
+
+}
+</style>
