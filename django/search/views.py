5f2cd7f8f2a72714d21808efd03c4fba54256e23
Gyorgy Fekete <f@pulilab.com>
2018-08-08 17:27:51 +0200

    refactor searchview into a searchviewset

--- django/search/views.py
+++ django/search/views.py
@@ -1,14 +1,16 @@
-from rest_framework import generics, filters
+from rest_framework import filters
 from rest_framework.response import Response
+from rest_framework.viewsets import GenericViewSet
+
 from .models import ProjectSearch
 
 
-class SearchView(generics.ListAPIView):
+class SearchViewSet(GenericViewSet):
     queryset = ProjectSearch.objects.all()
-    filter_backends = (filters.OrderingFilter,)
-    ordering_fields = ('project_name', 'contact_name')
+    # filter_backends = (filters.OrderingFilter,)
+    # ordering_fields = ('project_name', 'contact_name')
 
-    def list(self, request, *args, **kwargs):
+    def get(self, request, *args, **kwargs):
         queryset = ProjectSearch.search(self.get_queryset())
 
         queryset = self.filter_queryset(queryset)
