7b35b91318c9b5481e6f97450dceaef158ed63f0
Gyorgy Fekete <f@pulilab.com>
2018-08-08 11:12:07 +0200

    comment out the old search url

--- django/search/urls.py
+++ django/search/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r"^search/projects/$", view=views.search_project, name="search-project"),
+    # url(r"^search/projects/$", view=views.search_project, name="search-project"),
 ]
