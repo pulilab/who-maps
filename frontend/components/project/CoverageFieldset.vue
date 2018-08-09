6d43b7d0d3a67d7eb37182fa408ba101e8483312
lordfuoco <lordfuoco@gmail.com>
2018-08-09 16:29:07 +0200

    feat: coverage fieldset

---
+++ frontend/components/project/CoverageFieldset.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="CoverageFieldset">
+    <el-row
+      :gutter="20"
+      type="flex"
+    >
+      <el-col :span="8">
+        <el-form-item label="# Health workers">
+          <el-input
+            :disabled="disabled"
+            :value="healthWorkers"
+            type="number"
+            min="0"
+            max="10000000"
+            step="1"
+            @change="emitHealthWorkersChange" />
+        </el-form-item>
+      </el-col>
+      <el-col :span="8">
+        <el-form-item label="# Facilities">
+          <el-input
+            :disabled="disabled"
+            :value="facilities"
+            type="number"
+            min="0"
+            max="10000000"
+            step="1"
+            @change="emitFacilitiesChange"
+          />
+        </el-form-item>
+      </el-col>
+      <el-col :span="8">
+        <el-form-item label="# Clients">
+          <el-input
+            :disabled="disabled"
+            :value="clients"
+            type="number"
+            min="0"
+            max="10000000"
+            step="1"
+            @change="emitClientsChange"
+          />
+        </el-form-item>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    disabled: {
+      type: Boolean,
+      default: true
+    },
+    healthWorkers: {
+      type: [Number, String],
+      default: null
+    },
+    clients: {
+      type: [Number, String],
+      default: null
+    },
+    facilities: {
+      type: [Number, String],
+      default: null
+    }
+  },
+  methods: {
+    emitHealthWorkersChange (value) {
+      this.$emit('update:healthWorkers', value);
+    },
+    emitFacilitiesChange (value) {
+      this.$emit('update:facilities', value);
+    },
+    emitClientsChange (value) {
+      this.$emit('update:clients', value);
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
