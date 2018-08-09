93c5991d9b0d828b4f07100356263af38622c873
lordfuoco <lordfuoco@gmail.com>
2018-08-09 16:28:58 +0200

    feat: add-rm buttons for collection filedsets

---
+++ frontend/components/project/AddRmButtons.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="AddRmButtons">
+    <el-button
+      v-show="showAdd"
+      type="text"
+      @click="$emit('add')">
+      <fa icon="plus" />
+      Add more
+    </el-button>
+    <el-button
+      v-show="showRm"
+      type="text"
+      @click="$emit('rm')">
+      <fa icon="minus" />
+      remove
+    </el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    showRm: {
+      type: Boolean,
+      default: false
+    },
+    showAdd: {
+      type: Boolean,
+      default: true
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
