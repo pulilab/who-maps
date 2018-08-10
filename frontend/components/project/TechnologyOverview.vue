be8c4aee0eec959385252bcd1c81785586bd734f
lordfuoco <lordfuoco@gmail.com>
2018-08-10 17:45:33 +0200

    feat: bind license selector and license store functions to technology overview

--- frontend/components/project/TechnologyOverview.vue
+++ frontend/components/project/TechnologyOverview.vue
@@ -3,17 +3,39 @@
     id="technology"
     class="TechnologyOverview">
     <collapsible-card title="Thecnology Overview">
-      TechnologyOverview
+      <el-form-item
+        label="Technology deployment date"
+        prop="implementation_dates"
+      >
+        <el-date-picker
+          v-model="implementation_dates"
+          class="Date"
+          align="center"
+          placeholder="Pick a day"
+        />
+      </el-form-item>
+      <el-form-item label="Under what license is the project governed?">
+        <license-selector v-model="licenses" />
+      </el-form-item>
     </collapsible-card>
   </div>
 </template>
 
 <script>
+import { mapGettersActions } from '../../utilities/form';
 import CollapsibleCard from './CollapsibleCard';
+import LicenseSelector from './LicenseSelector';
 
 export default {
   components: {
-    CollapsibleCard
+    CollapsibleCard,
+    LicenseSelector
+  },
+  computed: {
+    ...mapGettersActions({
+      implementation_dates: ['project', 'getImplementationDates', 'setImplementationDates'],
+      licenses: ['project', 'getLicenses', 'setLicenses']
+    })
   }
 };
 </script>
