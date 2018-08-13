3d2125f2b59143da1dd519f6953f53523dbd1f5e
lordfuoco <lordfuoco@gmail.com>
2018-08-13 12:07:51 +0200

    feat: interoperability and standards fieldset completed

--- frontend/components/project/InteroperabilityAndStandards.vue
+++ frontend/components/project/InteroperabilityAndStandards.vue
@@ -3,17 +3,43 @@
     id="interoperability"
     class="InteroperabilityAndStandards">
     <collapsible-card title="Interoperability &amp; Standards">
-      InteroperabilityAndStandards
+      <el-form-item label="Does your digital health project link to a digital HIS?">
+        <interoperability-link-component
+          v-for="ir in interopearilbityLinksStructure"
+          :key="ir.id"
+          :item="ir"
+          :interoperability-links.sync="interoperability_links"
+        />
+      </el-form-item>
+      <el-form-item label="What data standards does your digital health project use?" >
+        <standards-selector v-model="interoperability_standards" />
+      </el-form-item>
     </collapsible-card>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+import { mapGettersActions } from '../../utilities/form';
+
 import CollapsibleCard from './CollapsibleCard';
+import InteroperabilityLinkComponent from './InteroperabilityLinkComponent';
+import StandardsSelector from './StandardsSelector';
 
 export default {
   components: {
-    CollapsibleCard
+    CollapsibleCard,
+    InteroperabilityLinkComponent,
+    StandardsSelector
+  },
+  computed: {
+    ...mapGetters({
+      interopearilbityLinksStructure: 'projects/getInteroperabilityLinks'
+    }),
+    ...mapGettersActions({
+      interoperability_links: ['project', 'getInteroperabilityLinks', 'setInteroperabilityLinks', 200],
+      interoperability_standards: ['project', 'getInteroperabilityStandards', 'setInteroperabilityStandards', 0]
+    })
   }
 };
 </script>
