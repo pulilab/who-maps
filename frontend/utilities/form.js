513ffbeecd13efc96e9ea232ad1e97d7389df6d9
lordfuoco <lordfuoco@gmail.com>
2018-08-13 12:05:57 +0200

    feat: introduce debounced setters

--- frontend/utilities/form.js
+++ frontend/utilities/form.js
@@ -1,17 +1,20 @@
+import debounce from 'lodash/debounce';
+
 export const mapGettersActions = (collection) => {
   const result = {};
   for (let item in collection) {
     const module = collection[item][0];
     const getter = module + '/' + collection[item][1];
     const setter = module + '/' + collection[item][2];
-
+    const debounceWait = collection[item][3] ? collection[item][3] : 300;
+    const setFunction = function (value) {
+      this.$store.dispatch(setter, value);
+    };
     result[item] = {
       get () {
         return this.$store.getters[getter];
       },
-      set (value) {
-        this.$store.dispatch(setter, value);
-      }
+      set: debounceWait ? debounce(setFunction, debounceWait) : setFunction
     };
   }
   return result;
