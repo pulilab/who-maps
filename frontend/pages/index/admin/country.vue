55f385a99ab1df76c7f7b7d33f328bc32bd6295f
András T. Takács <maurjin@gmail.com>
2018-08-13 15:41:09 +0200

    Wired collapsible card component into country admin

--- frontend/pages/index/admin/country.vue
+++ frontend/pages/index/admin/country.vue
@@ -1,25 +1,40 @@
 <template>
   <div class="CountryAdmin">
-    <country-chooser />
-    <dha-questionaire :label="'Country specific questionaire'"/>
 
-    <vue-map-customizer/>
+    <div class="AdminHeading">Country admin</div>
+
+    <collapsible-card title="Country information">
+      <h1>Country information</h1>
+    </collapsible-card>
+
+    <collapsible-card title="User management">
+      <h1>User management</h1>
+    </collapsible-card>
+
+    <collapsible-card title="Country specific questionaire">
+      <h1>Country specific questionaire</h1>
+      <dha-questionaire :label="'Country specific questionaire'"/>
+    </collapsible-card>
+
+    <collapsible-card title="Country map">
+      <vue-map-customizer/>
+    </collapsible-card>
 
   </div>
 </template>
 
 <script>
+import CollapsibleCard from '../../../components/project/CollapsibleCard';
 import VueMapCustomizer from '../../../components/admin/VueMapCustomizer';
 import DhaQuestionaire from '../../../components/admin/DhaQuestionaire';
-import CountryChooser from '../../../components/admin/CountryChooser';
 
 export default {
 
   name: 'CountryAdministrator',
   components: {
+    CollapsibleCard,
     VueMapCustomizer,
-    DhaQuestionaire,
-    CountryChooser
+    DhaQuestionaire
   },
 
   async fetch ({store}) {
@@ -30,3 +45,18 @@ export default {
 
 };
 </script>
+
+<style lang="less" scoped>
+  .CountryAdmin {
+    .AdminHeading {
+      color: #474747;
+      font-family: Arial;
+      font-size: 32px;
+      font-weight: bold;
+      letter-spacing: -1px;
+      line-height: 24px;
+      text-align: center;
+      margin: 40px 0;
+    }
+  }
+</style>
