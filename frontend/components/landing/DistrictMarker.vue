57afa1427cf49427f26e04002a25a8103eb884c6
lordfuoco <lordfuoco@gmail.com>
2018-08-13 16:12:58 +0200

    chore: clean district marker component

--- frontend/components/landing/DistrictMarker.vue
+++ frontend/components/landing/DistrictMarker.vue
@@ -5,14 +5,8 @@
       :lat-lng="pin.latlng"
       :icon="icon"
       class="MapMarker"
-    >
-      <l-popup
-        ref="tooltip"
-        :options="popupOptions"
-      >
-        10
-      </l-popup>
-    </l-marker>
+      @click="emitMarkerClick"
+    />
   </div>
 </template>
 
@@ -33,49 +27,20 @@ export default {
       type: Object,
       required: false,
       default: () => null
-    },
-    forceHovered: {
-      type: Boolean,
-      default: false
-    },
-    additionalTooltipClass: {
-      type: String,
-      default: ''
     }
   },
   data () {
-    return {
-      popUpHover: false,
-      popupOptions: {
-        className: `DetailsPopup ${this.additionalTooltipClass}`,
-        closeButton: false
-      }
-    };
+    return {};
   },
   computed: {
     ...mapGetters({
       geoJson: 'countries/getGeoJsonLibrary'
-    }),
-    paintTooltip () {
-      return this.$slots.default && (this.hovered || this.forceHovered);
-    }
+    })
   },
   methods: {
     ...mapActions({}),
     emitMarkerClick () {
       this.$emit('marker-click');
-    },
-    mouseEnterHandler (event) {
-      this.popUpHover = true;
-      this.$nextTick(() => {
-        this.$refs.tooltip.mapObject.update();
-      });
-    },
-    mouseLeaveHandler (event) {
-      this.popUpHover = false;
-      this.$nextTick(() => {
-        this.$refs.tooltip.mapObject.update();
-      });
     }
   }
 };
