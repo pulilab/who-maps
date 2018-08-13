9cca99148a7249b94545e803646f4bd4c5f5abff
lordfuoco <lordfuoco@gmail.com>
2018-08-13 17:03:30 +0200

    feat: switchView component

---
+++ frontend/components/common/SwitchViewBox.vue
@@ -0,0 +1,44 @@
+<template>
+
+  <div class="SwitchViewBox">
+    <div class="wrapper">
+      <el-switch
+        :value="activeTab"
+        active-text="National"
+        active-value="national"
+        inactive-value="subNational"
+        inactive-text="Sub-national"
+        @change="setActiveTab($event)"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    activeTab: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    setActiveTab ($event) {
+      this.$emit('update:activeTab', $event);
+    }
+  }
+};
+</script>
+
+<style lang="less">
+  .SwitchViewBox {
+    z-index: 1010;
+    position:relative;
+
+    .wrapper {
+      position: absolute;
+      right: 20px;
+      top: 20px;
+    }
+  }
+</style>
