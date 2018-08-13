8d448aae12e8923adae53e8178c102f671251767
lordfuoco <lordfuoco@gmail.com>
2018-08-13 12:07:03 +0200

    feat: interoperability link component

---
+++ frontend/components/project/InteroperabilityLinkComponent.vue
@@ -0,0 +1,65 @@
+<template>
+  <el-row>
+    <el-col :span="12">
+      <el-checkbox
+        :value="selected"
+        @change="selectedChangeHandler">
+        {{ item.pre }} {{ item.name }}
+      </el-checkbox>
+    </el-col>
+    <el-col :span="12">
+      <el-input
+        :disabled="!selected"
+        :value="link"
+        placeholder="Specify URL"
+        type="text"
+        @change="linkChangeHandler"
+      />
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true
+    },
+    interoperabilityLinks: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    interoperabilityLink () {
+      if (this.interoperabilityLinks && this.item) {
+        return this.interoperabilityLinks[this.item.id] || {};
+      }
+      return {};
+    },
+    selected () {
+      return this.interoperabilityLink.selected;
+    },
+    link () {
+      return this.interoperabilityLink.link;
+    }
+  },
+  methods: {
+    selectedChangeHandler (selected) {
+      const ir = { ...this.interoperabilityLinks };
+      ir[this.item.id] = {...this.interoperabilityLink, selected};
+      this.$emit('update:interoperabilityLinks', ir);
+    },
+    linkChangeHandler (link) {
+      const ir = { ...this.interoperabilityLinks };
+      ir[this.item.id] = {...this.interoperabilityLink, link};
+      this.$emit('update:interoperabilityLinks', ir);
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
