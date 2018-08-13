3f6a199fbf891080398ec941bdfb320f75f9af19
lordfuoco <lordfuoco@gmail.com>
2018-08-13 12:06:49 +0200

    feat: standard selector component

---
+++ frontend/components/project/StandardsSelector.vue
@@ -0,0 +1,43 @@
+<template>
+  <el-checkbox-group
+    :value="value"
+    @input="changeHandler">
+    <el-checkbox
+      v-for="standard in standards"
+      :key="standard.id"
+      :label="standard.id">
+      {{ standard.name }}
+    </el-checkbox>
+  </el-checkbox-group>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  model: {
+    prop: 'value',
+    event: 'change'
+  },
+  props: {
+    value: {
+      type: Array,
+      default: () => []
+    }
+  },
+  computed: {
+    ...mapGetters({
+      standards: 'projects/getInteroperabilityStandards'
+    })
+  },
+  methods: {
+    changeHandler (value) {
+      this.$emit('change', value);
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
