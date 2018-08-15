f7ec8fe79e3c548ba841ddcb8841b859907c404f
Gyorgy Fekete <f@pulilab.com>
2018-08-15 15:10:25 +0200

    include base_name for router

--- django/search/urls.py
+++ django/search/urls.py
@@ -3,6 +3,6 @@ from rest_framework.routers import SimpleRouter
 from .views import SearchViewSet
 
 router = SimpleRouter()
-router.register(r'search', SearchViewSet)
+router.register(r'search', SearchViewSet, base_name="search-list")
 
 urlpatterns = router.urls
