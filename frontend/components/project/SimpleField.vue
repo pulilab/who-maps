9db0dbf5dd6e2d8bad87e592fbde1e3c5beaece6
lordfuoco <lordfuoco@gmail.com>
2018-08-15 17:24:16 +0200

    feat: simple field component to display static data with a label

---
+++ frontend/components/project/SimpleField.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="SimpleField">
+    <div
+      v-if="header"
+      class="Header"
+    >
+      {{ header }}
+    </div>
+    <div class="Content">
+      <template v-if="content">
+        {{ content }}
+      </template>
+      <template v-if="!content">
+        <slot/>
+      </template>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    header: {
+      type: String,
+      default: null
+    },
+    content: {
+      type: [String, Number, Date],
+      default: null
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
