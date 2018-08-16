c5fda8813e8b78931aad3891dc5ec7975962391b
lordfuoco <lordfuoco@gmail.com>
2018-08-16 10:59:55 +0200

    feat: standard list item

---
+++ frontend/components/project/StandardsList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="StandardsList">
+    <ul>
+      <li
+        v-for="standard in selected"
+        :key="standard.id"
+      >
+        {{ standard.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+export default {
+  props: {
+    value: {
+      type: Array,
+      default: null
+    }
+  },
+  computed: {
+    ...mapGetters({
+      standards: 'projects/getInteroperabilityStandards'
+    }),
+    selected () {
+      return this.standards.filter(hfa => this.value.includes(hfa.id));
+    }
+  }
+};
+</script>
+
+<style lang="less">
+.StandardsList {
+  width: 100%;
+}
+</style>
