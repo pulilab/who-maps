3b9e95cadf4a3b096c2d921500d9180827cfe3a4
Gyorgy Fekete <f@pulilab.com>
2018-08-16 16:16:49 +0200

    remove previous search tests

--- django/search/tests.py
+++ django/search/tests.py
@@ -22,81 +22,12 @@ class SearchTests(SetupTests):
         response = self.test_user_client.put(url, project_data2, format="json")
         self.assertEqual(response.status_code, 200)
 
-    def test_search_two_fields(self):
-        url = reverse("search-project")
-        data = {"query": "phrase2", "project_name": True, "location": True}
-        response = self.test_user_client.post(url, data, format="json")
+    def test_search_no_results(self):
+        url = reverse("search-project-list")
+        data = {"q": "phrase2"}
+        response = self.test_user_client.get(url, data, format="json")
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.json()), 2)
-
-    def test_search_one_field(self):
-        url = reverse("search-project")
-        data = {
-            "query": "phrase1",
-            "project_name": True,
-        }
-        response = self.test_user_client.post(url, data, format="json")
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.json()), 1)
-
-    def test_search_org(self):
-        url = reverse("search-project")
-        data = {
-            "query": "org1",
-            "organisation": True,
-        }
-        response = self.test_user_client.post(url, data, format="json")
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.json()), 2)
-
-    def test_search_platform(self):
-        url = reverse("search-project")
-        data = {
-            "query": "Adobe",
-            "technology_platform": True,
-        }
-        response = self.test_user_client.post(url, data, format="json")
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.json()), 2)
-
-    def test_search_hsc(self):
-        url = reverse("search-project")
-        data = {
-            "query": "sexuality",
-        }
-        response = self.test_user_client.post(url, data, format="json")
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.json()), 2)
-
-    def test_search_not_found(self):
-        url = reverse("search-project")
-        data = {
-            "query": "nonexistent",
-            "project_name": True,
-            "location": True,
-            "health_topic": True,
-            "organisation": True
-        }
-        response = self.test_user_client.post(url, data, format="json")
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.json()), 0)
-
-    def test_search_not_in_main_categories(self):
-        url = reverse("search-project")
-        data = {"query": "overview"}
-        response = self.test_user_client.post(url, data, format="json")
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.json()), 2)
-
-    def test_search_not_in_main_categories_wiki(self):
-        url = reverse("search-project")
-        data = {"query": "wiki.org"}
-        response = self.test_user_client.post(url, data, format="json")
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.json()), 2)
-
-    def test_search_no_query(self):
-        url = reverse("search-project")
-        response = self.test_user_client.post(url, {}, format="json")
-        self.assertEqual(response.status_code, 400)
-        self.assertEqual(response.json()['query'][0], 'This field is required.')
+        self.assertEqual(len(response.json()['results']['projects']), 0)
+        self.assertEqual(response.json()['count'], 0)
+        self.assertEqual(response.json()['results'],
+                         {'projects': [], 'search_term': 'phrase2', 'search_in': [], 'type': 'map'})
