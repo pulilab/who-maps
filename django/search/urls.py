c628c9d8be7dd02347645474ffba7697692d832c
Gyorgy Fekete <f@pulilab.com>
2018-08-16 16:17:01 +0200

    change base name on search endpoint

--- django/search/urls.py
+++ django/search/urls.py
@@ -3,6 +3,6 @@ from rest_framework.routers import SimpleRouter
 from .views import SearchViewSet
 
 router = SimpleRouter()
-router.register(r'search', SearchViewSet, base_name="search-list")
+router.register(r'search', SearchViewSet, base_name="search-project")
 
 urlpatterns = router.urls
