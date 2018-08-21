a17ddb0a50b525d25f95f552b739a28a5f0aaeb3
lordfuoco <lordfuoco@gmail.com>
2018-08-21 13:04:19 +0200

    feat: complete validation, added api validation rules

--- frontend/components/project/ProjectForm.vue
+++ frontend/components/project/ProjectForm.vue
@@ -43,7 +43,8 @@ import ImplementationOverview from './ImplementationOverview';
 import TechnologyOverview from './TechnologyOverview';
 import InteroperabilityAndStandards from './InteroperabilityAndStandards';
 import { mapGetters, mapActions } from 'vuex';
-import { max, required, requiredList, url } from '../../utilities/form.js';
+import { max, required, requiredList, url, notEmpty } from '../../utilities/form.js';
+import FormAPIErrorsMixin from '../mixins/FormAPIErrorsMixin.js';
 
 export default {
   components: {
@@ -53,11 +54,12 @@ export default {
     TechnologyOverview,
     InteroperabilityAndStandards
   },
+  mixins: [FormAPIErrorsMixin],
   data () {
     return {
       readyElements: 0,
       maxElements: 4,
-      usePublishRules: true
+      usePublishRules: false
     };
   },
   computed: {
@@ -99,7 +101,7 @@ export default {
       return this.readyElements === this.maxElements;
     },
     draftRules () {
-      return {
+      const rules = {
         name: [
           required()
         ],
@@ -107,6 +109,13 @@ export default {
           {type: 'email', message: 'Please insert a valid email', trigger: 'blur'}
         ]
       };
+      for (let item in this.model) {
+        if (!rules[item]) {
+          rules[item] = [];
+        }
+        rules[item].push({ validator: this.validatorGenerator(item), trigger: 'blur' });
+      }
+      return rules;
     },
     publishRules () {
       const platformRules = [
@@ -134,29 +143,34 @@ export default {
         return { ...a, [`coverage_second_level.${index}`]: coverageRules };
       }, {});
 
-      return {
+      const rules = {
         name: [
-          ...this.draftRules.name,
+          required(),
           max(128)
         ],
         organisation: [
           required()
         ],
         country: [
-          required()
+          required('number')
         ],
         geographic_scope: [],
         implementation_overview: [
+          // required(),
           max(512)
         ],
-        start_date: [],
-        end_date: [],
+        start_date: [
+          {validator: this.dateValidator, trigger: 'blur'}
+        ],
+        end_date: [
+          {validator: this.dateValidator, trigger: 'blur'}
+        ],
         contact_name: [
           required(),
           max(256)
         ],
         contact_email: [
-          ...this.draftRules.contact_email,
+          {type: 'email', message: 'Please insert a valid email', trigger: 'blur'},
           required()
         ],
         ...platforms,
@@ -169,7 +183,7 @@ export default {
           requiredList()
         ],
         ...coverage,
-        coverageData: {},
+        coverageData: [],
         ...coverage_second_level,
         'national_level_deployment.health_workers': [
           this.project.coverageType === 2 ? required() : {type: 'number'}
@@ -181,7 +195,7 @@ export default {
           this.project.coverageType === 2 ? required() : {type: 'number'}
         ],
         government_investor: [
-          required()
+          required('number')
         ],
         ...implementing_partners,
         implementation_dates: [
@@ -203,6 +217,13 @@ export default {
         interoperability_links: [],
         interoperability_standards: []
       };
+      for (let item in this.model) {
+        if (!rules[item]) {
+          rules[item] = [];
+        }
+        rules[item].push({ validator: this.validatorGenerator(item), trigger: 'blur' });
+      }
+      return rules;
     },
     rules () {
       return this.usePublishRules ? this.publishRules : this.draftRules;
@@ -212,7 +233,8 @@ export default {
     ...mapActions({
       createProject: 'project/createProject',
       saveDraft: 'project/saveDraft',
-      discardDraft: 'project/discardDraft'
+      discardDraft: 'project/discardDraft',
+      publishProject: 'project/publishProject'
     }),
     digitalHealthInterventionsValidator (rule, value, callback) {
       const ownDhi = this.project.digitalHealthInterventions.filter(dhi => dhi.platform === value && dhi.id);
@@ -227,38 +249,73 @@ export default {
       }
     },
     coverageDataValidator (rule, value, callback) {
-      const cov = this.project.coverageData[value];
-      console.log(cov);
-      // if (ownDhi.length === 0) {
-      //   const error = {
-      //     message: 'Please select one or more Digital Health Intervetions for this Software',
-      //     field: rule.fullField
-      //   };
-      //   callback(error);
-      // } else {
-      //   callback();
-      // }
+      if (this.project.coverageType === 1) {
+        const cov = this.project.coverageData[value];
+        if (cov && notEmpty(cov.facilities) && notEmpty(cov.health_workers) && notEmpty(cov.clients)) {
+          callback();
+        } else {
+          const error = {
+            message: 'Health workers facilities or clients value missing or malformed',
+            field: rule.fullField
+          };
+          callback(error);
+        }
+      }
+    },
+    dateValidator (rule, value, callback) {
+      if (this.project.start_date && this.project.end_date) {
+        const start = new Date(this.project.start_date);
+        const end = new Date(this.project.end_date);
+        if (start.getTime() > end.getTime()) {
+          const error = {
+            message: 'End Date must be greater than Start Date',
+            field: rule.fullField
+          };
+          callback(error);
+        } else {
+          callback();
+        }
+      } else {
+        const error = {
+          message: 'This field is required',
+          field: rule.fullField
+        };
+        callback(error);
+      }
     },
     mountedHandler () {
       setTimeout(() => {
         this.readyElements += 1;
       }, 300);
     },
-    doSaveDraft () {
-      this.$refs.projectForm.validate(async (valid, fields) => {
-        console.log(fields);
-        if (valid) {
-          if (this.isNewProject) {
-            const id = await this.createProject();
-            const localised = this.localePath({name: 'index-projects-id-edit', params: {id}});
-            this.$router.push(localised);
-          } else if (this.isDraft) {
-            await this.saveDraft(this.$route.params.id);
-          }
-          this.$alert('Your draft has been saved successfully', 'Congratulation', {
-            confirmButtonText: 'Close'
-          });
+    scrollToError () {
+      this.$nextTick(() => {
+        const errors = this.$el.querySelectorAll('.is-error');
+        if (errors[0]) {
+          errors[0].scrollIntoView();
         }
+      });
+    },
+    async doSaveDraft () {
+      this.usePublishRules = false;
+      this.$nextTick(() => {
+        this.deleteFormAPIErrors();
+        this.$refs.projectForm.validate(async valid => {
+          if (valid) {
+            if (this.isNewProject) {
+              const id = await this.createProject();
+              const localised = this.localePath({name: 'index-projects-id-edit', params: {id}});
+              this.$router.push(localised);
+            } else if (this.isDraft) {
+              await this.saveDraft(this.$route.params.id);
+            }
+            this.$alert('Your draft has been saved successfully', 'Congratulation', {
+              confirmButtonText: 'Close'
+            });
+          } else {
+            this.scrollToError();
+          }
+        });
       });
     },
     async doDiscardDraft () {
@@ -282,7 +339,28 @@ export default {
     },
     async doPublishProject () {
       this.usePublishRules = true;
-      console.log(this.rules);
+      this.$nextTick(() => {
+        this.deleteFormAPIErrors();
+        this.$refs.projectForm.validate(async (valid) => {
+          if (valid) {
+            try {
+              await this.publishProject(this.$route.params.id);
+              const localised = this.localePath({name: 'index-projects-id-published', params: {...this.$route.params}});
+              this.$router.push(localised);
+              this.$alert('Your draft has been published successfully', 'Congratulation', {
+                confirmButtonText: 'Close'
+              });
+            } catch (e) {
+              this.setFormAPIErrors(e);
+              this.$refs.projectForm.validate(() => {
+                this.scrollToError();
+              });
+            }
+          } else {
+            this.scrollToError();
+          }
+        });
+      });
     }
   }
 
