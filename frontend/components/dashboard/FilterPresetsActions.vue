78412e1775e05c9c96b1774c12d9e949adfd0687
lordfuoco <lordfuoco@gmail.com>
2018-08-21 15:09:35 +0200

    feat: filter presets component

---
+++ frontend/components/dashboard/FilterPresetsActions.vue
@@ -0,0 +1,45 @@
+<template>
+  <el-row>
+    <el-col :span="12">
+      <el-popover
+        placement="bottom-center"
+        title="My filters presets"
+        width="200"
+        popper-class="AdvancedSearchPresetsDropdown"
+        trigger="click"
+      >
+        <el-button
+          slot="reference"
+          type="text"
+        >
+          Load filters
+          <fa icon="arrow-down" />
+        </el-button>
+        <div class="FilterList">
+          <div>Filter One</div>
+          <div>Filter Two</div>
+        </div>
+      </el-popover>
+    </el-col>
+    <el-col :span="6">
+      <el-button type="text">
+        Save
+      </el-button>
+    </el-col>
+    <el-col :span="6">
+      <el-button type="text">
+        Clear
+      </el-button>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
