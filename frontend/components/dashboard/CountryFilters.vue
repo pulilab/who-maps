976b3f92b31a1b23ea82c2821e2fbdd893d86dab
lordfuoco <lordfuoco@gmail.com>
2018-08-21 15:09:22 +0200

    feat: country filters component

---
+++ frontend/components/dashboard/CountryFilters.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="CountryFilters">
+    <country-select v-model="selectedCountry" />
+    <region-select v-model="selectedRegion" />
+  </div>
+</template>
+
+<script>
+import CountrySelect from '../common/CountrySelect';
+import RegionSelect from '../common/RegionSelect';
+export default {
+  components: {
+    CountrySelect,
+    RegionSelect
+  },
+  data () {
+    return {
+      selectedCountry: null,
+      selectedRegion: null
+    };
+  }
+};
+</script>
+
+<style>
+
+</style>
