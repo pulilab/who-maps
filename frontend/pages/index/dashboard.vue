d3dbb51cbe3a167b5802bb421e8ac847a10c39df
lordfuoco <lordfuoco@gmail.com>
2018-08-21 15:08:29 +0200

    feat: dashboard parent page with filter area

--- frontend/pages/index/dashboard.vue
+++ frontend/pages/index/dashboard.vue
@@ -1,15 +1,41 @@
 <template>
-  <div>
-    Dashboard component
+  <div class="DashboardArea">
+    <div class="ChildContainer">
+      <nuxt-child />
+    </div>
+    <div class="FilterArea">
+      <advanced-search />
+    </div>
   </div>
 </template>
 
 <script>
+import AdvancedSearch from '../../components/dashboard/AdvancedSearch';
+
 export default {
-  middleware: ['isLoggedIn']
+  components: {
+    AdvancedSearch
+  },
+  middleware: ['isLoggedIn'],
+  async fetch ({store}) {
+    await Promise.all([
+      store.dispatch('projects/loadUserProjects'),
+      store.dispatch('projects/loadProjectStructure')
+    ]);
+  }
 };
 </script>
 
-<style>
+<style lang="less">
+.DashboardArea {
+  display: flex;
+  .ChildContainer {
+    width: 75%;
+  }
+  .FilterArea {
+    width: 25%;
+    flex-grow: 0;
+  }
+}
 
 </style>
