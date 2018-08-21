ad1ec60595d7c4d3e5a33c8ebd2e14d127581297
lordfuoco <lordfuoco@gmail.com>
2018-08-21 15:09:52 +0200

    feat: filter switch component

---
+++ frontend/components/dashboard/FilterSwitch.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="FilterSwitchs">
+
+    <el-switch
+      :value="value"
+      @change="switchChangeHandler"
+    />
+    {{ label }}
+    <el-tooltip
+      v-model="showTooltip"
+      :content="tooltip"
+      effect="dark"
+      placement="top"
+      manual
+    >
+      <el-button
+        type="text"
+        @click="showTooltip = !showTooltip"
+      >
+        <fa icon="question-circle" />
+      </el-button>
+    </el-tooltip>
+  </div>
+</template>
+
+<script>
+export default {
+  model: {
+    prop: 'value',
+    event: 'change'
+  },
+  props: {
+    value: {
+      type: Boolean,
+      default: false
+    },
+    label: {
+      type: String,
+      required: true
+    },
+    tooltip: {
+      type: String,
+      required: true
+    }
+  },
+  data () {
+    return {
+      showTooltip: false
+    };
+  },
+  methods: {
+    switchChangeHandler (value) {
+      console.log(value);
+      this.$emit('change', value);
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
