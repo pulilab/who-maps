550fb09a948ee913ba860410b02827247b9333ed
lordfuoco <lordfuoco@gmail.com>
2018-08-21 15:09:44 +0200

    feat: filter item component

---
+++ frontend/components/dashboard/FilterItem.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="FilterItem">
+    <div class="FilterItemHeader">
+      <div class="Label">
+        {{ label }}
+      </div>
+      <el-button type="text">
+        <span v-show="!selected">
+          All
+        </span>
+        <span v-show="selected">
+          Filtered
+        </span>
+        <fa icon="pencil-alt" />
+      </el-button>
+    </div>
+    <div class="Selected">
+      <slot/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      required: true
+    },
+    selected: {
+      type: Boolean,
+      default: false
+    }
+  }
+};
+</script>
+
+<style lang="less">
+.FilterItemHeader {
+  display: flex;
+}
+
+</style>
