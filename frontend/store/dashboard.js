fe6b926570f2123b514624472432fb6a0e97a19c
lordfuoco <lordfuoco@gmail.com>
2018-08-22 16:09:23 +0200

    feat: add selectedX item to dashboard store module

--- frontend/store/dashboard.js
+++ frontend/store/dashboard.js
@@ -65,20 +65,60 @@ export const state = () => ({
     { id: 4, name: 'Chris', country: 'Sierra Leone', organisation_name: 'Pulilab', donors: ['Donor1, donor2'], contact_name: 'Torben', implementation_overview: 'Implementation', geographic_scope: 'Geo Scope', health_focus_areas: [1, 2, 3] },
     { id: 5, name: 'Dan', country: 'Sierra Leone', organisation_name: 'Pulilab', donors: ['Donor1, donor2'], contact_name: 'Torben', implementation_overview: 'Implementation', geographic_scope: 'Geo Scope', health_focus_areas: [1, 2, 3] },
     { id: 6, name: 'John', country: 'Sierra Leone', organisation_name: 'Pulilab', donors: ['Donor1, donor2'], contact_name: 'Torben', implementation_overview: 'Implementation', geographic_scope: 'Geo Scope', health_focus_areas: [1, 2, 3] }
-  ]
+  ],
+  selectedDHI: [],
+  selectedHFA: [],
+  selectedHSC: [],
+  selectedHIS: [],
+  selectedPlatforms: []
 });
 export const getters = {
   getAvailableColumns: state => [...state.columns.map(c => ({...c, selected: state.selectedColumns.includes(c.id)}))],
   getSelectedColumns: state => [...state.columns.filter(c => state.selectedColumns.includes(c.id)).map(c => ({...c}))],
-  getRows: state => [...state.rows.map(r => ({...r}))]
+  getRows: state => [...state.rows.map(r => ({...r}))],
+  getSelectedDHI: state => state.selectedDHI,
+  getSelectedHFA: state => state.selectedHFA,
+  getSelectedHSC: state => state.selectedHSC,
+  getSelectedHIS: state => state.selectedHIS,
+  getSelectedPlatforms: state => state.selectedPlatforms
 };
 export const actions = {
   setSelectedColumns ({commit}, columns) {
     commit('SET_SELECTED_COLUMNS', columns);
+  },
+  setSelectedDHI ({commit}, columns) {
+    commit('SET_SELECTED_DHI', columns);
+  },
+  setSelectedHFA ({commit}, columns) {
+    commit('SET_SELECTED_HFA', columns);
+  },
+  setSelectedHSC ({commit}, columns) {
+    commit('SET_SELECTED_HSC', columns);
+  },
+  setSelectedHIS ({commit}, columns) {
+    commit('SET_SELECTED_HIS', columns);
+  },
+  setSelectedPlatforms ({commit}, columns) {
+    commit('SET_SELECTED_PLATFORMS', columns);
   }
 };
 export const mutations = {
   SET_SELECTED_COLUMNS: (state, columns) => {
     state.selectedColumns = columns;
+  },
+  SET_SELECTED_DHI: (state, dhi) => {
+    state.selectedDHI = dhi;
+  },
+  SET_SELECTED_HFA: (state, hfa) => {
+    state.selectedHFA = hfa;
+  },
+  SET_SELECTED_HSC: (state, hsc) => {
+    state.selectedHSC = hsc;
+  },
+  SET_SELECTED_HIS: (state, his) => {
+    state.selectedHIS = his;
+  },
+  SET_SELECTED_PLATFORMS: (state, platforms) => {
+    state.selectedPlatforms = platforms;
   }
 };
