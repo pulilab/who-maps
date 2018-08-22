eea84d02f852fe80eeff3365f303d24b04ae2c2a
lordfuoco <lordfuoco@gmail.com>
2018-08-22 16:09:42 +0200

    feat: add filter dialog state to layout module

--- frontend/store/layout.js
+++ frontend/store/layout.js
@@ -1,19 +1,27 @@
 export const state = () => ({
-  digitalHealthInterventionsDialogState: null
+  digitalHealthInterventionsDialogState: null,
+  dashboardFiltersDialogState: true
 });
 
 export const getters = {
-  getDigitalHealthInterventionsDialogState: state => state.digitalHealthInterventionsDialogState
+  getDigitalHealthInterventionsDialogState: state => state.digitalHealthInterventionsDialogState,
+  getDashboardFiltersDialogState: state => state.dashboardFiltersDialogState
 };
 
 export const actions = {
   setDigitalHealthInterventionsDialogState ({commit}, value) {
     commit('SET_DIGITAL_HEALTH_INTERVENTIONS_DIALOG_STATE', value);
+  },
+  setDashboardFiltersDialogState ({commit}, value) {
+    commit('SET_DASHBOARD_FILTERS_DIALOG_STATE', value);
   }
 };
 
 export const mutations = {
   SET_DIGITAL_HEALTH_INTERVENTIONS_DIALOG_STATE: (state, value) => {
     state.digitalHealthInterventionsDialogState = value;
+  },
+  SET_DASHBOARD_FILTERS_DIALOG_STATE: (state, value) => {
+    state.dashboardFiltersDialogState = value;
   }
 };
