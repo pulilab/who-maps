7d20e2a3339740b0b3271e3a0f49e7f8ff41584b
lordfuoco <lordfuoco@gmail.com>
2018-08-22 12:15:05 +0200

    feat: main table component for dashboard list

---
+++ frontend/components/dashboard/MainTable.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="MainTable">
+    <vue-good-table
+      :columns="columns"
+      :rows="rows"
+      :fixed-header="true"
+      :sort-options="sortOptions"
+      :pagination-options="paginationOptions"
+      :select-options="selectOptions"
+    />
+  </div>
+</template>
+
+<script>
+import { VueGoodTable } from 'vue-good-table';
+import { mapGetters } from 'vuex';
+
+export default {
+  components: {
+    VueGoodTable
+  },
+  data () {
+    return {
+      sortOptions: {
+        enabled: true
+      },
+      paginationOptions: {
+        enabled: true
+      },
+      selectOptions: {
+        enabled: true
+      }
+    };
+  },
+  computed: {
+    ...mapGetters({
+      columns: 'dashboard/getSelectedColumns',
+      rows: 'dashboard/getRows'
+    })
+  }
+};
+</script>
+
+<style lang="less">
+@import 'vue-good-table/dist/vue-good-table.css';
+
+</style>
