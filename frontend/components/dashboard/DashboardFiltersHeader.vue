65480961c4d4767af75cb5f217294f74b56e7858
lordfuoco <lordfuoco@gmail.com>
2018-08-24 09:36:26 +0200

    feat: simple filter header component

---
+++ frontend/components/dashboard/DashboardFiltersHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="DashboardFiltersHeader">
+    <fa icon="filter"/>
+    Projects to show
+    <fa icon="caret-right" />
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
