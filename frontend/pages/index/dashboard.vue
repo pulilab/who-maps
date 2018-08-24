a56b1a91eb49064279f1fcaa44009787750d1587
Zoli <z@pulilab.com>
2018-08-24 17:25:12 +0200

    style: fix layout issues

--- frontend/pages/index/dashboard.vue
+++ frontend/pages/index/dashboard.vue
@@ -40,6 +40,7 @@ export default {
 
     .ChildContainer {
       width: calc(100vw - @advancedSearchWidth);
+      min-width: @appWidthMinLimit - @advancedSearchWidth;
       max-width: @appWidthMaxLimit - @advancedSearchWidth;
       height: 100%;
 
