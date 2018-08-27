e1f749812ddcee37447c84fa04dc07be6cbec724
András T. Takács <maurjin@gmail.com>
2018-08-27 13:41:28 +0200

    fix getter error by better defaulting state

--- frontend/store/admin/country.js
+++ frontend/store/admin/country.js
@@ -1,6 +1,6 @@
 export const state = () => ({
-  country: null,
-  editableCountry: null,
+  country: {},
+  editableCountry: {},
   userSelection: [],
   adminSelection: [],
   superadminSelection: []
