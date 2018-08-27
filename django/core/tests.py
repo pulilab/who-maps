4f2844bb40b54756fc06c1af0b13c2b5468bf1d1
Gyorgy Fekete <f@pulilab.com>
2018-08-27 13:57:39 +0200

    remove lazy encoder tests

--- django/core/tests.py
+++ django/core/tests.py
@@ -1,6 +1,5 @@
 from io import BytesIO
 from PIL import Image
-from unittest import skip
 
 from django.contrib.admin import AdminSite
 from django.contrib.admin.widgets import AdminTextInputWidget
@@ -15,10 +14,6 @@ from core.admin.widgets import AdminArrayFieldWidget, AdminArrayField, NoneReadO
 from country.forms import CountryFieldAdminForm
 from country.models import CountryField, Country
 from user.models import UserProfile
-from django.utils.translation import ugettext_lazy as _
-from .utils import lazyJSONDumps, LazyEncoder
-from mock import patch
-from django.core.serializers.json import DjangoJSONEncoder # noqa
 
 
 class AuthTest(TestCase):
@@ -195,25 +190,6 @@ class TestStaticDataEndpoint(TestCase):
         self.assertEqual(response.status_code, 200)
         name_list = [l['name'] for l in response.json()['languages']]
         self.assertEqual(name_list, ['Anglais', 'Français', 'Espagnol', 'Portugais'])
-
-
-class TestUtils(TestCase):
-
-    def test_lazy_encoder_default(self):
-        with patch('django.core.serializers.json.DjangoJSONEncoder.default') as mock:
-            le = LazyEncoder()
-            le.default(1)
-            self.assertTrue(mock.called)
-
-    def test_lazy_json_dumps_normal_json(self):
-        obj = dict(a=1, b=2)
-        json = lazyJSONDumps(obj)
-        self.assertEqual(json, '{"a": 1, "b": 2}')
-
-    def test_lazy_json_dumps_translated_json(self):
-        obj = dict(a=_('test1'), b=_('test2'))
-        json = lazyJSONDumps(obj)
-        self.assertEqual(json, '{"a": "test1", "b": "test2"}')
 
 
 def get_temp_image(name='test', ext='png'):
