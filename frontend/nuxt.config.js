bb326a88f972fef2fcbfb5d3922d7bad917f0e12
lordfuoco <lordfuoco@gmail.com>
2018-08-28 10:58:48 +0200

    chore: setup the correct favicon

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -14,7 +14,7 @@ const config = {
       { hid: 'description', name: 'description', content: 'DHA' }
     ],
     link: [
-      { rel: 'icon', type: 'image/pmg', href: '/favicon.png' },
+      { rel: 'icon', type: 'image/ico', href: '/favicon.ico' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' }
     ]
   },
