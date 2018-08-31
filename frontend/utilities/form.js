d0ddfad35e56b2026bac0077d2cc5ca7fc050281
lordfuoco <lordfuoco@gmail.com>
2018-08-31 15:55:50 +0200

    fix: default to zero and not 300 on debounce function

--- frontend/utilities/form.js
+++ frontend/utilities/form.js
@@ -6,15 +6,16 @@ export const mapGettersActions = (collection) => {
     const module = collection[item][0];
     const getter = module + '/' + collection[item][1];
     const setter = module + '/' + collection[item][2];
-    const debounceWait = collection[item][3] ? collection[item][3] : 300;
+    const debounceWait = collection[item][3] ? collection[item][3] : 0;
     const setFunction = function (value) {
       this.$store.dispatch(setter, value);
     };
+    const set = debounceWait ? debounce(setFunction, debounceWait) : setFunction;
     result[item] = {
       get () {
         return this.$store.getters[getter];
       },
-      set: debounceWait ? debounce(setFunction, debounceWait) : setFunction
+      set
     };
   }
   return result;
