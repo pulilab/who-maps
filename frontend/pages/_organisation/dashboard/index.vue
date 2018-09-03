2b555450d7bbf27f1c70e2e1d0d3bbbb524b3365
lordfuoco <lordfuoco@gmail.com>
2018-09-03 11:05:53 +0200

    chore: wire dashboard project box, minor styling

--- frontend/pages/_organisation/dashboard/index.vue
+++ frontend/pages/_organisation/dashboard/index.vue
@@ -1,19 +1,25 @@
 <template>
   <div class="DashboardMapView">
     <dashboard-map />
+    <dashboard-project-box />
   </div>
 </template>
 
 <script>
 import DashboardMap from '../../../components/dashboard/DashboardMap';
+import DashboardProjectBox from '../../../components/dashboard/DashboardProjectBox';
 
 export default {
   components: {
-    DashboardMap
+    DashboardMap,
+    DashboardProjectBox
   }
 };
 </script>
 
-<style>
+<style lang="less">
+  .DashboardMapView {
+    position: relative;
+  }
 
 </style>
