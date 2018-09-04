44ed17726e5b7490c9e3e9e484785ef4413182d7
lordfuoco <lordfuoco@gmail.com>
2018-09-04 16:09:30 +0200

    feat: differentiate map and list data retrival and storage

--- frontend/pages/_organisation/dashboard/index.vue
+++ frontend/pages/_organisation/dashboard/index.vue
@@ -6,10 +6,35 @@
 
 <script>
 import DashboardMap from '../../../components/dashboard/DashboardMap';
-
+import { mapGetters, mapActions } from 'vuex';
 export default {
   components: {
     DashboardMap
+  },
+  async fetch ({store}) {
+    await Promise.all([
+      store.dispatch('projects/loadUserProjects'),
+      store.dispatch('projects/loadProjectStructure'),
+      store.dispatch('dashboard/loadProjectsMap')
+    ]);
+  },
+  computed: {
+    ...mapGetters({
+      searchParameters: 'dashboard/getSearchParameters'
+    })
+  },
+  watch: {
+    searchParameters: {
+      immediate: false,
+      handler (params) {
+        this.loadProjectsMap();
+      }
+    }
+  },
+  methods: {
+    ...mapActions({
+      loadProjectsMap: 'dashboard/loadProjectsMap'
+    })
   }
 };
 </script>
--- frontend/pages/_organisation/dashboard/list.vue
+++ frontend/pages/_organisation/dashboard/list.vue
@@ -12,11 +12,37 @@
 <script>
 import MainTable from '../../../components/dashboard/MainTable';
 import TableTopActions from '../../../components/dashboard/TableTopActions';
+import { mapGetters, mapActions } from 'vuex';
 
 export default {
   components: {
     MainTable,
     TableTopActions
+  },
+  async fetch ({store}) {
+    await Promise.all([
+      store.dispatch('projects/loadUserProjects'),
+      store.dispatch('projects/loadProjectStructure'),
+      store.dispatch('dashboard/loadProjectList')
+    ]);
+  },
+  computed: {
+    ...mapGetters({
+      searchParameters: 'dashboard/getSearchParameters'
+    })
+  },
+  watch: {
+    searchParameters: {
+      immediate: false,
+      handler (params) {
+        this.loadProjectList();
+      }
+    }
+  },
+  methods: {
+    ...mapActions({
+      loadProjectList: 'dashboard/loadProjectList'
+    })
   }
 };
 </script>
