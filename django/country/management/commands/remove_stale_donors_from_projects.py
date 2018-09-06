35fea47d281cc4a851d899cdc6fafc553268e3bc
Gyorgy Fekete <f@pulilab.com>
2018-09-06 16:43:52 +0200

    call the new classmethod from mgmt cmd

--- django/country/management/commands/remove_stale_donors_from_projects.py
+++ django/country/management/commands/remove_stale_donors_from_projects.py
@@ -8,18 +8,5 @@ class Command(BaseCommand):
     help = "Remove stale donors from projects and rebuild search"
 
     def handle(self, *args, **options):
-        stale_ids = []
-        donor_ids = set(Donor.objects.values_list('id', flat=True))
-
-        for p in Project.objects.all():
-            if p.data and 'donors' in p.data:
-                published_donors = set(p.data.get('donors', []))
-                stale_ids.extend(list(published_donors - donor_ids))
-                p.data['donors'] = list(published_donors & donor_ids)
-            if p.draft and 'donors' in p.draft:
-                draft_donors = set(p.draft.get('donors', []))
-                stale_ids.extend(list(draft_donors - donor_ids))
-                p.draft['donors'] = list(draft_donors & donor_ids)
-            p.save()
-
+        stale_ids = Project.remove_stale_donors()
         self.stdout.write('Removed {} donors'.format(len(set(stale_ids))))
