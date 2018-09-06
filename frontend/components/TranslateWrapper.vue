c31a42124abde5d39f6ff3d4273367cbb7e41ff6
lordfuoco <lordfuoco@gmail.com>
2018-09-06 14:26:06 +0200

    feat: translation component wrapper

---
+++ frontend/components/TranslateWrapper.vue
@@ -0,0 +1,23 @@
+<template>
+  <span>
+    {{ $t(text) }}
+  </span>
+</template>
+
+<script>
+export default {
+  computed: {
+    text () {
+      if (this.$slots && this.$slots.default && this.$slots.default[0] && this.$slots.default[0]) {
+        return this.$slots.default[0].text.trim();
+      } else {
+        console.error('Empty translate block');
+      }
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
