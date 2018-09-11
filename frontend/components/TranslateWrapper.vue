bb51933c085591e436f1ec3efb27c2ed402c0a8a
lordfuoco <lordfuoco@gmail.com>
2018-09-11 12:56:22 +0200

    fix: inline all the translation operation, slots do not trigger computed properties

--- frontend/components/TranslateWrapper.vue
+++ frontend/components/TranslateWrapper.vue
@@ -1,20 +1,11 @@
 <template>
-  <span>
-    {{ $t(text) }}
+  <span v-if="$slots.default && $slots.default[0].text">
+    {{ $t($slots.default[0].text.trim()) }}
   </span>
 </template>
 
 <script>
 export default {
-  computed: {
-    text () {
-      if (this.$slots && this.$slots.default && this.$slots.default[0] && this.$slots.default[0]) {
-        return this.$slots.default[0].text.trim();
-      } else {
-        console.error('Empty translate block');
-      }
-    }
-  }
 };
 </script>
 
