5af51f3d975be943887b5efec5d81d4f755676f4
Gyorgy Fekete <f@pulilab.com>
2018-09-14 17:38:22 +0200

    update existing answer test

--- django/project/tests/customfields_tests.py
+++ django/project/tests/customfields_tests.py
@@ -96,3 +96,26 @@ def test_country_answer_for_published(self):
     project = Project.objects.last()
     self.assertEqual(project.data['country_custom_answers'], {str(q.id): 'lol1'})
     self.assertEqual(project.draft['country_custom_answers'], {str(q.id): 'lol1'})
+
+def test_country_answer_update_existing_answer(self):
+    q = CountryCustomQuestion.objects.create(question="test", country_id=self.country_id)
+    url = reverse("country-custom-answer",
+                  kwargs={
+                      "country_id": self.country_id,
+                      "project_id": self.project_id
+                  })
+    data = [dict(question_id=q.id, answer="lol1", draft=False)]
+
+    response = self.test_user_client.post(url, data=data, format='json')
+    self.assertEqual(response.status_code, 200)
+    self.assertEqual(response.json(), [{'question_id': q.id, 'answer': 'lol1', 'draft': False}])
+
+    data = [dict(question_id=q.id, answer="lol2", draft=False)]
+
+    response = self.test_user_client.post(url, data=data, format='json')
+    self.assertEqual(response.status_code, 200)
+    self.assertEqual(response.json(), [{'question_id': q.id, 'answer': 'lol2', 'draft': False}])
+
+    project = Project.objects.last()
+    self.assertEqual(project.data['country_custom_answers'], {str(q.id): 'lol2'})
+    self.assertEqual(project.draft['country_custom_answers'], {str(q.id): 'lol2'})
