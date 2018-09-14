0da19c96202aabb08ae194f0876a45f1074cf49a
Gyorgy Fekete <f@pulilab.com>
2018-09-14 09:59:08 +0200

    fix import

--- django/core/admin/__init__.py
+++ django/core/admin/__init__.py
@@ -5,7 +5,6 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
-from django.contrib.sites.models import Site
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.postgres.fields.array import ArrayField
 from django.forms.widgets import MediaDefiningClass
