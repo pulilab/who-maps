c0ba5569775590d97b490412eb7ff68b81011a4f
lordfuoco <lordfuoco@gmail.com>
2018-09-21 15:01:25 +0200

    fix: VeeValidationMixin to support customCountryAnswers backend validation

--- frontend/components/mixins/VeeValidationMixin.js
+++ frontend/components/mixins/VeeValidationMixin.js
@@ -17,7 +17,8 @@ export default {
   },
   data () {
     return {
-      scopes: []
+      scopes: [],
+      customCountryErrors: []
     };
   },
   watch: {
@@ -27,38 +28,12 @@ export default {
         this.errors.clear();
         this.scopes.forEach(s => this.errors.clear(s));
         this.scopes = [];
-        for (let field in errors) {
-          const item = errors[field];
-          if (Array.isArray(item)) {
-            const first = item[0];
-            if (first.constructor === Object) {
-              item.forEach((innerError, key) => {
-                const scope = errorLibrary[field] + key;
-                this.scopes.push(scope);
-                for (let innerField in innerError) {
-                  this.errors.add({
-                    field: innerField,
-                    msg: innerError[innerField][0],
-                    scope
-                  });
-                }
-              });
-            } else {
-              this.errors.add({
-                field,
-                msg: first
-              });
-            }
-          } else {
-            for (let inner in item) {
-              if (inner === 'non_field_errors') {
-                this.errors.add({
-                  field,
-                  msg: item.non_field_errors[0]
-                });
-              }
-            }
-          }
+        this.customCountryErrors = [];
+        if (errors && errors.project) {
+          this.coreProjectErrorHandling(errors.project);
+        }
+        if (errors && errors.country_custom_answers) {
+          this.countryCustomAnswersErrorHandling(errors.country_custom_answers);
         }
       }
     }
@@ -70,6 +45,54 @@ export default {
     },
     clear () {
       this.errors.clear();
+    },
+    countryCustomAnswersErrorHandling (errors) {
+      if (errors && Array.isArray(errors)) {
+        this.customCountryErrors = errors;
+      } else {
+        for (let key in errors) {
+          this.errors.add({
+            field: 'answer',
+            scope: 'country_custom_question_' + key,
+            msg: errors[key][0]
+          });
+        }
+      }
+    },
+    coreProjectErrorHandling (errors) {
+      for (let field in errors) {
+        const item = errors[field];
+        if (Array.isArray(item)) {
+          const first = item[0];
+          if (first.constructor === Object) {
+            item.forEach((innerError, key) => {
+              const scope = errorLibrary[field] + key;
+              this.scopes.push(scope);
+              for (let innerField in innerError) {
+                this.errors.add({
+                  field: innerField,
+                  msg: innerError[innerField][0],
+                  scope
+                });
+              }
+            });
+          } else {
+            this.errors.add({
+              field,
+              msg: first
+            });
+          }
+        } else {
+          for (let inner in item) {
+            if (inner === 'non_field_errors') {
+              this.errors.add({
+                field,
+                msg: item.non_field_errors[0]
+              });
+            }
+          }
+        }
+      }
     }
   }
 };
