b20bdaba186da14edfffdf248804a988f076c1ee
Gyorgy Fekete <f@pulilab.com>
2018-09-27 18:25:55 +0200

    go through donors and error handling on draft

--- django/project/views.py
+++ django/project/views.py
@@ -343,23 +343,22 @@ class ProjectDraftViewSet(TeamTokenAuthMixin, ViewSet):
                 if not country_answers.is_valid():
                     errors['country_custom_answers'] = country_answers.errors
 
-        if data_serializer.validated_data.get('donors'):
-            if 'donor_custom_answers' not in request.data:
-                raise ValidationError({'non_field_errors': 'Donor answers are missing'})
-            for donor_id in data_serializer.validated_data['donors']:
+        for donor_id in data_serializer.validated_data.get('donors'):
+            donor = Donor.objects.filter(id=donor_id).first()
+            if donor and donor.donor_questions.exists():
+                if 'donor_custom_answers' not in request.data:
+                    raise ValidationError({'non_field_errors': 'Donor answers are missing'})
                 if str(donor_id) not in request.data['donor_custom_answers']:
                     raise ValidationError({'non_field_errors': 'Donor answers are missing'})
-                donor = Donor.objects.get(id=donor_id)
-                if donor and donor.donor_questions.exists():
-                    donor_answers = DonorCustomAnswerSerializer(data=request.data['donor_custom_answers'][str(donor_id)], many=True,
-                                                                context=dict(question_queryset=donor.donor_questions, is_draft=True))
+                donor_answers = DonorCustomAnswerSerializer(data=request.data['donor_custom_answers'][str(donor_id)], many=True,
+                                                            context=dict(question_queryset=donor.donor_questions, is_draft=True))
 
-                    if not donor_answers.is_valid():
-                        errors.setdefault('donor_custom_answers', {})
-                        errors['donor_custom_answers'].setdefault(donor_id, {})
-                        errors['donor_custom_answers'][donor_id] = donor_answers.errors
-                    else:
-                        all_donor_answers.append((donor_id, donor_answers))
+                if not donor_answers.is_valid():
+                    errors.setdefault('donor_custom_answers', {})
+                    errors['donor_custom_answers'].setdefault(donor_id, {})
+                    errors['donor_custom_answers'][donor_id] = donor_answers.errors
+                else:
+                    all_donor_answers.append((donor_id, donor_answers))
 
         if errors:
             return Response(errors, status=status.HTTP_400_BAD_REQUEST)
