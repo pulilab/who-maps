b50e5f8c7c06d18d26db5ad78c26d13854274810
Nicolò Maria Mezzopera <nico@pulilab.com>
2018-09-28 16:01:36 +0200

    feat: dedicated component to display custom answers on the table

---
+++ frontend/components/dashboard/CustomAnswersCell.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="CustomAnswersCell">
+    <ul v-if="type > 3">
+      <li
+        v-for="(v, index) in values"
+        :key="index"
+      >
+        {{ v }}
+      </li>
+    </ul>
+    <p v-if="type < 4">
+      {{ values[0] }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    row: {
+      type: Object,
+      default: () => ({})
+    },
+    id: {
+      type: Number,
+      default: null
+    },
+    type: {
+      type: Number,
+      default: null
+    },
+    donorId: {
+      type: Number,
+      default: null
+    }
+  },
+  computed: {
+    source () {
+      return this.donorId ? 'donor_answers' : 'country_answers';
+    },
+    values () {
+      if (this.id && this.row && this.row[this.source]) {
+        const module = this.row[this.source];
+        if (this.donorId) {
+          return module[this.donorId] && module[this.donorId][this.id] ? module[this.donorId][this.id] : [];
+        }
+        return module[this.id] ? module[this.id] : [];
+      }
+      return [];
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
