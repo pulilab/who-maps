52709122d5f17726e0aa5400d6746aed52b2b5c3
Gyorgy Fekete <f@pulilab.com>
2018-10-01 13:35:41 +0200

    approvals by country url

--- django/project/urls.py
+++ django/project/urls.py
@@ -82,4 +82,9 @@ urlpatterns = [
             'get': 'list',
         }),
         name="project-map"),
+    path(r'approvals/<int:country_id>/',
+         view=views.ProjectApprovalViewSet.as_view({
+             'get': 'list'
+         }),
+         name="approval"),
 ]
