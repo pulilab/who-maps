26522d78b7cb14e121b93a1daedec5c90be66979
Nicolò Maria Mezzopera <nico@pulilab.com>
2018-10-05 09:02:16 +0200

    fix: better stripe width calculation
    fixes #1022

--- frontend/components/common/map/TabbedCardProjectList.vue
+++ frontend/components/common/map/TabbedCardProjectList.vue
@@ -86,7 +86,9 @@ export default {
           const tabsContainer = this.$el.querySelector('.el-tabs__nav-scroll').getBoundingClientRect();
           const stripeLeftCorner = tabNameBox.left - tabsContainer.left;
           const paddingLeft = parseInt(getComputedStyle(tabName).paddingLeft, 10);
-          const stripeWidth = tabNameBox.width - paddingLeft;
+          const paddingRight = parseInt(getComputedStyle(tabName).paddingRight, 10);
+          const padding = paddingLeft > paddingRight ? paddingLeft : paddingRight;
+          const stripeWidth = tabNameBox.width - padding;
           const stripeTranslate = stripeLeftCorner === 0 ? 0 : stripeLeftCorner + paddingLeft;
           stripe.style.width = `${stripeWidth}px`;
           stripe.style.transform = `translate(${stripeTranslate}px)`;
