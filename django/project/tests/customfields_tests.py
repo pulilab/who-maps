357025bc807adb6a230485876ad5251adaf0ff89
Gyorgy Fekete <f@pulilab.com>
2018-10-24 14:59:00 +0200

    fix country test

--- django/project/tests/customfields_tests.py
+++ django/project/tests/customfields_tests.py
@@ -175,22 +175,23 @@ class CustomFieldTests(SetupTests):
                                                   type=CountryCustomQuestion.TEXT)
         q2 = CountryCustomQuestion.objects.create(question="test multi", country_id=self.country_id,
                                                   type=CountryCustomQuestion.MULTI)
-        url = reverse("country-custom-answer",
+        url = reverse("project-publish",
                       kwargs={
                           "country_id": self.country_id,
                           "project_id": self.project_id
                       })
-        data = [dict(question_id=q1.id, answer=['1', '2'], draft=False)]
+        data = copy(self.project_data)
+        data.update({"country_custom_answers": [dict(question_id=q1.id, answer=['1', '2'])]})
 
-        response = self.test_user_client.post(url, data=data, format='json')
+        response = self.test_user_client.put(url, data=data, format='json')
         self.assertEqual(response.status_code, 400)
-        self.assertEqual(response.json(), [{'answer': ['There must be 1 answer only.']}])
+        self.assertEqual(response.json()['country_custom_answers'], [{'answer': ['There must be 1 answer only.']}])
 
-        data = [dict(question_id=q2.id, answer=['1', '2'], draft=False)]
+        data.update({"country_custom_answers": [dict(question_id=q2.id, answer=['1', '2'])]})
 
-        response = self.test_user_client.post(url, data=data, format='json')
+        response = self.test_user_client.put(url, data=data, format='json')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json(), [{'question_id': q2.id, 'answer': ['1', '2'], 'draft': False}])
+        self.assertEqual(response.json()['published']['country_custom_answers'], {str(q2.id): ['1', '2']})
 
     def test_country_answer_update_existing_answer(self):
         q = CountryCustomQuestion.objects.create(question="test", country_id=self.country_id)
