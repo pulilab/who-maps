53d5ba51a87b9be35e5c923cacc80af9d57e10a8
Gyorgy Fekete <f@pulilab.com>
2018-10-24 18:24:38 +0200

    csv export success test

--- django/project/tests/csv_export_tests.py
+++ django/project/tests/csv_export_tests.py
@@ -7,3 +7,19 @@ def test_csv_export_failed(self):
     url = reverse("csv-export")
     response = self.test_user_client.post(url, {"ids": [1, 2]}, format="json")
     self.assertEqual(response.status_code, 404)
+
+def test_csv_export_success(self):
+    url = reverse("csv-export")
+    response = self.test_user_client.post(url, {"ids": [1, 2, Project.objects.get().id]}, format="json")
+    self.assertEqual(response.status_code, 200)
+    headers = dict(response.items())
+    self.assertEqual(headers['Content-Type'], 'text/csv')
+    self.assertEqual(headers['Content-Disposition'], 'attachment; filename="csv.csv"')
+    self.assertContains(response, "Test Project1")
+    self.assertContains(response, "a@a.com")
+    self.assertContains(response, "National Level Deployment: [Clients: 20000, Health Workers: 0, Facilities: 0]")
+    self.assertContains(response, "District: dist1 [Clients: 20, Health Workers: 5, Facilities: 4], "
+                                  "District: dist2 [Clients: 10, Health Workers: 2, Facilities: 8]")
+    self.assertContains(response, "District: ward1 [Clients: 209, Health Workers: 59, Facilities: 49], "
+                                  "District: ward2 [Clients: 109, Health Workers: 29, Facilities: 89]")
+    self.assertContains(response, "Donor1, Donor2")
