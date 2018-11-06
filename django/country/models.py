639524de367517360b69df8addc36ea61a07a68c
Gyorgy Fekete <f@pulilab.com>
2018-11-06 15:21:18 +0100

    signal imports

--- django/country/models.py
+++ django/country/models.py
@@ -1,6 +1,8 @@
 from django.contrib.postgres.fields import JSONField
 from django.contrib.postgres.fields.array import ArrayField
 from django.db import models
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 from django.utils.translation import ugettext_lazy as _
 from django.core.validators import MinLengthValidator
 from ordered_model.models import OrderedModel
