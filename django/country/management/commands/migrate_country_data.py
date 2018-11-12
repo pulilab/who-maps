2c1eb119690411ebcc3ff45733ba4c7afb766453
Gyorgy Fekete <f@pulilab.com>
2018-11-12 17:37:24 +0100

    flake fixes

--- django/country/management/commands/migrate_country_data.py
+++ django/country/management/commands/migrate_country_data.py
@@ -5,6 +5,7 @@ from country.models import Country
 
 FILENAME = 'countries-12-11-2018.json'
 
+
 class Command(BaseCommand):
     help = "Imports map data of countries"
 
@@ -14,13 +15,14 @@ class Command(BaseCommand):
             countries = json.load(countries)
             for c in countries:
                 try:
-                    country, created = Country.objects.get_or_create(code=c['fields']['code'],
-                                                                    defaults={'name': c['fields']['name'],
-                                                                              'name_en': c['fields']['name_en'],
-                                                                              'region': c['fields']['region'],
-                                                                              'map_data': c['fields']['map_data'],
-                                                                              'lat': c['fields']['lat'],
-                                                                              'lon': c['fields']['lon']})
+                    country, created = \
+                        Country.objects.get_or_create(code=c['fields']['code'],
+                                                      defaults={'name': c['fields']['name'],
+                                                                'name_en': c['fields']['name_en'],
+                                                                'region': c['fields']['region'],
+                                                                'map_data': c['fields']['map_data'],
+                                                                'lat': c['fields']['lat'],
+                                                                'lon': c['fields']['lon']})
                     if not created:
                         country.name = c['fields']['name']
                         country.name_en = c['fields']['name_en']
@@ -30,5 +32,4 @@ class Command(BaseCommand):
                         country.lon = c['fields']['lon']
                         country.save()
                 except KeyError:
-                    import ipdb; ipdb.set_trace()
-
+                    raise
