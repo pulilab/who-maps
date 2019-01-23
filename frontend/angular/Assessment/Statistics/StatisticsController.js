5d8ea2cdc09659ad1e0a8c09fe0fc934f7925e90
Nicolò Maria Mezzopera <nico@pulilab.com>
2019-01-23 09:09:09 +0100

    fix: connect the snapshot button to snapshot functionality in the store.

--- frontend/angular/Assessment/Statistics/StatisticsController.js
+++ frontend/angular/Assessment/Statistics/StatisticsController.js
@@ -12,13 +12,23 @@ class StatisticsController {
     this.projectData = window.$nuxt.$store.getters['projects/getCurrentProject'];
     this.userProfile = window.$nuxt.$store.getters['user/getProfile'];
     this.currentVersion = window.$nuxt.$store.getters['projects/getToolkitVersions'].length;
+
+    this.unsubscribe = window.$nuxt.$store.watch((state, getters) => getters['projects/getToolkitVersions'],
+      (version) => {
+        this.scope.$evalAsync(() => {
+          this.currentVersion = version.length;
+        });
+      });
   }
 
   onDestroy () {
+    if (this.unsubscribe) {
+      this.unsubscribe();
+    }
   }
 
   snapShot () {
-    return this.snapShotProject();
+    window.$nuxt.$store.dispatch('projects/snapShotProject');
   }
 
   goToEditProject () {
