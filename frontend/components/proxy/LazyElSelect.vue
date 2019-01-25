a4a5f82dc047f5c3178c459d2a384debdb506d5b
Nicolò Maria Mezzopera <nico@pulilab.com>
2019-01-25 18:07:05 +0100

    feat: lazy-el-select proxy component to load dropdown elements only when needed

---
+++ frontend/components/proxy/LazyElSelect.vue
@@ -0,0 +1,53 @@
+<template>
+  <el-select
+    v-bind="propsAndAttrs"
+    v-on="listeners"
+  >
+    <template v-if="open">
+      <slot />
+    </template>
+  </el-select>
+</template>
+
+<script>
+export default {
+  name: 'LazyElSelect',
+  props: {
+    value: {
+      type: null,
+      required: true
+    }
+  },
+  data () {
+    return {
+      open: false
+    };
+  },
+  computed: {
+    propsAndAttrs () {
+      return { ...this.$props, ...this.$attrs };
+    },
+    listeners () {
+      return { ...this.$listeners, 'visible-change': this.onVisibleChange };
+    }
+  },
+  mounted () {
+    if (this.value) {
+      window.requestAnimationFrame(() => {
+        this.open = true;
+      });
+    }
+  },
+  methods: {
+    onVisibleChange (value) {
+      if (value && !this.open) {
+        this.open = true;
+      }
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
--- frontend/plugins/element.js
+++ frontend/plugins/element.js
@@ -1,7 +1,9 @@
 import Vue from 'vue';
 import Element from 'element-ui';
 import locale from 'element-ui/lib/locale/lang/en';
+import LazyElSelect from '@/components/proxy/LazyElSelect';
 
 export default () => {
   Vue.use(Element, { locale });
+  Vue.component(LazyElSelect.name, LazyElSelect);
 };
