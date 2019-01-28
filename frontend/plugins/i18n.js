eb3fc347e9d79109bfe63669fdb6ae5a538c0bc9
Nicolò Maria Mezzopera <nico@pulilab.com>
2019-01-28 20:28:07 +0100

    fix: discard custom implementation and use only $t inside translation mixin

--- frontend/plugins/i18n.js
+++ frontend/plugins/i18n.js
@@ -6,21 +6,7 @@ Vue.component('translate', TranslateWrapper);
 Vue.mixin({
   methods: {
     $gettext (word, parameters) {
-      // safely search for translation string if not found default to word
-      let msg = this.$i18n &&
-      this.$i18n.locale &&
-      this.$i18n.messages &&
-      this.$i18n.messages[this.$i18n.locale] &&
-      this.$i18n.messages[this.$i18n.locale][word]
-        ? this.$i18n.messages[this.$i18n.locale][word]
-        : word;
-
-      if (parameters) {
-        for (let k in parameters) {
-          msg = msg.replace(`{${k}}`, parameters[k]);
-        }
-      }
-      return msg;
+      return this.$t(word, parameters);
     }
   }
 });
