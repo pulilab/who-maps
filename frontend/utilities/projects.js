b6b60594356ddbfbc591321b5dc391634b3a2146
Nicolò Maria Mezzopera <nico@pulilab.com>
2019-02-06 16:25:58 +0100

    feat: refactor project clean state to utils to DRY the code

--- frontend/utilities/projects.js
+++ frontend/utilities/projects.js
@@ -16,3 +16,41 @@ export const fetchProjectData = async (store, params, error) => {
     return Promise.reject(e);
   }
 };
+
+export const projectFields = () => ({
+  name: null,
+  organisation: null,
+  country: null,
+  geographic_scope: null,
+  implementation_overview: null,
+  start_date: null,
+  end_date: null,
+  contact_name: null,
+  contact_email: null,
+  team: [],
+  viewers: [],
+  platforms: [],
+  digitalHealthInterventions: [],
+  health_focus_areas: [],
+  hsc_challenges: [],
+  his_bucket: [],
+  coverageType: 1,
+  coverage: [],
+  coverageData: {},
+  coverage_second_level: [],
+  national_level_deployment: {
+    health_workers: 0,
+    clients: 0,
+    facilities: 0
+  },
+  government_investor: null,
+  implementing_partners: [],
+  donors: [],
+  implementation_dates: null,
+  licenses: [],
+  repository: null,
+  mobile_application: null,
+  wiki: null,
+  interoperability_links: {},
+  interoperability_standards: []
+});
