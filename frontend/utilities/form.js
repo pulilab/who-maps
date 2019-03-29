b0890ce8fd3d46f94273c1e3009c90dc292e74a3
Nicolò Maria Mezzopera <nico@pulilab.com>
2019-03-29 09:29:12 +0100

    fix:  async keep the local state in sync with store

--- frontend/utilities/form.js
+++ frontend/utilities/form.js
@@ -7,16 +7,25 @@ export const mapGettersActions = (collection) => {
     const [module, getterName, setterName, waitTime, skipGetter] = collection[item];
     const getter = module + '/' + getterName;
     const setter = module + '/' + setterName;
-    const debounceWait = waitTime || 0;
     const localValue = Vue.observable({
       value: null
     });
-    const setFn = () => localValue.store.dispatch(setter, localValue.value);
-    const set = debounceWait !== null ? debounce(setFn, debounceWait) : setFn;
+    const setFn = () => {
+      localValue.store.dispatch(setter, localValue.value);
+      debounceUpdate();
+    };
+    const debounceUpdate = debounce(() => {
+      if (localValue && localValue.store) {
+        localValue.value = localValue.store.getters[getter];
+      }
+    }, waitTime * 1.5);
+
+    const set = waitTime !== null ? debounce(setFn, waitTime) : setFn;
     result[item] = {
       localValue,
       get () {
         if (skipGetter && localValue.value !== null) {
+          debounceUpdate();
           return localValue.value;
         }
         return this.$store.getters[getter];
