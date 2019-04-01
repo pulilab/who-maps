2e45f40e94033243c844b06d7055486b0df25ded
Nicolò Maria Mezzopera <nico@pulilab.com>
2019-04-01 14:57:23 +0200

    chore: rework store

--- frontend/store/admin/import.js
+++ frontend/store/admin/import.js
@@ -5,10 +5,11 @@ export const getters = {
   getQueue: state => state.queue
 };
 export const actions = {
-  async loadQueue ({ commit }) {
-    const { data } = await this.$axios.get('/api/projects/import/');
-    // const filtered = data.filter(q => q.rows.some(r => !r.project));
-    commit('SET_QUEUE', data);
+  async loadQueue ({ commit, state }) {
+    if (!state.queue || state.queue.length === 0) {
+      const { data } = await this.$axios.get('/api/projects/import/');
+      commit('SET_QUEUE', data);
+    }
   },
   async addDataToQueue ({ commit, state }, imported) {
     const { data } = await this.$axios.post(`api/projects/import/`, imported);
@@ -17,19 +18,12 @@ export const actions = {
       data
     ];
     commit('SET_QUEUE', newQueue);
-  },
-  async updateQueueItem ({ commit, state }, item) {
-    const { data } = await this.$axios.patch(`/api/projects/import/${item.id}/`, { ...item, id: null });
-    const index = state.queue.findIndex(i => i.id === item.id);
-    commit('UPDATE_QUEUE', { data, index });
+    return data;
   }
 };
 
 export const mutations = {
   SET_QUEUE: (state, queue) => {
     state.queue = queue;
-  },
-  UPDATE_QUEUE: (state, { data, index }) => {
-    state.queue.splice(index, 1, data);
   }
 };
