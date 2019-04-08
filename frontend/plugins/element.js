e8f0f47b7bc3df49920972421a23031a3c4358f7
Nicolò Maria Mezzopera <nico@pulilab.com>
2019-04-08 15:47:58 +0200

    chore: load character count input with element-ui

--- frontend/plugins/element.js
+++ frontend/plugins/element.js
@@ -2,8 +2,10 @@ import Vue from 'vue';
 import Element from 'element-ui';
 import locale from 'element-ui/lib/locale/lang/en';
 import LazyElSelect from '@/components/proxy/LazyElSelect';
+import CharacterCountInput from '@/components/proxy/CharacterCountInput';
 
 export default () => {
   Vue.use(Element, { locale });
   Vue.component(LazyElSelect.name, LazyElSelect);
+  Vue.component(CharacterCountInput.name, CharacterCountInput);
 };
