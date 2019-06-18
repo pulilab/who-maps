6f99a53dec8b98976d3add1a816e8c5ed2cd60f2
Nicolò Maria Mezzopera <nico@pulilab.com>
2019-06-18 11:44:14 +0200

    feat: move mapping to utils and add add template

--- frontend/components/admin/import/ImportValidation.vue
+++ frontend/components/admin/import/ImportValidation.vue
@@ -23,6 +23,8 @@
 
 <script>
 import { draftRules, publishRules } from '@/utilities/projects';
+import { nameMapping } from '@/utilities/import';
+
 export default {
   props: {
     headers: {
@@ -59,33 +61,7 @@ export default {
     },
     nameMapping () {
       return {
-        name: 'Project Name',
-        organisation: 'Organisation',
-        geographic_scope: 'Geographic Scope',
-        implementation_overview: 'Narrative of the project',
-        start_date: 'Start Date',
-        end_date: 'End Date',
-        contact_name: 'Contact name',
-        contact_email: 'Contact email',
-        platforms: 'Software',
-        health_focus_areas: 'Health Focus Areas',
-        hsc_challenges: 'Health System Challenges',
-        his_bucket: 'Health Information Systems',
-        government_investor: 'Governament Contribution',
-        implementing_partners: 'Partners',
-        implementation_dates: 'Technology Deployment Date',
-        licenses: 'Licenses',
-        repository: 'Link to Code documentation',
-        wiki: 'Link to Wiki',
-        mobile_application: 'Link to application',
-        interoperability_links: 'Interoperability Links',
-        interoperability_standards: 'Interoperability Standards',
-        health_workers: 'Health Workers',
-        clients: 'Clients',
-        facilities: 'Facilities',
-        sub_level: 'Coverage Type',
-        digitalHealthInterventions: 'Digital Health Interventions (associated to the Software)'
-
+        ...nameMapping
       };
     },
     errors () {
