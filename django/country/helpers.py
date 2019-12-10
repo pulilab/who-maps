4b0bf1728a6be2b4e8968fbf255bef34557682ff
Barnabas <bh@pulilab.com>
2019-12-10 11:45:17 +0100

    remove comment

--- django/country/helpers.py
+++ django/country/helpers.py
@@ -6,7 +6,6 @@ import tempfile
 def create_temp_file_with_random_content(extension='.txt', size_in_bytes=10):
     tmp_file = tempfile.NamedTemporaryFile(suffix=extension)
 
-    # put some random content to the file
     with open(tmp_file.name, 'w') as f:
         letters = string.ascii_lowercase
         content = ''.join(random.choice(letters) for _ in range(size_in_bytes))
