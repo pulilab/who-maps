cc6f169867b70c95fd90db0711f116a7594307da
Gyorgy Fekete <f@pulilab.com>
2019-12-12 17:03:51 +0100

    functionality with typing

--- django/core/utils.py
+++ django/core/utils.py
@@ -8,3 +8,25 @@ from django.utils.translation import override, ugettext
 def send_mail_wrapper(subject: str, email_type: str,
                       to: Union[str, List[str]], language: str,
                       context: Dict = None) -> None:
+    if context is None:
+        context = {}
+
+    html_template = loader.get_template("email/master-inline.html")
+    context.update(
+        {
+            "type": email_type,
+            "language": language,
+            "email": to
+        }
+    )
+
+    with override(language):
+        html_message = html_template.render(context)
+        subject = ugettext(subject)
+
+    send_mail(
+        subject=subject,
+        message="",
+        from_email=settings.FROM_EMAIL,
+        recipient_list=[to] if type(to) == str else to,
+        html_message=html_message)
