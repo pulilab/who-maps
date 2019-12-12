9a63c84faa8c7b134e89e9280f023692cd9f70d2
Barnabas <bh@pulilab.com>
2019-12-12 16:45:23 +0100

    remove unused import

--- django/country/views.py
+++ django/country/views.py
@@ -11,7 +11,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.parsers import FormParser, MultiPartParser
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.viewsets import ModelViewSet
 
 from search.models import ProjectSearch
 from user.models import UserProfile
