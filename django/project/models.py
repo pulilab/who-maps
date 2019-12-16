32b464ab4a132eb36ad33bc77932e03c8be439c0
Gyorgy Fekete <f@pulilab.com>
2019-12-16 14:56:05 +0100

    lint fix

--- django/project/models.py
+++ django/project/models.py
@@ -4,7 +4,7 @@ from collections import namedtuple
 from django.db import models
 from django.db.models import Q
 from django.contrib.auth.models import User
-from django.contrib.postgres.fields import JSONField, ArrayField
+from django.contrib.postgres.fields import JSONField
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils.translation import ugettext_lazy as _
