84f87c4e47bd316de9b53b8645b253197e5541dc
Barnabas <bh@pulilab.com>
2020-01-03 15:37:31 +0100

    DonorImageTests moved to a separate class

--- django/country/tests/donor_tests.py
+++ django/country/tests/donor_tests.py
@@ -108,47 +108,6 @@ class DonorTests(DonorBaseTests):
         response = self.test_user_client.patch(url, data=data, HTTP_ACCEPT_LANGUAGE='en')
         self.assertEqual(response.status_code, 403)
 
-    def test_donor_admin_update_images_noperm_fail(self):
-        url = reverse("donor-image-detail", kwargs={"pk": self.donor.id})
-        cover = get_temp_image("cover")
-        logo = get_temp_image("logo")
-        data = {
-            "cover": cover,
-            "logo": logo
-        }
-        response = self.test_user_client.patch(url, data=data, format='multipart', HTTP_ACCEPT_LANGUAGE='en')
-        self.assertEqual(response.status_code, 403)
-
-    def test_donor_admin_update_images(self):
-        UserProfile.objects.filter(id=self.test_user['user_profile_id']).update(
-            account_type=UserProfile.SUPER_DONOR_ADMIN, donor=self.donor)
-        self.donor.super_admins.add(self.test_user['user_profile_id'])
-
-        url = reverse("donor-image-detail", kwargs={"pk": self.donor.id})
-        cover = get_temp_image("cover")
-        logo = get_temp_image("logo")
-        data = {
-            "cover": cover,
-            "logo": logo
-        }
-        response = self.test_user_client.patch(url, data=data, format='multipart', HTTP_ACCEPT_LANGUAGE='en')
-        self.assertEqual(response.status_code, 200)
-
-    def test_donor_superuser_update_images(self):
-        user = UserProfile.objects.get(id=self.test_user['user_profile_id']).user
-        user.is_superuser = True
-        user.save()
-
-        url = reverse("donor-image-detail", kwargs={"pk": self.donor.id})
-        cover = get_temp_image("cover")
-        logo = get_temp_image("logo")
-        data = {
-            "cover": cover,
-            "logo": logo
-        }
-        response = self.test_user_client.patch(url, data=data, format='multipart', HTTP_ACCEPT_LANGUAGE='en')
-        self.assertEqual(response.status_code, 200)
-
     def test_donor_admin_retrieve_admin_requests(self):
         UserProfile.objects.filter(id=self.test_user['user_profile_id']).update(
             account_type=UserProfile.DONOR_ADMIN, donor=self.donor)
