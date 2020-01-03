cc16cf7395ee9d2ed7649e65b67ceee244899124
Barnabas <bh@pulilab.com>
2020-01-03 16:23:00 +0100

    donor logo tests moved to a separate class

--- django/country/tests/donor_tests.py
+++ django/country/tests/donor_tests.py
@@ -5,7 +5,6 @@ from django.core import mail
 from django.urls import reverse
 from django.utils.translation import override
 
-from core.tests import get_temp_image
 from user.models import UserProfile
 
 
@@ -282,98 +281,3 @@ class DonorTests(DonorBaseTests):
         self.assertEqual(response.status_code, 200)
         self.donor.refresh_from_db()
         self.assertTrue(userprofile1.id not in self.donor.super_admins.all())
-
-    def test_donor_partner_logos_create_get(self):
-        UserProfile.objects.filter(id=self.test_user['user_profile_id']).update(
-            account_type=UserProfile.SUPER_DONOR_ADMIN, donor=self.donor)
-        self.donor.super_admins.add(self.test_user['user_profile_id'])
-
-        url = reverse("donor-partner-logo-list")
-        logo = get_temp_image("logo")
-        data = {
-            "donor": self.donor.id,
-            "image": logo
-        }
-        response = self.test_user_client.post(url, data)
-        self.assertEqual(response.status_code, 201)
-
-    def test_donor_superuser_partner_logos_create_get(self):
-        user = UserProfile.objects.get(id=self.test_user['user_profile_id']).user
-        user.is_superuser = True
-        user.save()
-
-        url = reverse("donor-partner-logo-list")
-        logo = get_temp_image("logo")
-        data = {
-            "donor": self.donor.id,
-            "image": logo
-        }
-        response = self.test_user_client.post(url, data)
-        self.assertEqual(response.status_code, 201)
-
-    def test_donor_superuser_partner_logos_delete(self):
-        user = UserProfile.objects.get(id=self.test_user['user_profile_id']).user
-        user.is_superuser = True
-        user.save()
-
-        url = reverse("donor-partner-logo-list")
-        logo = get_temp_image("logo")
-        data = {
-            "donor": self.donor.id,
-            "image": logo
-        }
-        response = self.test_user_client.post(url, data)
-        self.assertEqual(response.status_code, 201)
-
-        url = reverse("donor-partner-logo-detail", kwargs={"pk": response.json()["id"]})
-        response = self.test_user_client.delete(url)
-        self.assertEqual(response.status_code, 204)
-
-    def test_donor_partner_logos_list(self):
-        UserProfile.objects.filter(id=self.test_user['user_profile_id']).update(
-            account_type=UserProfile.SUPER_DONOR_ADMIN, donor=self.donor)
-        self.donor.super_admins.add(self.test_user['user_profile_id'])
-
-        url = reverse("donor-partner-logo-list")
-        logo1 = get_temp_image("donorlogo1")
-        data = {
-            "donor": self.donor.id,
-            "image": logo1
-        }
-        response = self.test_user_client.post(url, data)
-        self.assertEqual(response.status_code, 201)
-        logo2 = get_temp_image("donorlogo2")
-        data = {
-            "donor": self.donor.id,
-            "image": logo2
-        }
-        response = self.test_user_client.post(url, data)
-        self.assertEqual(response.status_code, 201)
-
-        url = reverse("donor-detail", kwargs={"pk": self.donor.id})
-        response = self.test_user_client.get(url, HTTP_ACCEPT_LANGUAGE='en')
-
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.json()['partner_logos']), 3)
-        self.assertEqual(response.json()['partner_logos'][1]['donor'], self.donor.id)
-        self.assertEqual(response.json()['partner_logos'][1]['image'], 'http://testserver/media/donorlogo1.png')
-        self.assertEqual(response.json()['partner_logos'][2]['donor'], self.donor.id)
-        self.assertEqual(response.json()['partner_logos'][2]['image'], 'http://testserver/media/donorlogo2.png')
-
-    def test_donor_partner_logos_delete(self):
-        UserProfile.objects.filter(id=self.test_user['user_profile_id']).update(
-            account_type=UserProfile.SUPER_DONOR_ADMIN, donor=self.donor)
-        self.donor.super_admins.add(self.test_user['user_profile_id'])
-
-        url = reverse("donor-partner-logo-list")
-        logo = get_temp_image("logo")
-        data = {
-            "donor": self.donor.id,
-            "image": logo
-        }
-        response = self.test_user_client.post(url, data)
-        self.assertEqual(response.status_code, 201)
-
-        url = reverse("donor-partner-logo-detail", kwargs={"pk": response.json()["id"]})
-        response = self.test_user_client.delete(url)
-        self.assertEqual(response.status_code, 204)
