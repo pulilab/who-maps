819c60f32291f15325b52068cbe4a454a33263a6
Barnabas <bh@pulilab.com>
2020-01-03 14:22:16 +0100

    move donor admin tests to a separate file

--- django/country/tests/tests.py
+++ django/country/tests/tests.py
@@ -857,23 +857,6 @@ class MockRequest:
     pass
 
 
-class DonorAdminTests(TestCase):
-    def setUp(self):
-        self.user = User.objects.create(username="alma", email="test@test.com", is_staff=True, is_superuser=True)
-        self.user.set_password('korte')
-        self.user.save()
-
-    def test_donor_code_min_length(self):
-        self.assertTrue(self.client.login(username='test@test.com', password='korte'))
-        url = reverse('admin:country_donor_add')
-        data = {
-            'name': 'Some donor',
-            'code': 'aa',
-        }
-        response = self.client.post(url, data, follow=True)
-        self.assertContains(response, "Ensure this value has at least 3 characters")
-
-
 class CountryAdminTests(TestCase):
     def setUp(self):
         self.site = AdminSite()
