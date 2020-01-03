49047c5b17f1fb2e6d323d181845243d525d2be3
Barnabas <bh@pulilab.com>
2020-01-03 14:37:26 +0100

    move country command tests to a separate file

--- django/country/tests/tests.py
+++ django/country/tests/tests.py
@@ -1,7 +1,5 @@
-import os
 from copy import copy
 from datetime import datetime
-from fnmatch import fnmatch
 from unittest.mock import patch
 
 from django.contrib.auth.models import User
@@ -13,15 +11,11 @@ from requests import RequestException
 
 from country.tests.base import CountryBaseTests
 from django.core import mail
-from django.core.management import call_command
-from django.test import TestCase
 
 from core.tests import get_temp_image
 from country.models import Country, Donor, CustomQuestion
 from project.models import TechnologyPlatform, DigitalStrategy
 from user.models import UserProfile, Organisation
-from django.utils.six import StringIO
-from django.conf import settings
 
 
 class CountryTests(CountryBaseTests):
@@ -849,41 +843,3 @@ class CountryTests(CountryBaseTests):
         response = self.test_user_client.post(url, data=data)
         self.assertEqual(response.status_code, 201)
         self.assertEqual(response.json()['options'], ['yes', 'maybe'])
-
-
-class CountryManagementCommandTest(TestCase):
-    null_topo = os.path.join(settings.STATIC_ROOT, 'country-geodata', 'null.json')
-    backup_folder = os.path.join(settings.MEDIA_ROOT, 'topojson-backups')
-
-    def setUp(self):
-        call_command('loaddata', 'null_land.json', verbosity=0)
-        try:
-            open(self.null_topo, 'x')
-        except Exception:  # pragma: no cover
-            pass
-
-    def tearDown(self):
-        if os.path.isfile(self.null_topo):  # pragma: no cover
-            os.remove(self.null_topo)
-        for file in os.listdir(self.backup_folder):
-            if fnmatch(file, '*null.json'):
-                os.remove(os.path.join(self.backup_folder, file))
-
-    def test_country_management_command_clean_maps(self):
-        out = StringIO()
-        call_command('clean_maps', stdout=out)
-        output = out.getvalue().strip()
-        self.assertEqual(output, 'No country code provided')
-
-        out = StringIO()
-        call_command('clean_maps', 'something', stdout=out)
-        output = out.getvalue().strip()
-        self.assertEqual(output, 'Selected country does not exist')
-
-        out = StringIO()
-        call_command('clean_maps', 'NULL', stdout=out)
-        output = out.getvalue().strip()
-        self.assertEqual(output, 'Removing unused features from Null Land geojson')
-
-        topo_stats = os.stat(self.null_topo)
-        self.assertTrue(topo_stats.st_size > 10)
