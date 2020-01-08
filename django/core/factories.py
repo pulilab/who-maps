c0283bf799bd8b485352e49a6a31aa0b3043919f
Barnabas <bh@pulilab.com>
2020-01-08 10:49:25 +0100

    UserFactory

---
+++ django/core/factories.py
@@ -0,0 +1,26 @@
+import factory
+from django.contrib.auth.models import User
+from django.utils import timezone
+from factory.faker import faker
+from factory.fuzzy import FuzzyDateTime
+
+
+class UserFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = User
+        django_get_or_create = ('username',)
+
+    username = factory.LazyAttribute(
+        lambda a: '{}'.format(faker.Faker().profile()['name'].lower().replace(' ', '_')))
+    email = factory.LazyAttribute(lambda a: f'{a.username}@example.com')
+    password = factory.PostGenerationMethodCall('set_password', 'test')
+    is_active = True
+    date_joined = FuzzyDateTime(
+        start_dt=timezone.now() - timezone.timedelta(days=10),
+        end_dt=timezone.now()
+    )
+
+    @factory.post_generation
+    def password(self, create, extracted, **kwargs):
+        if extracted:
+            self.set_password(extracted)
