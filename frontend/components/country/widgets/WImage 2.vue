28a983fdedcc79e2966dee8c2211bb6922660d7c
Alonso Lamas <alonsolamas@don-nico.localdomain>
2020-01-08 10:18:44 +0100

    Merge with development

---
+++ frontend/components/country/widgets/WImage 2.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    class="grid-content img-center"
+    :style="{backgroundImage: `url(${url})`}"
+  />
+</template>
+
+<script>
+export default {
+  name: 'WImage',
+  props: {
+    url: {
+      type: String,
+      required: false,
+      default: ''
+    }
+  },
+};
+</script>
+
+<style lang="less" scoped>
+  .grid-content.img-center {
+    min-height: 400px;
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+  }
+</style>
