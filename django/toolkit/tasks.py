6ababe07342b6810f11148d5e647f7ce17d39967
Barnabas <bh@pulilab.com>
2020-01-10 10:34:54 +0100

    notify only those users who receives notification

--- django/toolkit/tasks.py
+++ django/toolkit/tasks.py
@@ -23,7 +23,7 @@ def send_daily_toolkit_digest():
         toolkit = Toolkit.objects.get_object_or_none(project_id=project.id)
         has_passed_creation = toolkit.modified - toolkit.created > timezone.timedelta(seconds=10)
         if toolkit and has_passed_creation:
-            for profile in project.team.all():
+            for profile in project.team.filter(daily_toolkit_digest_notification=True):
                 context = {"project_id": project.id}
                 subject = _("Your Digital Health Atlas project assessment has been updated")
                 send_mail_wrapper(subject=subject,
