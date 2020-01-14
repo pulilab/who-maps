bcbcf2d714667a259dc21339d661e3d242950b3a
Gyorgy Fekete <f@pulilab.com>
2020-01-14 15:48:56 +0100

    remove nico

--- django/who_maps/settings.py
+++ django/who_maps/settings.py
@@ -376,7 +376,7 @@ elif SITE_ID == 4:
     ENVIRONMENT_COLOR = "orange"
     # redirect all emails to the forced addresses
     EMAIL_BACKEND = 'core.middleware.TestCeleryEmailBackend'
-    TEST_FORCED_TO_ADDRESS = ["t@pulilab.com", "nico@pulilab.com", "f@pulilab.com"]
+    TEST_FORCED_TO_ADDRESS = ["t@pulilab.com", "f@pulilab.com"]
     TOOLKIT_DIGEST_PERIOD = 1  # hours
     PROJECT_UPDATE_DIGEST_PERIOD = 1  # hours
     APPROVAL_DIGEST_PERIOD = 1  # hours
