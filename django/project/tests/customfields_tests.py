cb0e623b88ff0c74987498a6bd3ec631d3c9ebc6
Gyorgy Fekete <f@pulilab.com>
2020-02-03 15:26:05 +0100

    same test for donors

--- django/project/tests/customfields_tests.py
+++ django/project/tests/customfields_tests.py
@@ -661,3 +661,64 @@ class CustomFieldTests(SetupTests):
             elif send_mail_wrapper.call_args_list[i][1]['to'] == u2.email:
                 self.assertTrue(p2 in send_mail_wrapper.call_args_list[i][1]['context']['projects'])
 
+    @mock.patch('country.tasks.send_mail_wrapper', return_value=None)
+    def test_custom_donor_question_digest(self, send_mail_wrapper):
+        q = DonorCustomQuestionFactory(question="test", donor=self.d1, required=True)
+        DonorCustomQuestionFactory(question="test2", donor=self.d1, required=False)
+
+        url = reverse("project-publish",
+                      kwargs={
+                          "country_id": self.country_id,
+                          "project_id": self.project_id
+                      })
+        data = copy(self.project_data)
+        data.update({"donor_custom_answers": {str(self.d1.id): [dict(question_id=q.id, answer=["lol1"])]}})
+
+        response = self.test_user_client.put(url, data=data, format='json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()['published']['donor_custom_answers'], {str(self.d1.id): {str(q.id): ['lol1']}})
+        self.assertEqual(response.json()['draft']['donor_custom_answers'], {str(self.d1.id): {str(q.id): ['lol1']}})
+
+        project = Project.objects.last()
+        self.assertEqual(project.data['donor_custom_answers'], {str(self.d1.id): {str(q.id): ['lol1']}})
+        self.assertEqual(project.draft['donor_custom_answers'], {str(self.d1.id): {str(q.id): ['lol1']}})
+
+        u1 = UserFactory(username='username1', email='user1@user.org')
+        up1 = UserProfileFactory(name="USER1", user=u1, account_type=UserProfile.IMPLEMENTER,
+                                 country_id=self.country_id)
+
+        u2 = UserFactory(username='username2', email='user2@user.org')
+        up2 = UserProfileFactory(name="USER2", user=u2, account_type=UserProfile.IMPLEMENTER,
+                                 country_id=self.country_id)
+
+        p1 = Project.objects.get(id=self.project_id)
+        p1.team.add(up1)
+
+        p2 = ProjectFactory(name="published in country")
+        p2.team.add(self.userprofile)
+        p2.team.add(up2)
+
+        url = reverse("project-publish",
+                      kwargs={
+                          "country_id": self.country_id,
+                          "project_id": p2.id
+                      })
+
+        data = copy(self.project_data)
+        data['project']['name'] = "published in country"
+        data.update({"donor_custom_answers": {str(self.d1.id): [dict(question_id=q.id, answer=["lol2"])]}})
+        response = self.test_user_client.put(url, data=data, format='json')
+        self.assertEqual(response.status_code, 200, response)
+
+        send_new_custom_donor_question_digest.apply()
+
+        self.assertEqual(send_mail_wrapper.call_count, 3)
+
+        for i in range(3):
+            if send_mail_wrapper.call_args_list[i][1]['to'] == self.userprofile.user.email:
+                self.assertTrue(p1 in send_mail_wrapper.call_args_list[i][1]['context']['projects'])
+                self.assertTrue(p2 in send_mail_wrapper.call_args_list[i][1]['context']['projects'])
+            elif send_mail_wrapper.call_args_list[i][1]['to'] == u1.email:
+                self.assertTrue(p1 in send_mail_wrapper.call_args_list[i][1]['context']['projects'])
+            elif send_mail_wrapper.call_args_list[i][1]['to'] == u2.email:
+                self.assertTrue(p2 in send_mail_wrapper.call_args_list[i][1]['context']['projects'])
