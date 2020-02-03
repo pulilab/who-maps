98ded0aed1c6540af5811cec86819724e9ed18cb
Gyorgy Fekete <f@pulilab.com>
2020-02-03 15:21:51 +0100

    country question reminder

--- django/country/tasks.py
+++ django/country/tasks.py
@@ -6,3 +6,18 @@ from scheduler.celery import app
 @app.task(name='update_gdhi_data_task')
 def update_gdhi_data_task(country_code, override):
     call_command('gdhi', country_code=country_code, override=override)
+
+
+@app.task(name="send_new_custom_country_question_digest")
+def send_new_custom_country_question_digest():
+    """
+    Sends daily digest to all project's team members that a new custom question has been asked by the country
+    """
+    from country.models import CountryCustomQuestion
+    from search.models import ProjectSearch
+    from user.models import UserProfile
+
+    questions = CountryCustomQuestion.objects.filter(
+        created__gt=timezone.now() - timezone.timedelta(hours=settings.NEW_QUESTION_DIGEST_PERIOD))\
+        .select_related('country')
+    countries = set(questions.values_list('country', flat=True))
