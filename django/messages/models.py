ba2a5269999b9e83aa1715440fdcb8bad746db6a
Barnabas <bh@pulilab.com>
2020-03-16 13:18:57 +0100

    SystemMessage model

--- django/messages/models.py
+++ django/messages/models.py
@@ -1,1 +1,21 @@
-# from django.db import models
+from django.db import models
+from core.models import ExtendedModel
+
+
+class SystemMessage(ExtendedModel):
+    ALL_USERS = 'AU'
+    PROJECT_OWNERS = 'APO'
+    PROJECT_OWNERS_WITH_PUBLISHED_PROJECTS = 'APOWP'
+    RECEIVER_TYPE_CHOICES = (
+        (ALL_USERS, _('All users')),
+        (PROJECT_OWNERS, _('Project owners')),
+        (PROJECT_OWNERS_WITH_PUBLISHED_PROJECTS, _('Project owners with published projects')),
+    )
+
+    subject = models.CharField(max_length=200)
+    receiver_type = models.CharField(max_length=5, choices=RECEIVER_TYPE_CHOICES, default=ALL_USERS)
+    message = models.TextField()
+    receivers_number = models.IntegerField()
+
+    def __str__(self):
+        return "{} {}".format(self.created, self.subject)
