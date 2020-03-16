03c4415c87f67098e85ec6f2b16a0c2c32f5ff42
Barnabas <bh@pulilab.com>
2020-03-16 15:56:14 +0100

    SystemMessageAdmin

--- django/systemmessages/admin.py
+++ django/systemmessages/admin.py
@@ -1,1 +1,9 @@
-# from django.contrib import admin
+from django.contrib import admin
+
+from systemmessages.models import SystemMessage
+
+
+@admin.register(SystemMessage)
+class SystemMessageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'created', 'receiver_type', 'receivers_number', 'subject')
+    readonly_fields = ('receivers_number',)
