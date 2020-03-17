d3a4177bcf9a7233171e0fd345605cf8806a90e2
Barnabas <bh@pulilab.com>
2020-03-17 15:12:10 +0100

    ProjectStage model

--- django/project/models.py
+++ django/project/models.py
@@ -181,6 +181,40 @@ def on_create_init(sender, instance, created, **kwargs):
         ProjectApproval.objects.get_or_create(project_id=instance.id)
 
 
+class ProjectStage(ExtendedModel):
+    OPPORTUNITY_AND_IDEATION = 'OI'
+    PREPARATION = 'PREP'
+    ANALYSIS_AND_DESIGN = 'AD'
+    IMPLEMENTATION_PLANNING = 'IP'
+    DEVELOPING_OR_ADAPTING_SOLUTION = 'DAS'
+    PILOTING_AND_EVIDENCE_GENERATION = 'PEG'
+    PACKAGE_AND_CHAMPION = 'PC'
+    DEPLOYING = 'DEP'
+    SCALING_UP = 'SU'
+    SCALE_AND_HANDOVER = 'SH'
+    UNDER_REVIEW = 'UR'
+
+    STAGE_TYPE_CHOICES = (
+        (OPPORTUNITY_AND_IDEATION, _('Opportunity and Ideation')),
+        (PREPARATION, _('Preparation')),
+        (ANALYSIS_AND_DESIGN, _('Analysis and Design')),
+        (IMPLEMENTATION_PLANNING, _('Implementation Planning')),
+        (DEVELOPING_OR_ADAPTING_SOLUTION, _('Developing or Adapting Solution')),
+        (PILOTING_AND_EVIDENCE_GENERATION, _('Piloting and Evidence Generation')),
+        (PACKAGE_AND_CHAMPION, _('Package and Champion')),
+        (DEPLOYING, _('Deploying')),
+        (SCALING_UP, _('Scaling up')),
+        (SCALE_AND_HANDOVER, _('Scale and Handover')),
+        (UNDER_REVIEW, _('Under Review')),
+    )
+
+    project = models.ForeignKey(Project, related_name='stages', on_delete=models.CASCADE)
+    stage_type = models.CharField(max_length=5, choices=STAGE_TYPE_CHOICES)
+
+    def __str__(self):  # pragma: no cover
+        return "{} {} {}".format(self.project, self.stage_type, self.created)
+
+
 class ProjectApproval(ExtendedModel):
     project = models.OneToOneField('Project', related_name='approval', on_delete=models.CASCADE)
     user = models.ForeignKey(UserProfile, blank=True, null=True,
