ae2ab6d6720b0f547ddb5ae9a9c1ab21e082d116
Barnabas <bh@pulilab.com>
2020-03-20 14:39:02 +0100

    SystemMessageAdmin - has_change_permission set to False

--- django/systemmessages/admin.py
+++ django/systemmessages/admin.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.contrib import admin
 
 from systemmessages.models import SystemMessage
@@ -7,15 +6,10 @@ from systemmessages.models import SystemMessage
 @admin.register(SystemMessage)
 class SystemMessageAdmin(admin.ModelAdmin):
     list_display = ('id', 'created', 'receiver_type', 'receivers_number', 'subject')
+    readonly_fields = ['receivers_number']
+
+    def has_change_permission(self, request, obj=None):
+        return False
 
     def has_delete_permission(self, request, obj=None):
         return False
-
-    def get_readonly_fields(self, request, obj=None):
-        if obj:
-            languages = dict(settings.LANGUAGES).keys()
-            language_fields = [f'subject_{lang}' for lang in languages] + \
-                              [f'message_{lang}' for lang in languages]
-            return ['message', 'receiver_type', 'receivers_number', 'subject'] + language_fields
-        else:
-            return ['receivers_number']
