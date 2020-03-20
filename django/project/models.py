697f7f213e517e64c37c598016cdd8dfb53c0b5f
Barnabas <bh@pulilab.com>
2020-03-20 09:57:28 +0100

    ProjectStage model - date field change

--- django/project/models.py
+++ django/project/models.py
@@ -209,7 +209,7 @@ class ProjectStage(ExtendedModel):
     project = models.ForeignKey(Project, related_name='stages', on_delete=models.CASCADE)
     stage_type = models.CharField(max_length=5, choices=STAGE_TYPE_CHOICES)
     note = models.CharField(max_length=256, blank=True, null=True)
-    date = models.DateTimeField()
+    date = models.CharField(max_length=128)
 
     def __str__(self):  # pragma: no cover
         return "{} {} {}".format(self.project, self.stage_type, self.date)
