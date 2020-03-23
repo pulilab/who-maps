ff6610d9e5877fc1f3ee5885dd439292b169ee21
Barnabas <bh@pulilab.com>
2020-03-23 14:01:26 +0100

    modify test

--- django/project/tests/project_stage_tests.py
+++ django/project/tests/project_stage_tests.py
@@ -4,7 +4,6 @@ from django.utils import timezone
 from rest_framework import status
 from rest_framework.reverse import reverse
 
-from project.models import ProjectStage
 from project.tests.setup import SetupTests
 
 
@@ -18,6 +17,7 @@ class ProjectStageTests(SetupTests):
         data['project']['name'] = 'Test Project 100'
         data['project']['start_date'] = str((now - timezone.timedelta(days=10)).date())
         data['project']['end_date'] = str((now - timezone.timedelta(days=1)).date())
+        del data['project']['stages']
 
         # create project
         url = reverse("project-create", kwargs={"country_id": self.country_id})
@@ -26,24 +26,25 @@ class ProjectStageTests(SetupTests):
 
         project_id = response.json()['id']
 
-        # update stages
-        data['project']['stages'] = [
+        # add stages
+        stages = [
             {
-                'stage_type': ProjectStage.PREPARATION,
+                'id': 1,
                 'date': str((now - timezone.timedelta(days=10)).date()),
                 'note': 'preparation note'
             },
             {
-                'stage_type': ProjectStage.ANALYSIS_AND_DESIGN,
+                'id': 2,
                 'date': str((now - timezone.timedelta(days=7)).date()),
                 'note': 'analysis note'
             },
             {
-                'stage_type': ProjectStage.DEVELOPING_OR_ADAPTING_SOLUTION,
+                'id': 3,
                 'date': str((now - timezone.timedelta(days=3)).date()),
                 'note': 'developing note'
             }
         ]
+        data['project']['stages'] = stages
 
         url = reverse("project-draft", kwargs={"project_id": project_id, "country_id": self.country_id})
         response = self.test_user_client.put(url, data, format="json")
@@ -52,22 +53,18 @@ class ProjectStageTests(SetupTests):
         self.assertIn('stages', resp_data['draft'])
         self.assertEqual(len(resp_data['draft']['stages']), 3)
 
-        self.assertEqual(ProjectStage.objects.filter(project_id=project_id).count(), 3)
+        # try to publish without stages
+        del data['project']['stages']
+        url = reverse("project-publish", kwargs={"project_id": project_id, "country_id": self.country_id})
+        response = self.test_user_client.put(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST, response.json())
+        self.assertEqual(response.json(), {'project': {'stages': ['This field is required.']}})
 
-        # publish
-        data['project']['stages'].append(
-            {
-                'stage_type': ProjectStage.DEPLOYING,
-                'date': str((now - timezone.timedelta(days=3)).date()),
-                'note': 'deploying note'
-            }
-        )
-
+        # publish with stages
+        data['project']['stages'] = stages
         url = reverse("project-publish", kwargs={"project_id": project_id, "country_id": self.country_id})
         response = self.test_user_client.put(url, data, format="json")
         self.assertEqual(response.status_code, status.HTTP_200_OK, response.json())
         resp_data = response.json()
         self.assertIn('stages', resp_data['draft'])
-        self.assertEqual(len(resp_data['draft']['stages']), 4)
-
-        self.assertEqual(ProjectStage.objects.filter(project_id=project_id).count(), 4)
+        self.assertEqual(len(resp_data['draft']['stages']), 3)
