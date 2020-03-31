5dea73850e2541cb0cc82273591695c2f7724f50
Barnabas <bh@pulilab.com>
2020-03-31 11:19:12 +0200

    project test skeleton - use login command

---
+++ frontend/cypress/integration/project-tests.js
@@ -0,0 +1,11 @@
+describe('Create new project', function() {
+  beforeEach(function () {
+    cy.logIn();
+  })
+
+  it('Create Project with max field lengths', function() {
+    cy.log('Create Project with max field lengths');
+
+
+  })
+});
