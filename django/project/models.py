3c1e28aa720b0479b5290f5430ba64cd6f8e29ed
Barnabas <bh@pulilab.com>
2020-04-06 14:48:06 +0200

    tooltip field added

--- django/project/models.py
+++ django/project/models.py
@@ -200,6 +200,7 @@ class ProjectApproval(ExtendedModel):
 class Stage(ExtendedModel):
     name = models.CharField(max_length=128)
     order = models.IntegerField(blank=True, null=True)
+    tooltip = models.CharField(max_length=256, blank=True, null=True)
 
     class Meta:
         ordering = ['order', 'name']
