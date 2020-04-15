ac03d315850f76c10c83ee45381d62d3dac79a79
Barnabas <bh@pulilab.com>
2020-04-15 13:23:50 +0200

    signUp command

--- frontend/cypress/support/commands.js
+++ frontend/cypress/support/commands.js
@@ -25,6 +25,19 @@
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
 
+Cypress.Commands.add('signUp', () => {
+  cy.visit("http://localhost:3000");
+  cy.randomAlphaNumeric(10).then((response) => {
+    Cypress.env('testUser', "cypress_test_user_" + response + "@example.com");  // save user to env
+    cy.get('input[id="email"]').type(Cypress.env('testUser'));
+    cy.get('input[id="password"]').type('puli1234');
+    cy.get('input[id="passwordAgain"]').type('puli1234');
+    cy.contains('Sign up now').click();
+    cy.location('pathname', {timeout: 5000}).should('include', '/edit-profile');
+  });
+});
+
+
 Cypress.Commands.add('initData', () => {
   cy.request('http://localhost/api/cypress-test-data/')  .then((response) => {
     expect(response.status).to.eq(200);
