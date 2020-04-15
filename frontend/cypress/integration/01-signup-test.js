74e7e02beea47f769ec7b4641ee6f503bbe5d007
Barnabas <bh@pulilab.com>
2020-04-15 13:12:54 +0200

    faster signup

--- frontend/cypress/integration/01-signup-test.js
+++ frontend/cypress/integration/01-signup-test.js
@@ -6,11 +6,8 @@ describe('Signup test', function() {
   it('Signup test', function() {
     cy.log('Signup test');
     cy.visit("http://localhost:3000");
-    cy.contains('Signup').click();
-    cy.location('pathname', {timeout: 5000}).should('include', '/signup');
     cy.randomAlphaNumeric(10).then((response) => {
       Cypress.env('testUser', "cypress_test_user_" + response + "@example.com");  // save user to env
-      cy.wait(1000);  // this is necessary here because of some rendering
       cy.get('input[id="email"]').type(Cypress.env('testUser'));
       cy.get('input[id="password"]').type('puli1234');
       cy.get('input[id="passwordAgain"]').type('puli1234');
