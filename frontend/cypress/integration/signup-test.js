9e95544311a5b267b15212df2938e38cab6d0e35
Barnabas <bh@pulilab.com>
2020-04-15 11:48:13 +0200

    rewrite signup test to not use init

--- frontend/cypress/integration/signup-test.js
+++ frontend/cypress/integration/signup-test.js
@@ -1,18 +1,17 @@
 describe('Signup test', function() {
-  beforeEach(function () {
-    cy.initData();
-  })
-
   it('Signup test', function() {
     cy.log('Signup test');
-    cy.visit(Cypress.env('testData').url);
+    cy.visit("http://localhost:3000");
     cy.contains('Signup').click();
     cy.location('pathname', {timeout: 5000}).should('include', '/signup');
-    cy.wait(1000);  // this is necessary here because of some rendering
-    cy.get('input[id="email"]').type(Cypress.env('testData').signup_username);
-    cy.get('input[id="password"]').type('puli1234');
-    cy.get('input[id="passwordAgain"]').type('puli1234');
-    cy.contains('Sign up now').click();
-    cy.location('pathname', {timeout: 5000}).should('include', '/edit-profile');
+    cy.randomAlphaNumeric(10).then((response) => {
+      Cypress.env('testUser', "cypress_test_user_" + response + "@example.com");  // save user to env
+      cy.wait(1000);  // this is necessary here because of some rendering
+      cy.get('input[id="email"]').type(Cypress.env('testUser'));
+      cy.get('input[id="password"]').type('puli1234');
+      cy.get('input[id="passwordAgain"]').type('puli1234');
+      cy.contains('Sign up now').click();
+      cy.location('pathname', {timeout: 5000}).should('include', '/edit-profile');
+    });
   })
 });
