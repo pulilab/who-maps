590213a9c87b3579f88a8f82544eb4299cb3185b
Barnabas <bh@pulilab.com>
2020-05-18 15:28:22 +0200

    increase timeouts for pathname checks

--- e2e/cypress/integration/test.js
+++ e2e/cypress/integration/test.js
@@ -10,7 +10,7 @@ describe('End to end tests', function() {
   it('Fill user profile test', function() {
     cy.log('Fill user profile test');
     // after signup the page redirected to edit profile
-    cy.location('pathname', {timeout: 5000}).should('include', '/edit-profile');
+    cy.location('pathname', {timeout: 10000}).should('include', '/edit-profile');
     cy.get("input[data-vv-name=\"name\"]").type("Cypress User");
     cy.get("div[data-vv-name=\"organisation\"]").as('orgInput');
     cy.get("@orgInput").type("XYZXYZ");
@@ -21,6 +21,6 @@ describe('End to end tests', function() {
     cy.get("input[data-vv-name=\"title\"]").type("Cypress");
     cy.get("input[data-vv-name=\"linkedin\"]").type("http://linkedin.com/123456/");
     cy.contains("Save settings").click();
-    cy.location('pathname', {timeout: 5000}).should('include', '/dashboard');
+    cy.location('pathname', {timeout: 10000}).should('include', '/dashboard');
   })
 });
