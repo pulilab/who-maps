2eb5a4c24e6e4952664b87f97bf3b4e80e933c39
Alonso Lamas <al@pulilab.com>
2021-03-22 10:53:14 +0100

    Rebase with development

---
+++ frontend/components/project/sections/view/Custom.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <custom-readonly-field
+      v-for="item in items"
+      :id="item.id"
+      :key="item.id"
+      :question="item.question"
+      is-draft
+      :type="item.type"
+    />
+
+    <!-- :donor-id="item.id" -->
+  </div>
+</template>
+
+
+
+<script>
+import CustomReadonlyField from "@/components/project/CustomReadonlyField";
+
+export default {
+  components: {
+    CustomReadonlyField,
+  },
+  props: {
+    items: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+
---
+++ frontend/components/project/sections/view/Stages.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="graph">
+    <c-stages v-if="stages" />
+    <div v-else class="no-data">
+      <translate> No Data available </translate>
+    </div>
+  </div>
+</template>
+
+<script>
+import cStages from "@/components/common/charts/cStages";
+
+export default {
+  components: {
+    cStages,
+  },
+  computed: {
+    stages() {
+      return this.$store.getters["project/getStages"];
+    },
+  },
+};
+</script>
+
+<style lang="less">
+.graph {
+  margin: -50px 0 60px 0;
+  .no-data {
+    padding: 22px 74px 0px 40px;
+  }
+}
+</style>
