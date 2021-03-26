1d1ef2c3c67c77e30146e77b5a2e3ee92c93d0ac
Alonso Lamas <al@pulilab.com>
2021-03-26 09:43:57 +0100

    Remove purge css on build mode

--- frontend/lang/langReq.js
+++ frontend/lang/langReq.js
@@ -1,6 +1,11 @@
 import axios from 'axios'
 export default ln => {
-  const base = process.server && process.env.NODE_ENV === 'production' ? 'http://nginx:9010' : ''
+  const base =
+    process.server && process.env.NODE_ENV === 'production'
+      ? 'http://nginx:9010'
+      : ''
+  // const base = process.server && process.env.NODE_ENV === 'production' ? '' : ''
+
   return axios.get(`${base}/translation/json/`, {
     headers: {
       'Accept-Language': ln
