25d051259b5b1d242cd5ef4a8193f6d8e0c66831
lordy <dk@pulilab.com>
2021-03-29 13:42:54 +0200

    API to generate tokens

--- django/user/urls.py
+++ django/user/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
 from allauth.account.views import confirm_email
 from rest_framework_jwt.views import obtain_jwt_token
-
+from rest_framework.authtoken.views import obtain_auth_token
 import user.views as views
 
 router = DefaultRouter()
@@ -17,5 +17,6 @@ urlpatterns = [
     url(r"^rest-auth/", include("rest_auth.urls")),
     url(r"^rest-auth/registration/", include("rest_auth.registration.urls")),
     url(r'^api-token-auth/', obtain_jwt_token, name="api_token_auth"),
+    url(r'^external-api-token-auth/', obtain_auth_token, name="external_api_token_auth"),
     url(r"^email-confirmation/(?P<key>\w+)/$", confirm_email, name="account_confirm_email"),
 ]
