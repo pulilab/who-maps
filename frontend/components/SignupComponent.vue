c11b50032aa1423e5251fb4893e6169ed6c3f66b
paizai <paizai@gmail.com>
2021-04-20 00:17:20 +0200

    fixes I.

--- frontend/components/SignupComponent.vue
+++ frontend/components/SignupComponent.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="SingupComponent">
-    <el-card
-      :shadow="shadow"
-      :body-style="{ padding: '0px' }"
-    >
+    <el-card :shadow="shadow" :body-style="{ padding: '0px' }">
       <div slot="header">
         <template v-if="token">
           <translate>Set your password</translate>
@@ -28,10 +25,7 @@
             :label="$gettext('Email address') | translate"
             prop="email"
           >
-            <el-input
-              id="email"
-              v-model="signupForm.email"
-            />
+            <el-input id="email" v-model="signupForm.email" />
           </el-form-item>
 
           <el-form-item
@@ -58,37 +52,32 @@
               v-if="nonFieldErrors"
               class="el-form-item__error ModifiedFormError"
             >
-              <br>
+              <br />
               {{ nonFieldErrors }}
             </div>
           </el-form-item>
         </fieldset>
         <div class="CardActionsBottom">
-          <el-row
-            type="flex"
-            justify="space-between"
-            align="middle"
-          >
-            <el-col
-              :span="12"
-              class="SecondaryAction LoginLink"
-            >
+          <el-row type="flex" justify="space-between" align="middle">
+            <el-col :span="12" class="SecondaryAction LoginLink">
               <h6><translate>Already signed up?</translate></h6>
               <nuxt-link
-                :to="localePath({name: 'organisation-login', params: $route.params})"
+                :to="
+                  localePath({
+                    name: 'organisation-login',
+                    params: $route.params
+                  })
+                "
                 class="NuxtLink Small"
               >
                 <span><translate>Login here</translate></span>
               </nuxt-link>
             </el-col>
-            <el-col
-              :span="12"
-              class="PrimaryAction"
-            >
+            <el-col :span="12" class="PrimaryAction">
               <el-button
-                type="primary"
                 size="medium"
                 native-type="submit"
+                class="el-button--accent"
               >
                 <translate v-if="token">
                   Set Password
@@ -106,146 +95,179 @@
 </template>
 
 <script>
-import { mapActions } from 'vuex'
-import FormAPIErrorsMixin from '~/components/mixins/FormAPIErrorsMixin'
+import { mapActions } from "vuex";
+import FormAPIErrorsMixin from "~/components/mixins/FormAPIErrorsMixin";
 
 export default {
   mixins: [FormAPIErrorsMixin],
   props: {
     shadow: {
       type: String,
-      default: 'always'
+      default: "always"
     },
     token: {
       type: String,
-      default: '',
+      default: "",
       required: false
     },
     uid: {
       type: String,
-      default: '',
+      default: "",
       required: false
     },
     type: {
       type: String,
-      default: '',
+      default: "",
       required: false
     }
   },
-  data () {
+  data() {
     return {
       signupForm: {
-        email: '',
-        password1: '',
-        password2: ''
+        email: "",
+        password1: "",
+        password2: ""
       },
       rules: {
         email: (() => {
           return this.token
             ? []
             : [
-              { required: true, message: this.$gettext('This field is required'), trigger: 'blur' },
-              { type: 'email', message: this.$gettext('Has to be a valid email address'), trigger: 'blur' },
-              { validator: this.validatorGenerator('email'), trigger: 'blur' }
-            ]
+                {
+                  required: true,
+                  message: this.$gettext("This field is required"),
+                  trigger: "blur"
+                },
+                {
+                  type: "email",
+                  message: this.$gettext("Has to be a valid email address"),
+                  trigger: "blur"
+                },
+                { validator: this.validatorGenerator("email"), trigger: "blur" }
+              ];
         })(),
         password1: [
-          { required: true, message: this.$gettext('This field is required'), trigger: 'blur' },
-          { min: 8, message: this.$gettext('This field should be at least 8 characters'), trigger: 'blur' },
-          { validator: this.validatorGenerator('password1'), trigger: 'blur' }
+          {
+            required: true,
+            message: this.$gettext("This field is required"),
+            trigger: "blur"
+          },
+          {
+            min: 8,
+            message: this.$gettext(
+              "This field should be at least 8 characters"
+            ),
+            trigger: "blur"
+          },
+          { validator: this.validatorGenerator("password1"), trigger: "blur" }
         ],
         password2: [
-          { required: true, message: this.$gettext('This field is required'), trigger: 'blur' },
-          { validator: this.passwordMatching, trigger: 'blur' }
+          {
+            required: true,
+            message: this.$gettext("This field is required"),
+            trigger: "blur"
+          },
+          { validator: this.passwordMatching, trigger: "blur" }
         ]
       }
-    }
+    };
   },
   methods: {
     ...mapActions({
-      doSignup: 'user/doSignup',
-      dorResetPassword: 'user/dorResetPassword'
+      doSignup: "user/doSignup",
+      dorResetPassword: "user/dorResetPassword"
     }),
-    passwordMatching (rule, value, callback) {
-      value === this.signupForm.password1 ? callback() : callback(Error(this.$gettext('The password must match')))
+    passwordMatching(rule, value, callback) {
+      value === this.signupForm.password1
+        ? callback()
+        : callback(Error(this.$gettext("The password must match")));
     },
-    submitForm () {
-      this.token ? this.resetPassword() : this.signup()
+    submitForm() {
+      this.token ? this.resetPassword() : this.signup();
     },
-    signup () {
+    signup() {
       return this.setForm({
-        message: this.$gettext('User created succesfully'),
-        apiCall: () => this.doSignup({
-          account_type: 'I',
-          password1: this.signupForm.password1,
-          password2: this.signupForm.password2,
-          email: this.signupForm.email
-        }),
-        pathName: 'organisation-edit-profile'
-      })
+        message: this.$gettext("User created succesfully"),
+        apiCall: () =>
+          this.doSignup({
+            account_type: "I",
+            password1: this.signupForm.password1,
+            password2: this.signupForm.password2,
+            email: this.signupForm.email
+          }),
+        pathName: "organisation-edit-profile"
+      });
     },
-    resetPassword () {
+    resetPassword() {
       return this.setForm({
-        message: this.$gettext('Your password was reset successfully'),
-        apiCall: () => this.dorResetPassword({
-          new_password1: this.signupForm.password1,
-          new_password2: this.signupForm.password2,
-          uid: this.uid,
-          token: this.token,
-          errMessage: this.$gettext('The password reset link was invalid, possibly because it has already been used. Please request a new password reset.')
-        }),
-        pathName: 'organisation-login'
-      })
+        message: this.$gettext("Your password was reset successfully"),
+        apiCall: () =>
+          this.dorResetPassword({
+            new_password1: this.signupForm.password1,
+            new_password2: this.signupForm.password2,
+            uid: this.uid,
+            token: this.token,
+            errMessage: this.$gettext(
+              "The password reset link was invalid, possibly because it has already been used. Please request a new password reset."
+            )
+          }),
+        pathName: "organisation-login"
+      });
     },
-    setForm ({ message, apiCall, pathName }) {
-      this.deleteFormAPIErrors()
+    setForm({ message, apiCall, pathName }) {
+      this.deleteFormAPIErrors();
       this.$refs.form.validate(async valid => {
         if (valid) {
           try {
             // locale needs to be saved in this place due to i18n being unavailable right after the signup call
-            const locale = this.$i18n.locale
-            this.$nuxt.$loading.start()
-            await apiCall()
-            const path = this.localePath({ ...this.$route, name: pathName }, locale)
-            this.$router.push(path)
+            const locale = this.$i18n.locale;
+            this.$nuxt.$loading.start();
+            await apiCall();
+            const path = this.localePath(
+              { ...this.$route, name: pathName },
+              locale
+            );
+            this.$router.push(path);
             this.$message({
               message,
-              type: 'success',
+              type: "success",
               showClose: true
-            })
+            });
           } catch (e) {
-            console.log(e)
-            this.$nuxt.$loading.finish()
-            this.setFormAPIErrors(e)
-            this.$refs.form.validate(() => {})
+            console.log(e);
+            this.$nuxt.$loading.finish();
+            this.setFormAPIErrors(e);
+            this.$refs.form.validate(() => {});
           }
         }
-      })
+      });
     }
   }
-}
+};
 </script>
 
 <style lang="less">
-  @import "../assets/style/variables.less";
-  @import "../assets/style/mixins.less";
+@import "../assets/style/variables.less";
+@import "../assets/style/mixins.less";
 
-  .SingupComponent {
-    width: @cardSizeSmall;
-    min-height: calc(100vh - @topBarHeight - @actionBarHeight - @appFooterHeight - 160px);
-    margin: 80px auto;
+.SingupComponent {
+  width: @cardSizeSmall;
+  min-height: calc(
+    100vh - @topBarHeight - @actionBarHeight - @appFooterHeight - 160px
+  );
+  margin: 80px auto;
 
-    fieldset {
-      padding: 40px 80px;
-    }
+  fieldset {
+    padding: 40px 80px;
+  }
 
-    .SecondaryAction {
-      h6 {
-        margin: 0 0 2px;
-        font-size: @fontSizeSmall;
-        font-weight: 400;
-        color: @colorTextSecondary;
-      }
+  .SecondaryAction {
+    h6 {
+      margin: 0 0 2px;
+      font-size: @fontSizeSmall;
+      font-weight: 400;
+      color: @colorTextSecondary;
     }
   }
+}
 </style>
--- frontend/components/common/CountryChooser.vue
+++ frontend/components/common/CountryChooser.vue
@@ -19,13 +19,8 @@
           :src="countryFlag"
           alt="country flag"
           class="CountryFlag"
-        >
-        <fa
-          v-else
-          size="lg"
-          class="AllIcon CountryFlag"
-          icon="globe"
         />
+        <fa v-else size="lg" class="AllIcon CountryFlag" icon="globe" />
         <div class="CountryName">
           <template v-if="landingData">
             {{ landingData.name }}
@@ -39,15 +34,8 @@
     </el-button>
 
     <div class="Search-container">
-      <el-input
-        v-model="search"
-        size="small"
-        placeholder="Search country"
-      >
-        <i
-          slot="prefix"
-          class="el-input__icon "
-        >
+      <el-input v-model="search" size="small" placeholder="Search country">
+        <i slot="prefix" class="el-input__icon ">
           <fa icon="search" />
         </i>
       </el-input>
@@ -65,37 +53,24 @@
       >
         <div class="List-container CustomPopoverList">
           <ul>
-            <li
-              :class="{Active: active()}"
-              @click="selectCountry()"
-            >
-              <fa
-                size="lg"
-                class="AllIcon"
-                icon="globe"
-              />
+            <li :class="{ Active: active() }" @click="selectCountry()">
+              <fa size="lg" class="AllIcon" icon="globe" />
               <translate>All countries</translate>
-              <fa
-                icon="check"
-                class="check"
-              />
+              <fa icon="check" class="check" />
             </li>
             <li
               v-for="country in filteredCountries"
               :key="country.code"
-              :class="{Active: active(country.code)}"
+              :class="{ Active: active(country.code) }"
               @click="selectCountry(country)"
             >
               <img
                 :src="getCountryFlag(country.code)"
                 alt="country flag"
                 class="CountryInnerFlag"
-              >
-              {{ country.name }}
-              <fa
-                icon="check"
-                class="check"
               />
+              {{ country.name }}
+              <fa icon="check" class="check" />
             </li>
           </ul>
         </div>
@@ -105,136 +80,144 @@
 </template>
 
 <script>
-import { mapGetters, mapActions } from 'vuex'
+import { mapGetters, mapActions } from "vuex";
 
 export default {
-  data () {
+  data() {
     return {
-      search: '',
+      search: "",
       chooserOpen: false
-    }
+    };
   },
   computed: {
     ...mapGetters({
-      landingData: 'landing/getLandingPageData',
-      countries: 'countries/getCountries'
+      landingData: "landing/getLandingPageData",
+      countries: "countries/getCountries"
     }),
-    countryFlag () {
+    countryFlag() {
       if (this.landingData) {
-        return `/static/flags/${this.landingData.code.toLowerCase()}.png`
+        return `/static/flags/${this.landingData.code.toLowerCase()}.png`;
       }
-      return false
+      return false;
     },
-    filteredCountries () {
+    filteredCountries() {
       return this.countries.filter(country => {
-        return country.id !== process.env.GlobalCountryID && country.name.toLowerCase().indexOf(this.search.toLowerCase()) > -1
-      })
+        return (
+          country.id !== process.env.GlobalCountryID &&
+          country.name.toLowerCase().indexOf(this.search.toLowerCase()) > -1
+        );
+      });
     },
-    displayListHeight () {
+    displayListHeight() {
       if (this.filteredCountries.length > 9) {
-        return 306
+        return 306;
       }
-      return (this.filteredCountries.length + 1) * 34
+      return (this.filteredCountries.length + 1) * 34;
     }
   },
   methods: {
     ...mapActions({
-      setProjectBoxActiveGlobalTab: 'landing/setProjectBoxActiveGlobalTab'
+      setProjectBoxActiveGlobalTab: "landing/setProjectBoxActiveGlobalTab"
     }),
-    getCountryFlag (code) {
-      return `/static/flags/${code.toLowerCase()}.png`
+    getCountryFlag(code) {
+      return `/static/flags/${code.toLowerCase()}.png`;
     },
-    selectCountry (country) {
-      this.chooserOpen = false
-      this.setProjectBoxActiveGlobalTab(false)
-      const organisation = country ? country.code.toLowerCase() : '-'
-      const localised = this.localePath({ name: 'organisation', params: { organisation } })
-      this.$router.push(localised)
+    selectCountry(country) {
+      this.chooserOpen = false;
+      this.setProjectBoxActiveGlobalTab(false);
+      const organisation = country ? country.code.toLowerCase() : "-";
+      const localised = this.localePath({
+        name: "organisation",
+        params: { organisation }
+      });
+      this.$router.push(localised);
     },
-    active (code) {
+    active(code) {
       if (!this.landingData) {
-        return !code
+        return !code;
       }
-      return code === this.landingData.code
+      return code === this.landingData.code;
     }
   }
-}
+};
 </script>
 
 <style lang="less" scoped>
-  @import "../../assets/style/variables.less";
-  @import "../../assets/style/mixins.less";
+@import "../../assets/style/variables.less";
+@import "../../assets/style/mixins.less";
 
-  .AllIcon {
-    margin-right: 8px;
-    margin-left: 2px;
+.AllIcon {
+  margin-right: 8px;
+  margin-left: 2px;
+}
+.ChooserButton {
+  position: relative;
+  top: 1px;
+  padding: 0 !important;
+}
+.CustomPopover {
+  top: 15px !important;
+  .el-popover__title {
+    padding: 8px 16px !important;
   }
-  .ChooserButton {
-    padding: 0 !important;
-  }
-  .CustomPopover {
-    top: 15px !important;
-    .el-popover__title {
-      padding: 8px 16px !important;
+}
+.CountryHolder {
+  display: flex;
+  height: 24px;
+
+  .CountryFlag {
+    height: 16px;
+    margin-right: 6px;
+    padding: 3px 0;
+    &[src~="/who-logo-small.svg"] {
+      height: 20px;
+      padding: 2px 0 0 0;
     }
   }
-  .CountryHolder {
-    display: flex;
-    height: 24px;
+  .CountryName {
+    font-size: @fontSizeBase;
+    color: @colorBrandPrimary;
+    font-weight: 700;
+    line-height: 24px;
+    text-overflow: ellipsis;
+    max-width: 240px;
+    overflow: hidden;
+  }
+}
 
-    .CountryFlag {
+.Scroll-list {
+  overflow: hidden;
+  .List-container {
+    max-height: 299px;
+
+    li:first-child .CountryInnerFlag {
+      height: auto;
+    }
+
+    .CountryInnerFlag {
+      width: 23px;
       height: 16px;
       margin-right: 6px;
-      padding: 3px 0;
-      &[src~="/who-logo-small.svg"] {
-        height: 20px;
-        padding: 2px 0 0 0;
-      }
-    }
-    .CountryName {
-      font-size: @fontSizeBase;
-      color: @colorBrandPrimary;
-      font-weight: 700;
-      line-height: 24px;
-      text-overflow: ellipsis;
-      max-width: 240px;
-      overflow: hidden;
+      vertical-align: middle;
     }
   }
-
-  .Scroll-list {
-    overflow: hidden;
-    .List-container {
-      max-height: 299px;
-
-      li:first-child .CountryInnerFlag {
-        height: auto;
-      }
-
-      .CountryInnerFlag {
-        width: 23px;
-        height: 16px;
-        margin-right: 6px;
-        vertical-align: middle;
-      }
-    }
-    .CustomPopoverList ul li {
-      height: 34px !important;
-      line-height: 34px !important;
-      padding: 0 15px 0 15px !important;
-      &:hover {
-        background-color: @colorGrayLightest !important;
-        .check {
-          display: none;
-        }
+  .CustomPopoverList ul li {
+    height: 34px !important;
+    line-height: 34px !important;
+    padding: 0 15px 0 15px !important;
+    &:hover {
+      background-color: @colorGrayLightest !important;
+      .check {
+        display: none;
       }
     }
   }
-  .Search-container {
-    padding: 15px;
-    svg {
-      margin-left: 5px;
-      color: @colorTextPrimary;
-    }
+}
+.Search-container {
+  padding: 15px;
+  svg {
+    margin-left: 5px;
+    color: @colorTextPrimary;
   }
+}
 </style>
