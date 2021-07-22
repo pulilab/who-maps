9a70ffa260d542cceb43019192bca48fc358d153
David-Puli <dk@pulilab.com>
2021-07-22 12:47:39 +0200

    Fix linter

--- django/kpiexport/admin.py
+++ django/kpiexport/admin.py
@@ -1,10 +1,4 @@
-from adminsortable2.admin import SortableAdminMixin
 from django.contrib import admin
-from django.contrib.admin import SimpleListFilter
-from django.db.models import Q
-from django.utils.html import mark_safe
-from core.admin import AllObjectsAdmin
-from country.models import Country
 from .models import AuditLogTokens, AuditLogProjectStatus, AuditLogProjectStages, AuditLogUsers
 
 from django.conf import settings
@@ -52,5 +46,3 @@ class AuditLogProjectStagesAdmin(TestingOnlyAdmin):
         'date', 'modified', 'country', 'stages',
     ]
     list_filter = (('date', DateRangeFilter), 'country')
-
-
