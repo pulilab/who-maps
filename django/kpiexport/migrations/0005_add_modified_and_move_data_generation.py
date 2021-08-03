778bfa68037c2a81b590b3a1b93cc4600d4004ac
David-Puli <dk@pulilab.com>
2021-08-03 12:08:09 +0200

    Move data filling out of migrations.

--- django/kpiexport/migrations/0005_add_modified_and_move_data_generation.py
+++ django/kpiexport/migrations/0005_add_modified_and_move_data_generation.py
@@ -1,23 +1,6 @@
 # Generated manually 2.2.16 on 2021-07-29 11:26
 
 from django.db import migrations, models
-from kpiexport.tasks import update_auditlog_project_status_data_task
-from datetime import date, timedelta
-from kpiexport.tasks import update_auditlog_project_stages_data_task
-
-
-def pre_fill_data_project_status(apps, schema_editor):
-    generate_date = date.today() - timedelta(days=365)
-    while generate_date <= date.today():
-        update_auditlog_project_status_data_task(generate_date)
-        generate_date = generate_date + timedelta(days=1)
-
-
-def pre_fill_data_project_stages(apps, schema_editor):
-    generate_date = date.today() - timedelta(days=365)
-    while generate_date <= date.today():
-        update_auditlog_project_stages_data_task(generate_date)
-        generate_date = generate_date + timedelta(days=1)
 
 
 class Migration(migrations.Migration):
@@ -47,7 +30,4 @@ class Migration(migrations.Migration):
             name='modified',
             field=models.DateTimeField(auto_now=True),
         ),
-        migrations.RunPython(pre_fill_data_project_status, migrations.RunPython.noop),
-        migrations.RunPython(pre_fill_data_project_stages, migrations.RunPython.noop),
-
     ]
