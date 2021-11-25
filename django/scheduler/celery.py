f9e29f3bc44a2f3ff7454a79c71c20362a72dc8b
Gyorgy Fekete <f@pulilab.com>
2021-11-25 15:24:24 +0100

    install dotenv

--- django/scheduler/celery.py
+++ django/scheduler/celery.py
@@ -2,7 +2,9 @@ import os
 
 from celery import Celery
 from django.conf import settings
+from who_maps import load_env
 
+load_env.load_env()
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "who_maps.settings")
 
 app = Celery("scheduler", set_as_current=True)
