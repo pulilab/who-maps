e19af71fbdb991cb9abb8a74eced5c8fb0812314
Tibor Adam <tibor.adam@gmail.com>
2021-12-05 21:35:51 +0100

    Properly reorder fields

--- frontend/components/project/sections/view/Overview.vue
+++ frontend/components/project/sections/view/Overview.vue
@@ -29,6 +29,7 @@ export default {
   data () {
     return {
       loading: true,
+      donors: [],
       organisation: {},
       country: {},
       members: [],
@@ -39,15 +40,17 @@ export default {
     ...mapGetters({
       getOrganisationDetails: 'system/getOrganisationDetails',
       getCountryDetails: 'countries/getCountryDetails',
-      getProfiles: 'system/getUserProfilesNoFilter'
+      getProfiles: 'system/getUserProfilesNoFilter',
+      getDonors: 'system/getDonors'
     }),
     fields () {
       if (!isEmpty(this.project)) {
-        const { organisation, country, team, viewers } = this.project
+        const { donors, organisation, country, team, viewers } = this.project
         this.organisation = this.handleDetails(
           organisation,
           'getOrganisationDetails'
         )
+        this.donors = getList(donors, this.getDonors)
         this.country = this.handleDetails(country, 'getCountryDetails')
         this.members = getList(team, this.getProfiles)
         this.viewers = getList(viewers, this.getProfiles)
@@ -81,24 +84,36 @@ export default {
         {
           id: 2,
           prepend: 2,
-          header: this.$gettext('Organisation'),
-          content: this.handleCheckKey(this.organisation, 'name')
+          header: this.$gettext('Investment partner(s)'),
+          content: this.donors
         },
         {
           id: 3,
           prepend: 3,
-          header: this.$gettext('Project country'),
-          content: this.handleCheckKey(this.country, 'name')
+          header: this.$gettext('Implementing partner(s)'),
+          content: this.project.implementing_partners
         },
         {
           id: 4,
           prepend: 4,
-          header: this.$gettext('Geographic scope'),
-          content: this.project.geographic_scope
+          header: this.$gettext('Organisation'),
+          content: this.handleCheckKey(this.organisation, 'name')
         },
         {
           id: 5,
           prepend: 5,
+          header: this.$gettext('Project country'),
+          content: this.handleCheckKey(this.country, 'name')
+        },
+        {
+          id: 6,
+          prepend: 6,
+          header: this.$gettext('Geographic scope'),
+          content: this.project.geographic_scope
+        },
+        {
+          id: 7,
+          prepend: 7,
           header: this.$gettext(
             'Overview of the digital health implementation'
           ),
@@ -111,15 +126,15 @@ export default {
               id: 100,
               cols: [
                 {
-                  id: 6,
-                  prepend: 6,
+                  id: 8,
+                  prepend: 8,
                   header: this.$gettext('Contact name'),
                   content: this.project.contact_name,
                   span: 11
                 },
                 {
-                  id: 7,
-                  prepend: 7,
+                  id: 9,
+                  prepend: 9,
                   header: this.$gettext('Contact email'),
                   content: this.project.contact_email,
                   span: 13
@@ -129,14 +144,14 @@ export default {
           ]
         },
         {
-          id: 8,
-          prepend: 8,
+          id: 10,
+          prepend: 10,
           header: this.$gettext('Team members'),
           content: this.members
         },
         {
-          id: 9,
-          prepend: 9,
+          id: 11,
+          prepend: 11,
           header: this.$gettext('Viewers'),
           content: this.viewers
         }
