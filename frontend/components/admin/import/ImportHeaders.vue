20c48a494ae534e8555583c38b39455ae9046e1e
Tibor Adam <tibor.adam@gmail.com>
2022-07-05 04:17:15 +0200

    Add automatic smart column matching

--- frontend/components/admin/import/ImportHeaders.vue
+++ frontend/components/admin/import/ImportHeaders.vue
@@ -3,6 +3,7 @@
     <div v-if="internalValue.length > 0" class="Row">
       <div class="Column Thin Header Selected FlexCol">
         <slot :columns="columnState" />
+        <el-button size="mini" @click="matchHeaders">Smart match</el-button>
       </div>
 
       <div
@@ -75,6 +76,12 @@ import { projectFields } from '@/utilities/projects'
 const blackList = ['coverage', 'national_level_deployment',
   'coverageData', 'coverageType', 'coverage_second_level', 'interoperability_links', 'team', 'viewers']
 const addendumFields = ['clients', 'health_workers', 'facilities', 'sub_level']
+
+const isSubset = (fieldTitle, words) => {
+  if (words.length < fieldTitle.length) return false
+  return fieldTitle.every(element => words.includes(element))
+}
+
 export default {
   props: {
     headers: {
@@ -97,8 +104,145 @@ export default {
   data () {
     return {
       internalValue: null,
-      additonalHeader: null
+      additonalHeader: null,
+      smartHeaderMap: {
+        name: {
+          title: 'Project Name',
+          check: (words) => isSubset(['project','name'], words)
+        },
+        country: {
+          title: 'Country',
+          check: (words) => words.includes('country')
+        },
+        donors: {
+          title: 'Investors',
+          check: (words) => words.includes('investor')
+        },
+        organisation: {
+          title: 'Organisation',
+          check: (words) => words.includes('organisation')
+        },
+        geographic_scope: {
+          title: 'Geographic Scope',
+          check: (words) => isSubset(['geographic','scope'], words)
+        },
+        implementation_overview: {
+          title: 'Narrative of the project',
+          check: (words) => isSubset(['narrative','project'], words)
+        },
+        start_date: {
+          title: 'Start Date',
+          check: (words) => isSubset(['start','date'], words)
+        },
+        end_date: {
+          title: 'End Date',
+          check: (words) => isSubset(['end','date'], words)
+        },
+        end_date_note: {
+          title: 'End Date Note',
+          check: (words) => isSubset(['end','date','note'], words)
+        },
+        contact_name: {
+          title: 'Contact name',
+          check: (words) => isSubset(['contact','name'], words)
+        },
+        contact_email: {
+          title: 'Contact email',
+          check: (words) => isSubset(['contact','email'], words)
+        },
+        platforms: {
+          title: 'Software',
+          check: (words) => words.includes('software')
+        },
+        health_focus_areas: {
+          title: 'Health Focus Areas',
+          check: (words) => isSubset(['health','focus'], words)
+        },
+        hsc_challenges: {
+          title: 'Health System Challenges',
+          check: (words) => isSubset(['health','challenges'], words)
+        },
+        his_bucket: {
+          title: 'Health Information Systems',
+          check: (words) => isSubset(['health','information'], words)
+        },
+        government_investor: {
+          title: 'Government Contribution',
+          check: (words) => words.includes('contribution')
+        },
+        implementing_partners: {
+          title: 'Partners',
+          check: (words) => words.includes('partners')
+        },
+        implementing_team: {
+          title: 'Team',
+          check: (words) => words.includes('team')
+        },
+        implementing_viewers: {
+          title: 'Viewers',
+          check: (words) => words.includes('viewers')
+        },
+        implementation_dates: {
+          title: 'Technology Deployment Date',
+          check: (words) => isSubset(['deployment','date'], words)
+        },
+        licenses: {
+          title: 'Licenses',
+          check: (words) => words.includes('licenses')
+        },
+        repository: {
+          title: 'Link to Code documentation',
+          check: (words) => isSubset(['code','documentation'], words)
+        },
+        wiki: {
+          title: 'Link to Wiki',
+          check: (words) => words.includes('wiki')
+        },
+        mobile_application: {
+          title: 'Link to application',
+          check: (words) => words.includes('application')
+        },
+        interoperability_links: {
+          title: 'Interoperability Links',
+          check: (words) => isSubset(['interoperability','links'], words)
+        },
+        interoperability_standards: {
+          title: 'Interoperability Standards',
+          check: (words) => isSubset(['interoperability','standards'], words)
+        },
+        health_workers: {
+          title: 'Health Workers',
+          check: (words) => isSubset(['health','workers'], words)
+        },
+        clients: {
+          title: 'Clients',
+          check: (words) => words.includes('clients')
+        },
+        facilities: {
+          title: 'Facilities',
+          check: (words) => words.includes('facilities')
+        },
+        sub_level: {
+          title: 'Coverage Type',
+          check: (words) => words.includes('coverage')
+        },
+        digitalHealthInterventions: {
+          title: 'Digital Health Interventions (associated to the Software)',
+          check: (words) => isSubset(['health','interventions'], words)
+        },
+        hsc_challenges_other: {
+          title: 'Health System Challenges other',
+          check: (words) => false
+        },
+        research: {
+          title: 'Research',
+          check: (words) => false
+        },
+      }
     }
+  },
+  mounted() {
+    this.matchHeaders()
   },
   computed: {
     fields () {
@@ -172,6 +316,28 @@ export default {
     async columnChange () {
       const { data } = await this.$axios.patch(`/api/projects/import/${this.id}/`, { header_mapping: this.internalValue })
       this.$emit('update:headers', data.header_mapping)
+    },
+    smartFieldMatch(column) {
+      // 2. try match the exact title (based on template)
+      let fieldFound = this.notUsedFields.find(f => f.label === column)
+      if (!fieldFound) {
+        // 3. run through notUsedFields and run the check() function of corresponding smartHeaderMap
+        const words = column.toLowerCase().split(/[\s_]+/)
+        fieldFound = this.notUsedFields.find(field => this.smartHeaderMap[field.value].check(words))
+      }
+      return fieldFound
+    },
+    matchHeaders() {
+      let hasMapped = false
+      // 1. filter the unselected fields, then try to match them
+      this.internalValue.filter(f => !f.selected && !f.title.includes('__EMPTY')).forEach(column => {
+        const fieldMatch = this.smartFieldMatch(column.title)
+        if (fieldMatch) {
+          column.selected = fieldMatch.value
+          hasMapped = true
+        }
+      })      
+      if (hasMapped) this.columnChange()
     }
   }
 }
