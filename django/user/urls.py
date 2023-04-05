fff1679e573fa9b0f24e8a6138f9dc3b1879749f
Gyorgy Fekete <f@pulilab.com>
2023-04-05 11:23:49 +0200

    refactor user paths

--- django/user/urls.py
+++ django/user/urls.py
@@ -1,8 +1,12 @@
-from django.conf.urls import url, include
+from django.conf.urls import include
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 from allauth.account.views import confirm_email
-from rest_framework_jwt.views import obtain_jwt_token
-
+# from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 import user.views as views
 
 router = DefaultRouter()
@@ -11,16 +15,21 @@ router.register(r'userprofiles', views.UserProfileListViewSet)
 router.register(r'organisations', views.OrganisationViewSet)
 
 urlpatterns = [
-    url(r"^", include(router.urls)),
-    url(r"^", include("django.contrib.auth.urls")),
-    url(r"^all-auth/", include("allauth.urls")),
-    url(r"^rest-auth/", include("rest_auth.urls")),
-    url(r"^rest-auth/registration/", include("rest_auth.registration.urls")),
-    url(r'^api-token-auth/', obtain_jwt_token, name="api_token_auth"),
-    url(r"^email-confirmation/(?P<key>\w+)/$", confirm_email, name="account_confirm_email"),
-    url(r"^token/get/", view=views.TokenViewSet.as_view({'get': 'get'}), name="token-get"),
-    url(r"^token/create/", view=views.TokenViewSet.as_view({'post': 'create'}), name="token-create"),
-    url(r"^token/refresh/", view=views.TokenViewSet.as_view({'post': 'refresh'}), name="token-refresh"),
-    url(r"^token/delete/", view=views.TokenViewSet.as_view({'delete': 'delete'}), name="token-delete"),
-    url(r"^token/validate/", view=views.TokenCheckView.as_view(), name="token-check"),
+    path("", include(router.urls)),
+    path("", include("django.contrib.auth.urls")),
+    path("all-auth/", include("allauth.urls")),
+    path("rest-auth/", include("dj_rest_auth.urls")),
+    path("rest-auth/registration/", include("dj_rest_auth.registration.urls")),
+
+    path("email-confirmation/<key>/", confirm_email, name="account_confirm_email"),
+    path("token/get/", view=views.TokenViewSet.as_view({'get': 'get'}), name="token-get"),
+    path("token/create/", view=views.TokenViewSet.as_view({'post': 'create'}), name="token-create"),
+    path("token/refresh/", view=views.TokenViewSet.as_view({'post': 'refresh'}), name="token-refresh"),
+    path("token/delete/", view=views.TokenViewSet.as_view({'delete': 'delete'}), name="token-delete"),
+    path("token/validate/", view=views.TokenCheckView.as_view(), name="token-check"),
+
+    # path('api-token-auth/', obtain_jwt_token, name="api_token_auth"),  # DEPRECATED
+
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
