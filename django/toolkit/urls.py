6b7b71177b5083e4166a8889e2b123081e2d63ed
Gyorgy Fekete <f@pulilab.com>
2023-04-06 15:15:57 +0200

    fix toolkit urls

--- django/toolkit/urls.py
+++ django/toolkit/urls.py
@@ -3,12 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("^projects/<project_id>/toolkit/score/",
+    path("projects/<project_id>/toolkit/score/",
          view=views.ToolkitViewSet.as_view({
              "post": "create"
          }),
          name="toolkit-scores"),
-    path("^projects/<project_id>/toolkit/data/",
+    path("projects/<project_id>/toolkit/data/",
          view=views.ToolkitViewSet.as_view({
              "get": "retrieve"
          }),
