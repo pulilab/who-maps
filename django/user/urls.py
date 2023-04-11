b335a3fc2141ca0afd6aceb7d4cf5b0d16d5bfe4
Gyorgy Fekete <f@pulilab.com>
2023-04-11 15:33:35 +0200

    remove deprecated url

--- django/user/urls.py
+++ django/user/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import include
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from allauth.account.views import confirm_email
-# from rest_framework_jwt.views import obtain_jwt_token
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -27,8 +26,6 @@ urlpatterns = [
     path("token/refresh/", view=views.TokenViewSet.as_view({'post': 'refresh'}), name="token-refresh"),
     path("token/delete/", view=views.TokenViewSet.as_view({'delete': 'delete'}), name="token-delete"),
     path("token/validate/", view=views.TokenCheckView.as_view(), name="token-check"),
-
-    # path('api-token-auth/', obtain_jwt_token, name="api_token_auth"),  # DEPRECATED
 
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
