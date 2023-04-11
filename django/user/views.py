341663a03071eca15e1e3c65f53acfb25ed3789f
Gyorgy Fekete <f@pulilab.com>
2023-04-11 15:33:46 +0200

    fix user update permissions

--- django/user/views.py
+++ django/user/views.py
@@ -22,7 +22,7 @@ from rest_framework.permissions import IsAuthenticated
 from user.authentication import BearerTokenAuthentication
 
 
-class UserProfileViewSet(TokenAuthMixin, RetrieveModelMixin, UpdateModelMixin, GenericViewSet):
+class UserProfileViewSet(TokenAuthMixin, UpdateModelMixin, GenericViewSet):
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
 
@@ -43,8 +43,25 @@ class UserProfileViewSet(TokenAuthMixin, RetrieveModelMixin, UpdateModelMixin, G
         if hasattr(request.user, 'userprofile'):
             serializer = UserProfileSerializer(request.user.userprofile, context=dict(request=request))
             return Response(data=serializer.data)
-        else:
+        else:  # pragma: no cover
             raise ValidationError({"user_profile": "UserProfile doesn't exist"})
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        if instance.id == request.user.userprofile.id:
+            serializer = self.get_serializer(instance, data=request.data, partial=partial)
+            serializer.is_valid(raise_exception=True)
+            self.perform_update(serializer)
+
+            if getattr(instance, '_prefetched_objects_cache', None):  # pragma: no cover
+                # If 'prefetch_related' has been applied to a queryset, we need to
+                # forcibly invalidate the prefetch cache on the instance.
+                instance._prefetched_objects_cache = {}
+
+            return Response(serializer.data)
+        else:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
 
 
 class UserProfileListViewSet(TokenAuthMixin, ListModelMixin, GenericViewSet):
