ee1e60f3c8b57158de0ca9fc15f0b05195c2ba09
Tibor <82872706+TiborPL@users.noreply.github.com>
2023-05-03 10:42:56 +0200

    add client condition to isLoggedIn middleware

--- frontend/middleware/isLoggedIn.js
+++ frontend/middleware/isLoggedIn.js
@@ -1,5 +1,6 @@
 export default async function ({ redirect, $auth, app, route }) {
-  if (!$auth.$state.loggedIn) {
+  // sometimes $auth is not ready on server side
+  if (process.client && !$auth.$state.loggedIn) {
     const routeParams = {
       name: 'organisation-login',
       params: route.params,
