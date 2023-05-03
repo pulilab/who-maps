16bd20100f2e7603abab7e882c08e373094c0171
Tibor <82872706+TiborPL@users.noreply.github.com>
2023-05-03 10:43:28 +0200

    check different edge cases and auth config

--- frontend/plugins/axios.js
+++ frontend/plugins/axios.js
@@ -1,5 +1,10 @@
 export default function ({ $axios, app, store, redirect }) {
-  $axios.onRequest(config => {
+
+  /* $axios.onRequestError(error => {
+    console.log('🚀 ~ file: axios.js:4 ~ onRequestError:', error)
+  }) */
+
+  $axios.onRequest(async config => {
     if (config) {
       const lng = app.i18n.locale
       if (lng) {
@@ -9,11 +14,32 @@ export default function ({ $axios, app, store, redirect }) {
     }
   })
 
-  $axios.onError(async (error) => {
-    if (error?.name === 'ExpiredAuthSessionError') {
+  $axios.onResponse(response => {
+    console.log(`[${response.status}] ${response.config.url}`)
+  })
+
+  $axios.onResponseError(async error => {
+    const errorName = error?.name
+    const responseCode = error.response ? error.response.status : undefined
+    console.log('🚀 ~ file: axios.js:23 ~ errorName:', errorName)
+    console.log('🚀 ~ file: axios.js:24 ~ responseCode:', responseCode)
+    if (errorName === 'ExpiredAuthSessionError' || responseCode === 401) {
       await store.dispatch('user/logout')
       const path = app.localePath({ name: 'organisation-login' })
       redirect(path)
     }
+    throw error
   })
+
+  /* $axios.onError(async (error) => {
+    const errorName = error?.name
+    console.log('🚀 ~ file: axios.js:19 ~ $axios.onError ~ errorName:', errorName)
+    // if (errorName === 'TypeError') return Promise.reject(error)
+    if (errorName === 'ExpiredAuthSessionError' || errorName === 'TypeError') {
+      await store.dispatch('user/logout')
+      const path = app.localePath({ name: 'organisation-login' })
+      redirect(path)
+      return Promise.reject(error)
+    }
+  }) */
 }
