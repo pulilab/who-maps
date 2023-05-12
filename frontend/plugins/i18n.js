e9af2e6ffccf92cd9168514ca76b96f7490689f0
Tibor <82872706+TiborPL@users.noreply.github.com>
2023-05-12 10:35:06 +0200

    add translate tag white trim for frontend too

--- frontend/plugins/i18n.js
+++ frontend/plugins/i18n.js
@@ -5,32 +5,63 @@ Vue.component('Translate', TranslateWrapper)
 
 Vue.mixin({
   methods: {
-    $gettext (word, parameters, debug) {
-      let translated = this.$t(word, parameters)
-      if (debug) {
-        console.log(this.$i18n.messages[this.$i18n.locale][word])
-        console.log(this.$te(word))
-        console.log(`${word} => ${translated}`, parameters)
+    $gettext(word, parameters, debug) {
+      if (!word) {
+        this.$sentry.captureMessage('Empty or invalid text provided.', {
+          level: 'warning',
+          extra: {
+            translation: word,
+            translation_parameters: parameters,
+          },
+        })
+        return ''
       }
-      if (!this.$te(word) && parameters) {
+      const trimmedWord = word.replace(/\s+\n/g, '\n').replace(/\n\s+/g, '\n').replace(/\t/g, ' ').replace(/\n/g, ' ').replace(/ +/g, ' ')
+      let translated = this.$t(trimmedWord, parameters)
+      if (!this.$te(trimmedWord)) {
+        if (!word || trimmedWord.includes('_')) {
+          this.$sentry.captureMessage('Translation string invalid', {
+            level: 'warning',
+            extra: {
+              translation: word,
+              translation_trimmed: trimmedWord,
+              translation_parameters: parameters,
+            },
+          })
+        } else {
+          this.$sentry.captureMessage('No translation found, default string used to display', {
+            level: 'warning',
+            extra: {
+              translation: word,
+              translation_trimmed: trimmedWord,
+              translation_parameters: parameters,
+            },
+          })
+        }
+      }
+      if (debug) {
+        console.log(this.$i18n.messages[this.$i18n.locale][trimmedWord])
+        console.log(this.$te(trimmedWord))
+        console.log(`${trimmedWord} => ${translated}`, parameters)
+      }
+      if (!this.$te(trimmedWord) && parameters) {
         for (const k in parameters) {
           translated = translated.replace(`{${k}}`, parameters[k])
         }
       }
       return translated
-    }
-  }
+    },
+  },
 })
 
-Vue.filter('translate', value => value)
+Vue.filter('translate', (value) => value)
 
 export default function ({ app: { store, i18n } }) {
   // beforeLanguageSwitch called right before setting a new locale
-  i18n.beforeLanguageSwitch = (oldLocale, newLocale) => {
-  }
+  i18n.beforeLanguageSwitch = (oldLocale, newLocale) => {}
   // onLanguageSwitched called right after a new locale has been set
   i18n.onLanguageSwitched = (oldLocale, newLocale) => {
     store.dispatch('system/loadStaticData')
     store.dispatch('projects/loadProjectStructure', true)
   }
-};
+}
