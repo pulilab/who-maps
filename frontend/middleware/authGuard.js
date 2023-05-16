7401f39acdc86409afedf3bfa819d66896e3f9db
Tibor <82872706+TiborPL@users.noreply.github.com>
2023-05-16 10:09:48 +0200

    remove console.info() from authGuard

--- frontend/middleware/authGuard.js
+++ frontend/middleware/authGuard.js
@@ -1,7 +1,5 @@
 export default async function ({ app, $auth, route, redirect }) {
-  const executed = process.client ? 'client' : 'server'
   const refreshTokenValid = $auth.strategy.refreshToken.status().valid()
-  console.info(`authGuard on [${executed}] [loggedIn: ${$auth.$state.loggedIn}] [refreshToken: ${refreshTokenValid}] [route: ${route.path}]`)
   if (!refreshTokenValid) {
     const routeParams = {
         name: 'organisation-login',
