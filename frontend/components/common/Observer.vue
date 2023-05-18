09e2ce706e9d4b71819377632b0e25aa0abae599
Tibor <82872706+TiborPL@users.noreply.github.com>
2023-05-19 00:37:16 +0200

    remove console.log from observer component

--- frontend/components/common/Observer.vue
+++ frontend/components/common/Observer.vue
@@ -25,7 +25,6 @@ export default {
     const options = this.options || {}
     this.observer = new IntersectionObserver(([entry]) => {
       if (entry && entry.isIntersecting) {
-        console.log('🚀 ~ file: Observer.vue:28 ~ this.observer=newIntersectionObserver ~ entry:', entry)
         this.$emit('intersect', this.target)
       }
     }, options)
