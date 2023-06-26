6ece30d113d39661271b4edbd3410af7984c90f9
Gyorgy Fekete <f@pulilab.com>
2023-06-26 13:50:40 +0200

    remove imports

--- django/kpiexport/serializers.py
+++ django/kpiexport/serializers.py
@@ -1,6 +1,3 @@
-# from rest_framework import serializers
-# from kpiexport.models import AuditLogHFA
-
 # DEPRECATED: replaced by new JSON ORM functions
 
 # class AuditLogHFABasicSerializer(serializers.ModelSerializer):
