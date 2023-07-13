3b6c02baa4713853b2ccede481357428422f55a3
Gyorgy Fekete <f@pulilab.com>
2023-07-13 14:26:33 +0200

    max size validator for admin

---
+++ django/country/validators.py
@@ -0,0 +1,8 @@
+from django.core.exceptions import ValidationError
+from django.conf import settings
+
+
+def file_size(value):
+    if value.size > settings.MAX_ROAD_MAP_DOCUMENT_UPLOAD_SIZE:
+        raise ValidationError(f'File too large. '
+                              f'Size should not exceed {round(settings.MAX_ROAD_MAP_DOCUMENT_UPLOAD_SIZE/1024**2)} MB.')
