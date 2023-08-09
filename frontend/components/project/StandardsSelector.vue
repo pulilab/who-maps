1ef369eb5363d8c81f37873909e0028306675704
Tibor <82872706+TiborPL@users.noreply.github.com>
2023-08-09 14:49:52 +0200

    refine grouping of `StandardsSelector`

--- frontend/components/project/StandardsSelector.vue
+++ frontend/components/project/StandardsSelector.vue
@@ -1,17 +1,22 @@
 <template>
   <el-checkbox-group
     :value="value"
-    class="ThreePerRow"
+    class="TwoPerRow"
     @input="changeHandler"
   >
-    <el-checkbox
-      v-for="standard in standards"
-      :key="standard.id"
-      :label="standard.id"
-    >
-      {{ standard.name }}
-    </el-checkbox>
-    <tooltip :text="$gettext('Specify other digital health systems that ') | translate" />
+    <div v-for="(group,i) in standardsGroups" :key="i" class="Group">
+      <div class="name">{{ group.name }}</div>
+      <div class="inputs">
+        <el-checkbox
+          v-for="standard in getStandards(group.standardIds)"
+          :key="standard.id"
+          :label="standard.id"
+        >
+          {{ standard.name }}
+        </el-checkbox>
+      </div>
+      <tooltip :text="$gettext('Specify other digital health systems that ') | translate" />
+    </div>
   </el-checkbox-group>
 </template>
 
@@ -37,25 +42,22 @@ export default {
           standardIds: [1,4,5,6,17,8,9,11,13,18,15,16]
         },
         {
-          name: this.$gettext('Security & Privacy Standards'),
-          standardIds: [2,3,12]
-        },
-        {
           name: this.$gettext('Health Data Standardization'),
           standardIds: [28,21,27,30,22,31,26]
-        },
-        {
-          name: this.$gettext('Technical Standards'),
-          standardIds: [19,7,10,14,20,29]
         },
         {
           name: this.$gettext('Demographic Data Standardization'),
           standardIds: [24,25,23]
         },
+        {
+          name: this.$gettext('Security & Privacy Standards'),
+          standardIds: [2,3,12]
+        },
+        {
+          name: this.$gettext('Technical Standards'),
+          standardIds: [19,7,10,14,20,29]
+        },
       ],
-      standardsTooltips: [
-        {}
-      ]
     }
   },
   computed: {
@@ -66,13 +68,37 @@ export default {
   methods: {
     changeHandler (value) {
       this.$emit('change', value)
-    }
+    },
+    getStandards(ids) {
+      return this.standards.filter(s => ids.includes(s.id))
+    },
   }
 }
 </script>
 
-<style lang="less">
+<style lang="less" scoped>
   @import "../../assets/style/variables.less";
   @import "../../assets/style/mixins.less";
 
+  .TwoPerRow {
+    display: flex;
+    flex-wrap: wrap;
+    row-gap: 16px;
+    column-gap: 32px;
+
+    .Group {
+      padding: 8px 0;
+      .name {
+        margin-bottom: 10px;
+        font-size: @fontSizeBase;
+        border-bottom: 1px solid @colorGrayLight;
+      }
+      .inputs {
+        label {
+          display: block;
+          padding: 6px 0;
+        }
+      }
+    }
+  }
 </style>
