254ca73c7c22891e2de5ae77a04bfcac2d1e6813
Tibor <82872706+TiborPL@users.noreply.github.com>
2023-08-17 02:09:11 +0200

    remove `hsc` tooltips

--- frontend/components/project/HealthSystemChallengesSelector.vue
+++ frontend/components/project/HealthSystemChallengesSelector.vue
@@ -21,11 +21,11 @@
         class="hsc-item"
       >
         <span>{{ hsc.challenge }}</span>
-        <el-tooltip v-show="getChallengeTooltip(hsc.id)" class="item" effect="dark" popper-class="hsc-tooltip" :content="getChallengeTooltip(hsc.id)">
+        <!-- <el-tooltip v-show="getChallengeTooltip(hsc.id)" class="item" effect="dark" popper-class="hsc-tooltip" :content="getChallengeTooltip(hsc.id)">
           <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
           </svg>
-        </el-tooltip>
+        </el-tooltip> -->
       </el-option>
     </el-option-group>
   </lazy-el-select>
