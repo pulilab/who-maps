41bdd08d822729ce2f67a27dd16e10c01c83c2c0
Gyorgy Fekete <f@pulilab.com>
2023-10-05 14:22:14 +0200

    country admin only permission

--- django/country/permissions.py
+++ django/country/permissions.py
@@ -70,3 +70,8 @@ class InDonorSuperAdmin(permissions.BasePermission):
             return True
         else:
             return obj.donor.super_admins.filter(id=request.user.userprofile.id).exists()
+
+
+class CountryAdminOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.userprofile.is_government_type() and request.user.userprofile.account_type_approved
