7820265065bcce93a5a5e7bb23040b3c2a2b612d
Gyorgy Fekete <f@pulilab.com>
2023-10-05 14:22:45 +0200

    fix imports

--- django/project/models.py
+++ django/project/models.py
@@ -2,10 +2,12 @@ from collections import namedtuple
 from copy import deepcopy
 
 from django.contrib.contenttypes.fields import GenericRelation
+from django.db.models.fields.json import KeyTextTransform
+from django.db.models.functions import Cast
 from hashids import Hashids
 
 from django.db import models
-from django.db.models import Q
+from django.db.models import Q, IntegerField
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
