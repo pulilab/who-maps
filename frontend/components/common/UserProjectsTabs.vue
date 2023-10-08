5e717fd85652fd5717395a917690db0f77c6c93a
Tibor <ta@pulilab.com>
2023-10-08 23:06:55 +0200

    set up new layout and stores

--- frontend/components/common/UserProjectsTabs.vue
+++ frontend/components/common/UserProjectsTabs.vue
@@ -2,22 +2,22 @@
   <div>
     <div class="tab-wrapper">
       <div class="tabs">
-        <div @click="setActiveTab(0)" :class="getStyle(0)">
+        <div @click="setActiveTab(0)" :class="getStyle(0)" key="my">
           <i class="el-icon-star-off"></i>
           <translate :parameters="{ count: counters.myprojects }">
             My projects ({count})
           </translate>
         </div>
-        <div @click="setActiveTab(3)" :class="getStyle(3)">
-          <fa icon="globe-americas" />
-          <translate :parameters="{ count: counters.myprojects }">
-            Country projects ({count})
-          </translate>
-        </div>
-        <div @click="setActiveTab(1)" :class="getStyle(1)">
+        <div @click="setActiveTab(1)" :class="getStyle(1)" class="archived">
           <i class="el-icon-takeaway-box"></i>
           <translate :parameters="{ count: counters.archive }">
-            Archived projects ({count})
+            My archived projects ({count})
+          </translate>
+        </div>
+        <div @click="setActiveTab(2)" :class="getStyle(2)" key="country">
+          <fa icon="globe-americas" />
+          <translate :parameters="{ count: total }">
+            Country projects ({count})
           </translate>
         </div>
       </div>
@@ -32,15 +32,38 @@
         <b><translate>Viewer</translate></b>
         &nbsp;<translate>of.</translate>
       </div>
-      <div v-if="activeTab === 1 && counters.archive > 0" key="archived">
-        <translate>Here are all your archived projects. You can restore individual projects by submitting a restore request.</translate>
+      <translate tag="div" v-if="activeTab === 1 && counters.archive > 0" key="archived">
+        Here are all your archived projects. You can restore individual projects by submitting a restore request.
+      </translate>
+      <div v-if="activeTab === 2" key="countryprojects" class="country-info">
+        <el-input
+          v-model="countrySearch"
+          clearable
+          :placeholder="$gettext('Type something...') | translate"
+        >
+          <fa slot="prepend" icon="search" />
+        </el-input>
+        <el-tooltip
+          v-model="showTooltip"
+          :content="$gettext(`Search your country's projects by project name, name of team member or e-mail address. You can change individual project's team members (co-editors).`) | translate"
+          placement="right"
+          popper-class="country-tooltip"
+          effect="dark"
+          manual
+        >
+          <el-icon class="el-icon-question help-icon" @click.native="showTooltip = !showTooltip" />
+        </el-tooltip>
       </div>
     </div>
-    <slot />
+    <slot :search="countrySearch" />
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+import debounce from 'lodash/debounce'
+const tabPrams = ['myprojects','archive','country-projects']
+
 export default {
   props: {
     activeTab: {
@@ -52,12 +75,26 @@ export default {
       required: true
     },
   },
+  data() {
+    return {
+      showTooltip: false,
+      countrySearch: ''
+    }
+  },
+  computed: {
+    ...mapGetters({
+      total: 'projects/getTotal',
+    }),
+  },
   watch: {
     activeTab(val) {
       if (process.client) {
-        const list = val === 1 ? 'archive' : 'myprojects'
-        this.$router.replace({ ...this.$route, query: { list } })
+        const list = tabPrams[val]
+        this.$router.push({ ...this.$route, query: { list } })
       }
+    },
+    countrySearch() {
+      this.emitSearch()
     }
   },
   methods: {
@@ -66,7 +103,11 @@ export default {
     },
     setActiveTab(tab) {
       this.$emit('change', tab)
-    }
+    },
+    emitSearch: debounce(function() {
+      this.$emit('search', this.countrySearch)
+
+    }, 500)
   }
 }
 </script>
@@ -122,5 +163,19 @@ export default {
   align-items: center;
   justify-content: center;
   padding-top: 2em;
+  .country-info {
+    display: flex;
+    align-items: center;
+    gap: 0.8em;
+    .help-icon {
+      cursor: pointer;
+      font-size: 1.4em;
+      color: @colorGray;
+
+      &:hover {
+        color: lighten(@colorGray, 10%);
+      }
+    }
+  }
 }
 </style>
